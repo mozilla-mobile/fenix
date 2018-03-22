88f0a2a48d573875b88d3ba518bfc60294d71b21
Sebastian Kaspari <s.kaspari@gmail.com>
2018-03-22 10:41:01 +0100

    Remember to look up at the stars and not down at your feet. 🌟🔭
    Ring-ding-ding-ding-dingeringeding!
    Gering-ding-ding-ding-dingeringeding!
    Gering-ding-ding-ding-dingeringeding!
    What the fox say? 🦊
    
    https://www.youtube.com/watch?v=ls3rD8VfiSY

---
+++ app/src/main/java/org/mozilla/phoenix/MainActivity.kt
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.phoenix
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
