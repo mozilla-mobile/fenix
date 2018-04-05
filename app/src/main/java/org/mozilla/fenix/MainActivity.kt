de881aa7dd511761cffb206ad6982d7af201a482
Sebastian Kaspari <s.kaspari@gmail.com>
2018-04-05 14:57:01 -0700

    Fenix.

--- app/src/main/java/org/mozilla/fenix/MainActivity.kt
+++ app/src/main/java/org/mozilla/fenix/MainActivity.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.phoenix
+package org.mozilla.fenix
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
