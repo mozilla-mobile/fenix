0d8582db922e88db3c0a90cb39f374482947ba07
Christian Sadilek <christian.sadilek@gmail.com>
2018-05-25 12:25:44 -0400

    Issue #35: Integrate search functionality

--- app/src/main/java/mozilla/fenix/components/Components.kt
+++ app/src/main/java/mozilla/fenix/components/Components.kt
@@ -5,9 +5,12 @@
 package mozilla.fenix.components
 
 import android.content.Context
+import kotlinx.coroutines.experimental.async
 import mozilla.components.browser.engine.gecko.GeckoEngine
+import mozilla.components.browser.search.SearchEngineManager
 import mozilla.components.browser.session.Session
 import mozilla.components.concept.engine.Engine
+import mozilla.components.feature.search.SearchUseCases
 import mozilla.components.feature.session.DefaultSessionStorage
 import mozilla.components.feature.session.SessionProvider
 import mozilla.components.feature.session.SessionUseCases
@@ -17,14 +20,25 @@ import org.mozilla.geckoview.GeckoRuntime
  * Helper class for lazily instantiating components needed by the application.
  */
 class Components(private val applicationContext: Context) {
+
+    // Engine
     private val geckoRuntime by lazy {
         GeckoRuntime.getDefault(applicationContext)
     }
     val engine : Engine by lazy { GeckoEngine(geckoRuntime) }
 
+    // Session
     val sessionProvider : SessionProvider by lazy {
         SessionProvider(Session("https://www.mozilla.org"), DefaultSessionStorage(applicationContext))
     }
-
     val sessionUseCases = SessionUseCases(sessionProvider, engine)
+
+    // Search
+    private val searchEngineManager by lazy {
+        SearchEngineManager().apply {
+            async { load(applicationContext) }
+        }
+    }
+    private val searchUseCases = SearchUseCases(applicationContext, searchEngineManager, sessionProvider)
+    val defaultSearchUseCase = { searchTerms: String -> searchUseCases.defaultSearch.invoke(searchTerms) }
 }
