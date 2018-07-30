49b58d31ccebba88a10ab43df95782e0a805b855
Sebastian Kaspari <s.kaspari@gmail.com>
2018-07-30 18:02:53 +0200

    Update to components 0.16.1.

--- app/src/main/java/mozilla/fenix/IntentReceiverActivity.kt
+++ app/src/main/java/mozilla/fenix/IntentReceiverActivity.kt
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package mozilla.fenix
 
 import android.app.Activity
@@ -24,5 +28,4 @@ class IntentReceiverActivity : Activity() {
         startActivity(intent)
         finish()
     }
-
 }
