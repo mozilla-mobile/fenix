e34f7e166da296ac8833a2d99e9885a27466e8c4
Jeff Boek <jeff@jeffboek.com>
2018-12-07 15:11:50 -0500

    Initializes Android project
    Co-authored-by: Colin Lee <mncolinlee@gmail.com>
    Co-authored-by: Sawyer Blatz <sdblatz@gmail.com>
    Co-authored-by: Emily Kager <emilykager@gmail.com>

---
+++ app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -0,0 +1,12 @@
+package org.mozilla.fenix
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
