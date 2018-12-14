9f29d7a846bc68cfe3b7f51c1940cdd4644df08b
Emily Kager <ekager@mozilla.com>
2018-12-13 16:51:06 -0800

    No issue: Use AndroidX, set up some quality plugins

--- app/src/androidTest/java/org/mozilla/fenix/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ExampleInstrumentedTest.kt
@@ -1,12 +1,11 @@
 package org.mozilla.fenix
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import junit.framework.Assert.assertEquals
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
---
+++ quality/pre-push-recommended.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# We recommend you run this as a pre-push hook: to reduce
+# review turn-around time, we want all pushes to run tests
+# locally. Using this hook will guarantee your hook gets
+# updated as the repository changes.
+#
+# This hook tries to run as much as possible without taking
+# too long.
+#
+# You can use it by running this command from the project root:
+# `ln -s ../../quality/pre-push-recommended.sh .git/hooks/pre-push`
+
+# Descriptions for each gradle task below can be found in the
+# output of `./gradlew tasks`.
+./gradlew -q \
+        checkstyle \
+        ktlint \
+        pmd \
+        detektCheck \
+        assembleAndroidTest \
+        testDebugUnitTest
+
+# Tasks omitted because they take a long time to run:
+# - unit test on all variants
+# - UI tests
+# - lint (compiles all variants)
