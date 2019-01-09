c1500fe0bc081ebff44c370bf73a18cff07482b0
Jeff Boek <jeff@jeffboek.com>
2019-01-09 14:22:58 -0800

    For #150 - Adds a home fragment

--- app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -1,12 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.fenix
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.mozilla.fenix.home.HomeFragment
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        supportFragmentManager?.beginTransaction()?.apply {
+            replace(R.id.container, HomeFragment.create())
+            commit()
+        }
+
     }
 }
---
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import org.mozilla.fenix.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    companion object {
+        fun create() = HomeFragment()
+    }
+}
