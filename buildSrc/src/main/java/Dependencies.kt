c65594010fd460461256fdbb29a4c2f9fd285007
Stefan Arentz <stefan@arentz.ca>
2019-01-22 12:38:04 -0500

    Upgrade to Android Components 0.40.0-SNAPSHOT

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -10,7 +10,7 @@ private object Versions {
     const val androidx_appcompat = "1.0.2"
     const val androidx_constraint_layout = "2.0.0-alpha3"
 
-    const val mozilla_android_components = "0.39.0-SNAPSHOT"
+    const val mozilla_android_components = "0.40.0-SNAPSHOT"
 
     const val junit = "4.12"
     const val test_tools = "1.0.2"
