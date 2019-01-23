5a3fc1325f5ef7d5c6792d4934c12db6b44077b1
Emily Kager <ekager@mozilla.com>
2019-01-23 14:48:15 -0800

    Adds toolbar with autocomplete and menu to BrowserFragment

--- app/src/main/java/org/mozilla/fenix/components/Components.kt
+++ app/src/main/java/org/mozilla/fenix/components/Components.kt
@@ -5,6 +5,7 @@
 package org.mozilla.fenix.components
 
 import android.content.Context
+import org.mozilla.fenix.components.toolbar.Toolbar
 
 /**
  * Provides access to all components.
@@ -13,4 +14,11 @@ class Components(private val context: Context) {
     val core by lazy { Core(context) }
     val search by lazy { Search(context) }
     val useCases by lazy { UseCases(context, core.sessionManager, search.searchEngineManager) }
+    val toolbar by lazy {
+        Toolbar(
+            context,
+            useCases.sessionUseCases,
+            core.sessionManager
+        )
+    }
 }
