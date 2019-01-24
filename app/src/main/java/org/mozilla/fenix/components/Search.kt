c1607de62f5f4a41cf9de59cda4946070edb61f5
Jeff Boek <jeff@jeffboek.com>
2019-01-24 13:07:52 -0800

    Adds missing licenses

--- app/src/main/java/org/mozilla/fenix/components/Search.kt
+++ app/src/main/java/org/mozilla/fenix/components/Search.kt
@@ -1,10 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.fenix.components
 
 import android.content.Context
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import mozilla.components.browser.search.SearchEngineManager
-
 
 /**
  * Component group for all search engine integration related functionality.
