89b1d05c6cadb32cc20d7b18a9bc86d46cbd3b06
Jeff Boek <jeff@jeffboek.com>
2019-01-25 09:06:10 -0800

    For #139 - Adds the sessions placeholder

--- app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -4,27 +4,20 @@
 
 package org.mozilla.fenix.home
 
-import android.content.Context
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.annotation.RequiresApi
+
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.FragmentNavigator
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.mozilla.fenix.R
+import org.mozilla.fenix.home.sessions.SessionsAdapter
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -43,7 +36,7 @@ class HomeFragment : Fragment() {
         menuButton.visibility = View.GONE
         privateBrowsingButton.visibility = View.GONE
 
-        sessionsAdapter = SessionsAdapter(requireContext())
+        sessionsAdapter = SessionsAdapter()
 
         toolbar_wrapper.clipToOutline = false
         toolbar.setOnClickListener { it ->
@@ -64,30 +57,5 @@ class HomeFragment : Fragment() {
         super.onCreate(savedInstanceState)
         enterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.fade)
         exitTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.fade)
-    }
-}
-
-// Temporary adapter
-private class SessionsAdapter(val context: Context) : RecyclerView.Adapter<SessionsAdapter.ViewHolder>() {
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {}
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val textView = TextView(context).apply {
-            val lp = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-            setLayoutParams(lp)
-            setText("This is a sample TextView...")
-            setTextColor(Color.parseColor("#ff0000"))
-        }
-        return ViewHolder(textView)
-    }
-
-    override fun getItemCount(): Int = 100
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = "Cell: ${position}"
     }
 }
---
+++ app/src/main/java/org/mozilla/fenix/home/sessions/SessionsAdapter.kt
@@ -0,0 +1,53 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.home.sessions
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.mozilla.fenix.R
+
+
+class SessionsAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
+
+        return when(viewType) {
+            HeaderViewHolder.LAYOUT_ID -> HeaderViewHolder(view)
+            EmptyListViewHolder.LAYOUT_ID -> EmptyListViewHolder(view)
+            else -> EmptyListViewHolder(view)
+        }
+    }
+
+    override fun getItemViewType(position: Int) = when (position) {
+        0 -> HeaderViewHolder.LAYOUT_ID
+        1 -> EmptyListViewHolder.LAYOUT_ID
+        else -> -1
+    }
+
+    override fun getItemCount(): Int = 2
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is HeaderViewHolder) {
+            holder.headerText.text = "Today"
+        }
+    }
+
+    private class HeaderViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        val headerText = view.findViewById<TextView>(R.id.header_text)
+
+        companion object {
+            const val LAYOUT_ID = R.layout.session_list_header
+        }
+    }
+
+    private class EmptyListViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        companion object {
+            const val LAYOUT_ID = R.layout.session_list_empty
+        }
+    }
+}
