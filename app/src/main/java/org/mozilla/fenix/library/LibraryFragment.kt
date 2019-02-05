4f3c0ee8ad7de3952bce154a0c449a2a45a74671
Emily Kager <ekager@mozilla.com>
2019-02-05 13:52:04 -0800

    Closes #378 - Creates Library Fragment UI and adds resources

---
+++ app/src/main/java/org/mozilla/fenix/library/LibraryFragment.kt
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.library
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.mozilla.fenix.R
+
+class LibraryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_library, container, false)
+    }
+}
