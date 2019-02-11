ee229c970938fc64a3937dd1811766c3a5dd47cd
Colin Lee <mncolinlee@gmail.com>
2019-02-10 19:30:37 -0600

    Fixes #354: Overflow menu for multitasking in current session

--- app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -17,14 +17,12 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
-import kotlinx.android.synthetic.main.tab_list_header.view.*
 import mozilla.components.browser.menu.BrowserMenu
 import mozilla.components.browser.session.Session
 import mozilla.components.browser.session.SessionManager
 import org.mozilla.fenix.DefaultThemeManager
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
-import org.mozilla.fenix.ext.increaseTapArea
 import org.mozilla.fenix.ThemeManager
 import org.mozilla.fenix.ext.requireComponents
 import org.mozilla.fenix.home.sessions.SessionsComponent
@@ -92,12 +90,7 @@ class HomeFragment : Fragment() {
         view.toolbar.setCompoundDrawablesWithIntrinsicBounds(searchIcon, null, null, null)
         val roundToInt = (toolbarPaddingDp * Resources.getSystem().displayMetrics.density).roundToInt()
         view.toolbar.compoundDrawablePadding = roundToInt
-        view.toolbar.setOnClickListener { it ->
-            val directions = HomeFragmentDirections.actionHomeFragmentToSearchFragment(null)
-            Navigation.findNavController(it).navigate(directions)
-        }
-        view.add_tab_button.increaseTapArea(addTabButtonIncreaseDps)
-        view.add_tab_button.setOnClickListener {
+        view.toolbar.setOnClickListener {
             val directions = HomeFragmentDirections.actionHomeFragmentToSearchFragment(null)
             Navigation.findNavController(it).navigate(directions)
         }
@@ -210,6 +203,7 @@ class HomeFragment : Fragment() {
 
     companion object {
         const val addTabButtonIncreaseDps = 8
+        const val overflowButtonIncreaseDps = 8
         const val toolbarPaddingDp = 12f
         const val firstKeyTriggerFrame = 55
         const val secondKeyTriggerFrame = 90
