ff9636255186211817ff8ca74270e257b54a52cf
Colin Lee <mncolinlee@gmail.com>
2019-02-13 15:18:03 -0600

    Fixes #488: LeakCanary crash

--- app/src/debug/java/org/mozilla/fenix/DebugFenixApplication.kt
+++ app/src/debug/java/org/mozilla/fenix/DebugFenixApplication.kt
@@ -33,11 +33,11 @@ class DebugFenixApplication : FenixApplication() {
     }
 
     internal class ToggleableHeapDumper(
-        private val context: Context,
+        context: Context,
         private val defaultDumper: HeapDumper
     ) : HeapDumper {
         var prefs: SharedPreferences? = PreferenceManager.getDefaultSharedPreferences(context)
         var enabled = prefs?.getBoolean(context.getPreferenceKey(pref_key_leakcanary), true) ?: true
-        override fun dumpHeap(): File = if (enabled) defaultDumper.dumpHeap() else HeapDumper.RETRY_LATER
+        override fun dumpHeap(): File? = if (enabled) defaultDumper.dumpHeap() else HeapDumper.RETRY_LATER
     }
 }
