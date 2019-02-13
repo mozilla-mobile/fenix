4c40f6ea4bfb236e6c0e80c825a9a403658d755d
Jeff Boek <jeff@jeffboek.com>
2019-02-12 16:04:01 -0800

    For #456 - Adds popover menu to menu button

--- app/src/main/java/org/mozilla/fenix/home/HomeMenu.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeMenu.kt
@@ -1,9 +1,56 @@
-package org.mozilla.fenix.home
-
 /* This Source Code Form is subject to the terms of the Mozilla Public
    License, v. 2.0. If a copy of the MPL was not distributed with this
    file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-class HomeMenu {
+package org.mozilla.fenix.home
 
+import android.content.Context
+import mozilla.components.browser.menu.BrowserMenuBuilder
+import mozilla.components.browser.menu.item.BrowserMenuDivider
+import mozilla.components.browser.menu.item.BrowserMenuImageText
+import org.mozilla.fenix.DefaultThemeManager
+import org.mozilla.fenix.R
+
+class HomeMenu(
+    private val context: Context,
+    private val onItemTapped: (Item) -> Unit = {}
+) {
+    sealed class Item {
+        object Help : Item()
+        object Settings : Item()
+        object Library : Item()
+    }
+
+    val menuBuilder by lazy { BrowserMenuBuilder(menuItems) }
+
+    private val menuItems by lazy {
+        listOf(
+            BrowserMenuImageText(
+                context.getString(R.string.browser_menu_settings),
+                R.drawable.ic_settings,
+                context.getString(R.string.browser_menu_settings),
+                DefaultThemeManager.resolveAttribute(R.attr.browserToolbarIcons, context)
+            ) {
+                onItemTapped.invoke(HomeMenu.Item.Settings)
+            },
+
+            BrowserMenuImageText(
+                context.getString(R.string.browser_menu_library),
+                R.drawable.ic_library,
+                context.getString(R.string.browser_menu_library),
+                DefaultThemeManager.resolveAttribute(R.attr.browserToolbarIcons, context)
+            ) {
+                onItemTapped.invoke(HomeMenu.Item.Library)
+            },
+
+            BrowserMenuDivider(),
+            BrowserMenuImageText(
+                context.getString(R.string.browser_menu_help),
+                R.drawable.ic_help,
+                context.getString(R.string.browser_menu_help),
+                DefaultThemeManager.resolveAttribute(R.attr.browserToolbarIcons, context)
+            ) {
+                onItemTapped.invoke(HomeMenu.Item.Help)
+            })
+    }
 }
