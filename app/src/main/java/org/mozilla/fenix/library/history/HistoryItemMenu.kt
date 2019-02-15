4263f8ab3e3e78e137edc116af39f47f8ac32dc5
Jeff Boek <jeff@jeffboek.com>
2019-02-15 14:19:15 -0800

    For #357 - Adds menu to delete a single item

---
+++ app/src/main/java/org/mozilla/fenix/library/history/HistoryItemMenu.kt
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.library.history
+
+import android.content.Context
+import mozilla.components.browser.menu.BrowserMenuBuilder
+import mozilla.components.browser.menu.item.SimpleBrowserMenuItem
+import org.mozilla.fenix.R
+
+class HistoryItemMenu(
+    private val context: Context,
+    private val onItemTapped: (Item) -> Unit = {}
+) {
+    sealed class Item {
+        object Delete : Item()
+    }
+
+    val menuBuilder by lazy { BrowserMenuBuilder(menuItems) }
+
+    private val menuItems by lazy {
+        listOf(
+            SimpleBrowserMenuItem(
+                context.getString(R.string.history_delete_item)
+            ) {
+                onItemTapped.invoke(Item.Delete)
+            }
+        )
+    }
+}
