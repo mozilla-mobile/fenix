6730306d9065b389af13d79361cee705b7cb1297
Emily Kager <ekager@mozilla.com>
2019-02-20 16:27:11 -0800

    Close #420 - Default Browser Preference reacts to default browser status

---
+++ app/src/main/java/org/mozilla/fenix/settings/DefaultBrowserPreference.kt
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.Switch
+import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+import mozilla.components.support.utils.Browsers
+import org.mozilla.fenix.R
+
+class DefaultBrowserPreference : Preference {
+
+    private var switchView: Switch? = null
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, attributeSetId: Int) : super(
+        context,
+        attrs,
+        attributeSetId
+    )
+
+    init {
+        widgetLayoutResource = R.layout.preference_default_browser
+    }
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+        switchView = holder.findViewById(R.id.switch_widget) as Switch
+
+        updateSwitch()
+    }
+
+    private fun updateSwitch() {
+        val browsers = Browsers.all(context)
+        switchView?.isChecked = browsers.isDefaultBrowser
+    }
+}
