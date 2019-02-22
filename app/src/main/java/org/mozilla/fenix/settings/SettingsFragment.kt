20294e78e6b6a9fc2fe6662ea70efd2f9f26c4f1
Colin Lee <mncolinlee@gmail.com>
2019-02-22 11:49:56 -0600

    Fixes #293: Adds about settings page
    Co-authored-by: Sawyer Blatz <sblatz@mozilla.com>

--- app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -5,6 +5,8 @@
 package org.mozilla.fenix.settings
 
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
@@ -13,19 +15,31 @@ import androidx.navigation.Navigation
 import androidx.preference.Preference
 import androidx.preference.Preference.OnPreferenceClickListener
 import androidx.preference.PreferenceFragmentCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import mozilla.components.support.ktx.android.graphics.toDataUri
 import org.mozilla.fenix.BuildConfig
 import org.mozilla.fenix.FenixApplication
 import org.mozilla.fenix.R
-import org.mozilla.fenix.R.string.pref_key_about
 import org.mozilla.fenix.R.string.pref_key_leakcanary
 import org.mozilla.fenix.R.string.pref_key_make_default_browser
 import org.mozilla.fenix.ext.getPreferenceKey
 import org.mozilla.fenix.ext.requireComponents
+import java.io.File
+import kotlin.coroutines.CoroutineContext
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), CoroutineScope {
+
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        job = Job()
         (activity as AppCompatActivity).supportActionBar?.show()
     }
 
@@ -36,21 +50,70 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onResume() {
         super.onResume()
 
+        generateWordmark()
         setupPreferences()
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        when (preference.key) {
+            resources.getString(R.string.pref_key_help) -> {
+                requireComponents.useCases.tabsUseCases.addTab
+                    .invoke(SupportUtils.getSumoURLForTopic(context!!, SupportUtils.SumoTopic.HELP))
+                navigateToSettingsArticle()
+            }
+            resources.getString(R.string.pref_key_rate) -> {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(SupportUtils.RATE_APP_URL)))
+            }
+            resources.getString(R.string.pref_key_feedback) -> {
+                requireComponents.useCases.tabsUseCases.addTab.invoke(SupportUtils.FEEDBACK_URL)
+                navigateToSettingsArticle()
+            }
+            resources.getString(R.string.pref_key_about) -> {
+                requireComponents.useCases.tabsUseCases.addTab.invoke(aboutURL, true)
+                navigateToSettingsArticle()
+            }
+        }
+        return super.onPreferenceTreeClick(preference)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+    /**
+     * Shrinks the wordmark resolution on first run to ensure About Page loads quickly
+     */
+    private fun generateWordmark() {
+        val path = context?.filesDir
+        val file = File(path, wordmarkPath)
+        path?.let {
+            if (!file.exists()) {
+                launch(IO) {
+                    val options = BitmapFactory.Options().apply {
+                        inSampleSize = wordmarkScalingFactor
+                        inJustDecodeBounds = false
+                    }
+                    file.appendText(
+                        BitmapFactory.decodeResource(
+                            resources,
+                            R.drawable.ic_logo_wordmark, options
+                        ).toDataUri()
+                    )
+                }
+            }
+        }
     }
 
     private fun setupPreferences() {
         val makeDefaultBrowserKey = context?.getPreferenceKey(pref_key_make_default_browser)
-        val aboutKey = context?.getPreferenceKey(pref_key_about)
         val leakKey = context?.getPreferenceKey(pref_key_leakcanary)
 
         val preferenceMakeDefaultBrowser = findPreference<Preference>(makeDefaultBrowserKey)
-        val preferenceAbout = findPreference<Preference>(aboutKey)
         val preferenceLeakCanary = findPreference<Preference>(leakKey)
 
         preferenceMakeDefaultBrowser.onPreferenceClickListener =
             getClickListenerForMakeDefaultBrowser()
-        preferenceAbout.onPreferenceClickListener = getAboutPageListener()
 
         preferenceLeakCanary.isVisible = BuildConfig.DEBUG
         if (BuildConfig.DEBUG) {
@@ -81,18 +144,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
         }
     }
 
-    private fun getAboutPageListener(): OnPreferenceClickListener {
-        return OnPreferenceClickListener {
-            requireComponents.useCases.tabsUseCases.addTab.invoke(aboutURL, true)
-            view?.let {
-                Navigation.findNavController(it)
-                    .navigate(SettingsFragmentDirections.actionGlobalBrowser(null))
-            }
-            true
+    private fun navigateToSettingsArticle() {
+        requireComponents.useCases.tabsUseCases.addTab.invoke(aboutURL, true)
+        view?.let {
+            Navigation.findNavController(it)
+                .navigate(SettingsFragmentDirections.actionGlobalBrowser(null))
         }
     }
 
     companion object {
+        const val wordmarkScalingFactor = 2
+        const val wordmarkPath = "wordmark.b64"
         const val aboutURL = "about:version"
     }
 }
