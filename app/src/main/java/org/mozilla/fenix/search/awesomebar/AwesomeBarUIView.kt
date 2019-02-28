812ac035eae9a8d80fecf388aaa48f6b5486d3c4
Jeff Boek <jeff@jeffboek.com>
2019-02-28 11:55:39 -0800

    No issue - pulls logic outside of the UIView

--- app/src/main/java/org/mozilla/fenix/search/awesomebar/AwesomeBarUIView.kt
+++ app/src/main/java/org/mozilla/fenix/search/awesomebar/AwesomeBarUIView.kt
@@ -3,7 +3,6 @@ package org.mozilla.fenix.search.awesomebar
    License, v. 2.0. If a copy of the MPL was not distributed with this
    file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import io.reactivex.Observable
@@ -16,15 +15,12 @@ import mozilla.components.feature.awesomebar.provider.SearchSuggestionProvider
 import mozilla.components.feature.awesomebar.provider.SessionSuggestionProvider
 import mozilla.components.feature.search.SearchUseCases
 import mozilla.components.feature.session.SessionUseCases
-import mozilla.components.feature.toolbar.SearchUseCase
 import mozilla.components.support.ktx.android.graphics.drawable.toBitmap
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.mvi.UIView
 
 class AwesomeBarUIView(
-    useNewTab: Boolean,
-    isPrivate: Boolean,
     container: ViewGroup,
     actionEmitter: Observer<AwesomeBarAction>,
     changesObservable: Observable<AwesomeBarChange>
@@ -35,10 +31,22 @@ class AwesomeBarUIView(
         .findViewById(R.id.awesomeBar)
 
     init {
+        val loadUrlUseCase = object : SessionUseCases.LoadUrlUseCase {
+            override fun invoke(url: String) {
+                actionEmitter.onNext(AwesomeBarAction.URLTapped(url))
+            }
+        }
+
+        val searchUseCase = object : SearchUseCases.SearchUseCase {
+            override fun invoke(searchTerms: String) {
+                actionEmitter.onNext(AwesomeBarAction.SearchTermsTapped(searchTerms))
+            }
+        }
+
         with(container.context) {
             view.addProviders(ClipboardSuggestionProvider(
                 this,
-                getSessionUseCase(this, isPrivate, useNewTab),
+                loadUrlUseCase,
                 getDrawable(R.drawable.ic_link).toBitmap(),
                 getString(R.string.awesomebar_clipboard_title)
                 )
@@ -51,45 +59,18 @@ class AwesomeBarUIView(
                 ),
                 HistoryStorageSuggestionProvider(
                     components.core.historyStorage,
-                    getSessionUseCase(this, isPrivate, useNewTab)),
+                    loadUrlUseCase
+                ),
                 SearchSuggestionProvider(
                     components.search.searchEngineManager.getDefaultSearchEngine(this),
-                    getSearchUseCase(this, isPrivate, useNewTab),
+                    searchUseCase,
                     components.core.client,
                     SearchSuggestionProvider.Mode.MULTIPLE_SUGGESTIONS
                 )
             )
         }
-
-        view.setOnStopListener { actionEmitter.onNext(AwesomeBarAction.ItemSelected) }
     }
 
-    private fun getSearchUseCase(
-        context: Context,
-        isPrivate: Boolean,
-        useNewTab: Boolean
-    ): SearchUseCases.SearchUseCase {
-        if (!useNewTab) {
-            return context.components.useCases.searchUseCases.defaultSearch
-        }
-
-        return when (isPrivate) {
-            true -> context.components.useCases.searchUseCases.newPrivateTabSearch
-            false -> context.components.useCases.searchUseCases.newTabSearch
-        }
-    }
-
-    private fun getSessionUseCase(context: Context, isPrivate: Boolean, useNewTab: Boolean):
-            SessionUseCases.LoadUrlUseCase {
-        if (!useNewTab) {
-            return context.components.useCases.sessionUseCases.loadUrl
-        }
-
-        return when (isPrivate) {
-            true -> context.components.useCases.tabsUseCases.addPrivateTab
-            false -> context.components.useCases.tabsUseCases.addTab
-        }
-    }
     override fun updateView() = Consumer<AwesomeBarState> {
         view.onInputChanged(it.query)
     }
