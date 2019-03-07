7039ae6d36f60edfc0328a068abbfc6a1147b910
Jeff Boek <jeff@jeffboek.com>
2019-03-06 23:19:31 -0800

    Fixes #891 - Adds the leanplum sdk

---
+++ app/src/main/java/org/mozilla/fenix/LeanplumHelper.kt
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix
+
+import com.leanplum.Leanplum
+import com.leanplum.LeanplumActivityHelper
+import com.leanplum.annotations.Parser
+
+object LeanplumHelper {
+    fun setupLeanplumIfNeeded(application: FenixApplication) {
+        Leanplum.setApplicationContext(application)
+        Parser.parseVariables(application)
+        LeanplumActivityHelper.enableLifecycleCallbacks(application)
+        Leanplum.setAppIdForProductionMode(BuildConfig.LEANPLUM_ID, BuildConfig.LEANPLUM_TOKEN)
+        Leanplum.start(application)
+    }
+}
