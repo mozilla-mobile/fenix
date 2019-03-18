214cf41aab98c5540e4a31b440d0a71be76639e4
Sawyer Blatz <sdblatz@gmail.com>
2019-03-18 13:48:30 -0700

    Adds build settings back

--- app/src/main/java/org/mozilla/fenix/components/Analytics.kt
+++ app/src/main/java/org/mozilla/fenix/components/Analytics.kt
@@ -27,11 +27,10 @@ import org.mozilla.geckoview.BuildConfig.MOZ_APP_VERSION
 class Analytics(
     private val context: Context
 ) {
-
     val crashReporter: CrashReporter by lazy {
         val sentryService = SentryService(
             context,
-            SENTRY_TOKEN,
+            BuildConfig.SENTRY_TOKEN,
             tags = mapOf("geckoview" to "$MOZ_APP_VERSION-$MOZ_APP_BUILDID"),
             sendEventForNativeCrashes = true
         )
@@ -69,9 +68,5 @@ class Analytics(
             ),
             isTelemetryEnabled = { Settings.getInstance(context).isTelemetryEnabled }
         )
-    }
-
-    companion object {
-        val SENTRY_TOKEN = "https://718d381780b54062a571b990e1cecd07@sentry.prod.mozaws.net/368"
     }
 }
