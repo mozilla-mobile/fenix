f9c791c93b64c71328f425e8af192e95f37e70db
Johan Lorenzo <jlorenzo@mozilla.com>
2019-04-02 13:56:45 +0200

    Fix trust level in decision task

--- automation/taskcluster/decision_task.py
+++ automation/taskcluster/decision_task.py
@@ -37,6 +37,7 @@ BUILDER = TaskBuilder(
     scheduler_id=os.environ.get('SCHEDULER_ID', 'taskcluster-github'),
     tasks_priority=os.environ.get('TASKS_PRIORITY'),
     date_string=os.environ.get('BUILD_DATE'),
+    trust_level=os.environ.get('TRUST_LEVEL'),
 )
 
 
