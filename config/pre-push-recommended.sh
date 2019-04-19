052ab2383f849c2f28b093eb5b15449bd244b1fd
ekager <emilykager@gmail.com>
2019-04-19 12:11:43 -0700

    No issue: Add instructions to pre-push hook

--- config/pre-push-recommended.sh
+++ config/pre-push-recommended.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# We recommend you run this as a pre-push hook: to reduce
+# review turn-around time, we want all pushes to run tasks
+# locally. Using this hook will guarantee your hook gets
+# updated as the repository changes.
+#
+# This hook tries to run as much as possible without taking
+# too long.
+#
+# You can use it by running this command from the project root:
+# `ln -s ../../tools/pre-push-recommended.sh .git/hooks/pre-push`
+
+# Descriptions for each gradle task below can be found in the
+# output of `./gradlew tasks`.
+
 ./gradlew -q \
         ktlint \
         detekt \
