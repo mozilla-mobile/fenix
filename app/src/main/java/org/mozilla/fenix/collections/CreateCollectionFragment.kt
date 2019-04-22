ab4cf786d73b7ec817bbdfb3044cb00f3189fbec
Jeff Boek <jeff@jeffboek.com>
2019-04-22 15:07:03 -0700

    For #1843 - Scaffolds MVI component for Collection Creation

--- app/src/main/java/org/mozilla/fenix/collections/CreateCollectionFragment.kt
+++ app/src/main/java/org/mozilla/fenix/collections/CreateCollectionFragment.kt
@@ -1,23 +1,33 @@
 package org.mozilla.fenix.collections
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import kotlinx.android.synthetic.main.fragment_create_collection.view.*
 import org.mozilla.fenix.R
+import org.mozilla.fenix.mvi.ActionBusFactory
 
 class CreateCollectionFragment : Fragment() {
+
+    private lateinit var collectionCreationComponent: CollectionCreationComponent
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_collection, container, false)
+        val view = inflater.inflate(R.layout.fragment_create_collection, container, false)
+
+        collectionCreationComponent = CollectionCreationComponent(
+            view.create_collection_wrapper,
+            ActionBusFactory.get(this)
+        )
+
+        return view
     }
-
-
 }
