f1050eaff501bc6834f2ee1d4c1d78d3ade31cf8
Colin Lee <mncolinlee@gmail.com>
2019-04-29 16:54:48 -0500

    For #1951: Bookmarks library pane displays everything from root down (#2135)

--- app/src/main/java/org/mozilla/fenix/library/bookmarks/BookmarkFragment.kt
+++ app/src/main/java/org/mozilla/fenix/library/bookmarks/BookmarkFragment.kt
@@ -89,7 +89,7 @@ class BookmarkFragment : Fragment(), CoroutineScope, BackHandler, AccountObserve
         checkIfSignedIn()
 
         navigation.addOnDestinationChangedListener(onDestinationChangedListener)
-        val currentGuid = BookmarkFragmentArgs.fromBundle(arguments!!).currentRoot.ifEmpty { BookmarkRoot.Root.id }
+        val currentGuid = BookmarkFragmentArgs.fromBundle(arguments!!).currentRoot.ifEmpty { BookmarkRoot.Mobile.id }
 
         launch(IO) {
             currentRoot = requireComponents.core.bookmarksStorage.getTree(currentGuid) as BookmarkNode
