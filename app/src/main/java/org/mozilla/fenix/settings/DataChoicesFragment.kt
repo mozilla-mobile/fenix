0a0594c1669201c1faf9bd7fdfc5f96f9a337dd4
Colin Lee <mncolinlee@gmail.com>
2019-04-28 21:59:53 -0700

    For #1932: Crash on telemetry setting change when unattached

--- app/src/main/java/org/mozilla/fenix/settings/DataChoicesFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/DataChoicesFragment.kt
@@ -4,32 +4,45 @@
 
 package org.mozilla.fenix.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import org.mozilla.fenix.R
-import org.mozilla.fenix.ext.requireComponents
+import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.utils.Settings
 
 class DataChoicesFragment : PreferenceFragmentCompat() {
+
+    private val preferenceChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            when (key) {
+                getString(R.string.pref_key_telemetry) -> {
+                    if (sharedPreferences.getBoolean(key, Settings.getInstance(requireContext()).isTelemetryEnabled)) {
+                        context?.components?.analytics?.metrics?.start()
+                    } else {
+                        context?.components?.analytics?.metrics?.stop()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as AppCompatActivity).title = getString(R.string.preferences_data_choices)
         (activity as AppCompatActivity).supportActionBar?.show()
 
-        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener { sharedPreferences, key ->
-            when (key) {
-                getString(R.string.pref_key_telemetry) -> {
-                    if (sharedPreferences.getBoolean(key, Settings.getInstance(requireContext()).isTelemetryEnabled)) {
-                        requireComponents.analytics.metrics.start()
-                    } else {
-                        requireComponents.analytics.metrics.stop()
-                    }
-                }
-            }
+        context?.let {
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(preferenceChangeListener)
         }
+    }
+
+    override fun onDestroy() {
+        context?.let {
+            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(preferenceChangeListener)
+        }
+        super.onDestroy()
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
