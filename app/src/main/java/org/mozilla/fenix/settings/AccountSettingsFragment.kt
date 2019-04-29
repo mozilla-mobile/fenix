1cb5d9694a44bb3fd3bece894ecfd7325db144f8
Lina Cambridge <lina@yakshaving.ninja>
2019-04-29 14:43:30 -0700

    Send Glean pings for sync telemetry.

--- app/src/main/java/org/mozilla/fenix/settings/AccountSettingsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/AccountSettingsFragment.kt
@@ -7,6 +7,7 @@ package org.mozilla.fenix.settings
 import android.content.Context
 import android.os.Bundle
 import android.text.format.DateUtils
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.preference.Preference
@@ -15,19 +16,30 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import mozilla.components.concept.sync.StoreStats
+import mozilla.components.concept.sync.SyncResult
+import mozilla.components.concept.sync.StoreSyncStatus
+import mozilla.components.concept.sync.SyncStatus
 import mozilla.components.concept.sync.SyncStatusObserver
 import mozilla.components.feature.sync.getLastSynced
+import mozilla.components.service.glean.Glean
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.getPreferenceKey
 import org.mozilla.fenix.ext.requireComponents
 import java.lang.Exception
 import kotlin.coroutines.CoroutineContext
+import org.mozilla.fenix.GleanMetrics.BookmarksSync
+import org.mozilla.fenix.GleanMetrics.HistorySync
+import org.mozilla.fenix.GleanMetrics.Pings
+import java.util.Date
 
 class AccountSettingsFragment : PreferenceFragmentCompat(), CoroutineScope {
     private lateinit var job: Job
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
+
+    companion object {
+        private const val LOGTAG = "AccountSettingsFragment"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -97,10 +109,8 @@ class AccountSettingsFragment : PreferenceFragmentCompat(), CoroutineScope {
             }
         }
 
-        override fun onStoreSynced(stats: StoreStats) {}
-
         // Sync stopped successfully.
-        override fun onIdle() {
+        override fun onIdle(result: SyncResult) {
             CoroutineScope(Dispatchers.Main).launch {
                 val pref = findPreference<Preference>(context!!.getPreferenceKey(R.string.pref_key_sync_now))
                 pref?.let {
@@ -108,11 +118,73 @@ class AccountSettingsFragment : PreferenceFragmentCompat(), CoroutineScope {
                     pref.isEnabled = true
                     updateLastSyncedTimePref(context!!, pref, failed = false)
                 }
+
+                Log.i(LOGTAG, "Sync finished; recording telemetry")
+                for ((_, status) in result) {
+                    status.stats?.let {
+                        it.syncs.forEach {
+                            it.engines.forEach {
+                                when (it.name) {
+                                    "history" -> {
+                                        Log.i(LOGTAG, "Got history sync")
+                                        HistorySync.apply {
+                                            startedAt.set(Date(it.at.toLong()))
+                                            // Glean doesn't support recording arbitrary durations in
+                                            // timespans, so we record absolute start and end times instead.
+                                            finishedAt.set(Date((it.at + it.took).toLong()))
+                                            incoming["applied"].set(it.incoming?.applied ?: 0)
+                                            val failedToApply = it.incoming?.let {
+                                                it.failed + it.newFailed
+                                            } ?: 0;
+                                            incoming["failed_to_apply"].set(failedToApply)
+                                            incoming["reconciled"].set(it.incoming?.reconciled ?: 0)
+                                            val (uploaded, failedToUpload) = it.outgoing.fold(Pair(0, 0)) { totals, batch ->
+                                                val (uploaded, failedToUpload) = totals
+                                                Pair(uploaded + batch.sent, failedToUpload + batch.failed)
+                                            }
+                                            outgoing["uploaded"].set(uploaded)
+                                            outgoing["failed_to_upload"].set(failedToUpload)
+                                            outgoingBatches.set(it.outgoing.size)
+                                            // TODO: We don't currently record the failure reason, because an
+                                            // error while syncing causes us to discard all telemetry.
+                                        }
+                                        Log.i(LOGTAG, "Sending history ping")
+                                        Pings.historySync.send()
+                                    }
+                                    "bookmarks" -> {
+                                        BookmarksSync.apply {
+                                            startedAt.set(Date(it.at.toLong()))
+                                            finishedAt.set(Date((it.at + it.took).toLong()))
+                                            incoming["applied"].set(it.incoming?.applied ?: 0)
+                                            val failedToApply = it.incoming?.let {
+                                                it.failed + it.newFailed
+                                            } ?: 0;
+                                            incoming["failed_to_apply"].set(failedToApply)
+                                            incoming["reconciled"].set(it.incoming?.reconciled ?: 0)
+                                            val (uploaded, failedToUpload) = it.outgoing.fold(Pair(0, 0)) { totals, batch ->
+                                                val (uploaded, failedToUpload) = totals
+                                                Pair(uploaded + batch.sent, failedToUpload + batch.failed)
+                                            }
+                                            outgoing["uploaded"].set(uploaded)
+                                            outgoing["failed_to_upload"].set(failedToUpload)
+                                            outgoingBatches.set(it.outgoing.size)
+                                        }
+                                        Log.i(LOGTAG, "Sending bookmarks ping")
+                                        Pings.bookmarksSync.send()
+                                    }
+                                    "logins" -> {
+                                        // TODO: Collect stats for logins.
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
 
         // Sync stopped after encountering a problem.
-        override fun onError(error: Exception?) {
+        override fun onError(result: SyncResult) {
             CoroutineScope(Dispatchers.Main).launch {
                 val pref = findPreference<Preference>(context!!.getPreferenceKey(R.string.pref_key_sync_now))
                 pref?.let {
