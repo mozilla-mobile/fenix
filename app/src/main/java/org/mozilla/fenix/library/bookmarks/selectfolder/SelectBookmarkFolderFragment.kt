334868c283c3d0ee62a83ba2d93341fad9e501ee
Sawyer Blatz <sdblatz@gmail.com>
2019-05-15 10:51:04 -0700

    For #2224: Removes select bookmark and fixes crash

--- app/src/main/java/org/mozilla/fenix/library/bookmarks/selectfolder/SelectBookmarkFolderFragment.kt
+++ app/src/main/java/org/mozilla/fenix/library/bookmarks/selectfolder/SelectBookmarkFolderFragment.kt
@@ -73,7 +73,7 @@ class SelectBookmarkFolderFragment : Fragment(), CoroutineScope, AccountObserver
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_select_bookmark_folder, container, false)
         signInComponent = SignInComponent(
-            view.bookmark_layout,
+            view.select_bookmark_layout,
             ActionBusFactory.get(this),
             FenixViewModelProvider.create(
                 this,
