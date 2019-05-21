692724be2f6d422037d8b04ae9d08a8851899970
Jeff Boek <jeff@jeffboek.com>
2019-05-20 19:04:16 -0700

    For #2395 - Navigates to turn on sync on button press

--- app/src/main/java/org/mozilla/fenix/home/sessioncontrol/viewholders/onboarding/OnboardingFirefoxAccountViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/home/sessioncontrol/viewholders/onboarding/OnboardingFirefoxAccountViewHolder.kt
@@ -5,15 +5,22 @@
 package org.mozilla.fenix.home.sessioncontrol.viewholders.onboarding
 
 import android.view.View
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.onboarding_firefox_account.view.*
 import org.mozilla.fenix.R
+import org.mozilla.fenix.home.HomeFragmentDirections
 
 class OnboardingFirefoxAccountViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     init {
         val appName = view.context.getString(R.string.app_name)
         view.header_text.text = view.context.getString(R.string.onboarding_firefox_account_header, appName)
+
+        view.turn_on_sync_button.setOnClickListener {
+            val directions = HomeFragmentDirections.actionHomeFragmentToTurnOnSyncFragment()
+            Navigation.findNavController(view).navigate(directions)
+        }
     }
 
     companion object {
