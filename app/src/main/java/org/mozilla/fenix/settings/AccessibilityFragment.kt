833290799ec029f75379ab6d29c090c16a9bc2e4
Emily Kager <ekager@mozilla.com>
2019-05-23 15:10:35 -0700

    For #1286 - Add Automatic Font Sizing Preference

--- app/src/main/java/org/mozilla/fenix/settings/AccessibilityFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/AccessibilityFragment.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.requireComponents
 import org.mozilla.fenix.utils.Settings
@@ -25,12 +26,27 @@ class AccessibilityFragment : PreferenceFragmentCompat() {
                     // Value is mapped from 0->30 in steps of 1 so let's convert to float in range 0.5->2.0
                     val newTextScale = ((newValue as Int * STEP_SIZE) + MIN_SCALE_VALUE).toFloat() / PERCENT_TO_DECIMAL
                     Settings.getInstance(context!!).setFontSizeFactor(newTextScale)
-                    requireComponents.core.engine.settings.automaticFontSizeAdjustment = (newTextScale == 1f)
                     requireComponents.core.engine.settings.fontSizeFactor = newTextScale
                     requireComponents.useCases.sessionUseCases.reload.invoke()
                 }
                 true
             }
+
+        textSizePreference?.isVisible = !Settings.getInstance(context!!).shouldUseAutoSize
+
+        val useAutoSizePreference =
+            findPreference<SwitchPreference>(getString(R.string.pref_key_accessibility_auto_size))
+        useAutoSizePreference?.setOnPreferenceChangeListener { _, newValue ->
+            Settings.getInstance(context!!).setAutoSize(newValue as Boolean)
+            requireComponents.core.engine.settings.automaticFontSizeAdjustment = newValue
+            if (!newValue) {
+                requireComponents.core.engine.settings.fontInflationEnabled = true
+                requireComponents.core.engine.settings.fontSizeFactor = Settings.getInstance(context!!).fontSizeFactor
+            }
+            textSizePreference?.isVisible = !newValue
+            requireComponents.useCases.sessionUseCases.reload.invoke()
+            true
+        }
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
