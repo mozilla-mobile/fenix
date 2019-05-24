defad72adb2e47fb57c8aebb8e58a44e941c068f
Jeff Boek <jeff@jeffboek.com>
2019-05-24 11:35:51 -0700

    For #225 - Adds list items for delete browsing data

---
+++ app/src/main/java/org/mozilla/fenix/settings/DeleteBrowsingDataItem.kt
@@ -0,0 +1,56 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.delete_browsing_data_item.view.*
+import org.mozilla.fenix.R
+
+class DeleteBrowsingDataItem @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    init {
+        LayoutInflater.from(context).inflate(R.layout.delete_browsing_data_item, this, true)
+
+        setOnClickListener {
+            checkbox.isChecked = !checkbox.isChecked
+        }
+
+        attrs.let {
+            context.theme.obtainStyledAttributes(
+                it,
+                R.styleable.LibraryListItem,
+                0, 0
+            ).apply {
+                try {
+                    val iconId = getResourceId(
+                        R.styleable.DeleteBrowsingDataItem_deleteBrowsingDataItemIcon,
+                        R.drawable.library_icon_reading_list_circle_background
+                    )
+                    val titleId = getResourceId(
+                        R.styleable.DeleteBrowsingDataItem_deleteBrowsingDataItemTitle,
+                        R.string.browser_menu_your_library
+                    )
+                    val subtitleId = getResourceId(
+                        R.styleable.DeleteBrowsingDataItem_deleteBrowsingDataItemSubtitle,
+                        R.string.browser_menu_your_library
+                    )
+
+
+                    icon.background = resources.getDrawable(iconId, context.theme)
+                    title.text = resources.getString(titleId)
+                    subtitle.text = resources.getString(subtitleId)
+                } finally {
+                    recycle()
+                }
+            }
+        }
+    }
+}
