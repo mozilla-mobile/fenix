e7e644f07ee13d0ed988bb443ed1b3b6e5abd3e8
Jeff Boek <jeff@jeffboek.com>
2019-06-04 16:45:31 -0700

    For #2395 - Fixes test suite

--- app/src/test/java/org/mozilla/fenix/components/TestComponents.kt
+++ app/src/test/java/org/mozilla/fenix/components/TestComponents.kt
@@ -7,7 +7,7 @@ class TestComponents(private val context: Context) : Components(context) {
     override val backgroundServices by lazy {
         mockk<BackgroundServices>(relaxed = true)
     }
-    override val services by lazy { Services(backgroundServices.accountManager, useCases.tabsUseCases) }
+    override val services by lazy { Services(backgroundServices.accountManager) }
     override val core by lazy { TestCore(context) }
     override val search by lazy { Search(context) }
     override val useCases by lazy { UseCases(context, core.sessionManager, core.engine.settings, search.searchEngineManager) }
