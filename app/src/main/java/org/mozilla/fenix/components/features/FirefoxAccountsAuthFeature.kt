27fdd9605bf76c64d50eb036e0e77d32f6b3d38f
Jeff Boek <jeff@jeffboek.com>
2019-06-05 09:52:19 -0700

    For #2395 - Adds an early return statement and documents why we copy a feature

--- app/src/main/java/org/mozilla/fenix/components/features/FirefoxAccountsAuthFeature.kt
+++ app/src/main/java/org/mozilla/fenix/components/features/FirefoxAccountsAuthFeature.kt
@@ -11,6 +11,12 @@ import mozilla.components.service.fxa.manager.FxaAccountManager
 import org.mozilla.fenix.settings.SupportUtils
 import kotlin.coroutines.CoroutineContext
 
+// The FirefoxAccountsAuthFeature provided by android components only worked with
+// the tabs use case. We want to use our custom tab feature to improve the UX for both
+// signing in through settings and onboarding.
+
+// We're temporarily copying and creating our own:
+// https://github.com/mozilla-mobile/android-components/issues/3272
 class FirefoxAccountsAuthFeature(
     private val accountManager: FxaAccountManager,
     private val redirectUrl: String,
@@ -32,7 +38,7 @@ class FirefoxAccountsAuthFeature(
         CoroutineScope(coroutineContext).launch {
             // FIXME return a fallback URL provided by Config...
             // https://github.com/mozilla-mobile/android-components/issues/2496
-            val authUrl = beginAuthentication() ?: "https://accounts.firefox.com/signin"
+            val authUrl = beginAuthentication() ?: FALLBACK_URL
 
             // TODO
             // We may fail to obtain an authentication URL, for example due to transient network errors.
@@ -63,5 +69,9 @@ class FirefoxAccountsAuthFeature(
 
             return null
         }
+    }
+
+    companion object {
+        private const val FALLBACK_URL = "https://accounts.firefox.com/signin"
     }
 }
