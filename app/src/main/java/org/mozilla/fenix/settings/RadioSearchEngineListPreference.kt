a86ec60f8f18ab6cba98d2495ced3620046b11df
Jeff Boek <jeff@jeffboek.com>
2019-06-10 12:48:53 -0700

    For #3278 - Fixes search engine selector layout

--- app/src/main/java/org/mozilla/fenix/settings/RadioSearchEngineListPreference.kt
+++ app/src/main/java/org/mozilla/fenix/settings/RadioSearchEngineListPreference.kt
@@ -7,15 +7,13 @@ package org.mozilla.fenix.settings
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.CompoundButton
-import android.widget.RadioGroup
 import androidx.preference.PreferenceViewHolder
+import mozilla.components.browser.search.SearchEngine
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.utils.Settings
 
-class RadioSearchEngineListPreference : SearchEngineListPreference,
-    RadioGroup.OnCheckedChangeListener {
-
+class RadioSearchEngineListPreference : SearchEngineListPreference {
     override val itemResId: Int
         get() = R.layout.search_engine_radio_button
 
@@ -29,24 +27,14 @@ class RadioSearchEngineListPreference : SearchEngineListPreference,
 
     override fun onBindViewHolder(holder: PreferenceViewHolder?) {
         super.onBindViewHolder(holder)
-        searchEngineGroup!!.setOnCheckedChangeListener(this)
     }
 
     override fun updateDefaultItem(defaultButton: CompoundButton) {
         defaultButton.isChecked = true
     }
 
-    override fun onCheckedChanged(group: RadioGroup, checkedId: Int) {
-        /* onCheckedChanged is called intermittently before the search engine table is full, so we
-           must check these conditions to prevent crashes and inconsistent states. */
-        if (group.childCount != searchEngines.count() || group.getChildAt(checkedId) == null ||
-            !group.getChildAt(checkedId).isPressed
-        ) {
-            return
-        }
-
-        val newDefaultEngine = searchEngines[checkedId]
-        context.components.search.searchEngineManager.defaultSearchEngine = newDefaultEngine
-        Settings.getInstance(group.context).setDefaultSearchEngineByName(newDefaultEngine.name)
+    override fun onSearchEngineSelected(searchEngine: SearchEngine) {
+        context.components.search.searchEngineManager.defaultSearchEngine = searchEngine
+        Settings.getInstance(context).setDefaultSearchEngineByName(searchEngine.name)
     }
 }
