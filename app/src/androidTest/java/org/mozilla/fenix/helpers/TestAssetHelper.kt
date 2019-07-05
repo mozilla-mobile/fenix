e7d0bfe581ba66311ab40dc29666b0167a0c4ed2
Richard Pappalardo <rpappalax@gmail.com>
2019-07-05 10:38:09 -0700

    Update HomeScreenTest (#3882)

---
+++ app/src/androidTest/java/org/mozilla/fenix/helpers/TestAssetHelper.kt
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.helpers
+
+import android.net.Uri
+
+import okhttp3.mockwebserver.MockWebServer
+import org.mozilla.fenix.helpers.ext.toUri
+import java.util.concurrent.TimeUnit
+
+/**
+ * Helper for hosting web pages locally for testing purposes.
+ */
+object TestAssetHelper {
+    @Suppress("MagicNumber")
+    val waitingTime: Long = TimeUnit.SECONDS.toMillis(15)
+    val waitingTimeShort: Long = TimeUnit.SECONDS.toMillis(1)
+    data class TestAsset(val url: Uri, val content: String)
+
+    /**
+     * Hosts 3 simple websites, found at androidTest/assets/pages/generic[1|2|3].html
+     * Returns a list of TestAsset, which can be used to navigate to each and
+     * assert that the correct information is being displayed.
+     *
+     * Content for these pages all follow the same pattern. See [generic1.html] for
+     * content implementation details.
+     */
+    fun getGenericAssets(server: MockWebServer): List<TestAsset> {
+        @Suppress("MagicNumber")
+        return (1..3).map {
+            TestAsset(
+                server.url("pages/generic$it.html").toString().toUri()!!,
+                "Page content: $it"
+            )
+        }
+    }
+
+    fun getGenericAsset(server: MockWebServer, pageNum: Int): TestAsset {
+        val url = server.url("pages/generic$pageNum.html").toString().toUri()!!
+        val content = "Page content: $pageNum"
+
+        return TestAsset(url, content)
+    }
+
+    fun getLoremIpsumAsset(server: MockWebServer): TestAsset {
+        val url = server.url("pages/lorem-ipsum.html").toString().toUri()!!
+        val content = "Page content: lorem ipsum"
+        return TestAsset(url, content)
+    }
+
+    fun getRefreshAsset(server: MockWebServer): TestAsset {
+        val url = server.url("pages/refresh.html").toString().toUri()!!
+        val content = "Page content: refresh"
+
+        return TestAsset(url, content)
+    }
+
+    fun getUUIDPage(server: MockWebServer): TestAsset {
+        val url = server.url("pages/basic_nav_uuid.html").toString().toUri()!!
+        val content = "Page content: basic_nav_uuid"
+
+        return TestAsset(url, content)
+    }
+}
--- app/src/androidTest/java/org/mozilla/fenix/ui/HomeScreenTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/HomeScreenTest.kt
@@ -4,9 +4,16 @@
 
 package org.mozilla.fenix.ui
 
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mozilla.fenix.helpers.AndroidAssetDispatcher
 import org.mozilla.fenix.helpers.HomeActivityTestRule
+import org.mozilla.fenix.helpers.TestAssetHelper
 import org.mozilla.fenix.ui.robots.homeScreen
 
 /**
@@ -16,15 +23,50 @@ import org.mozilla.fenix.ui.robots.homeScreen
 
 class HomeScreenTest {
     /* ktlint-disable no-blank-line-before-rbrace */ // This imposes unreadable grouping.
+
+    private val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    private lateinit var mockWebServer: MockWebServer
+
     @get:Rule
     val activityTestRule = HomeActivityTestRule()
+
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer().apply {
+            setDispatcher(AndroidAssetDispatcher())
+            start()
+        }
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
     @Test
     fun homeScreenItemsTest() {
+
+        val defaultWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
+
+        // temp work-around until FIRST_RUN pref = false
+        // is added: https://github.com/mozilla-mobile/fenix/issues/3777
+        homeScreen {
+            verifyNavigationToolbar()
+        }.completeFirstRun(defaultWebPage.url) {
+        }
+
         homeScreen {
             verifyHomeScreen()
             verifyHomePrivateBrowsingButton()
             verifyHomeMenu()
             verifyHomeWordmark()
+            verifyOpenTabsHeader()
+            verifyAddTabButton()
+            verifyNoTabsOpenedHeader()
+            verifyNoTabsOpenedText()
+            verifyCollectionsHeader()
+            verifyNoCollectionsHeader()
+            verifyNoCollectionsText()
             verifyHomeToolbar()
             verifyHomeComponent()
         }
