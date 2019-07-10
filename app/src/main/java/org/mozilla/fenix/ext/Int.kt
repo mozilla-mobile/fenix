52d4c4391610aee5936fd7c259a9588fd78d90cd
Tiger Oakes <toakes@mozilla.com>
2019-07-10 11:19:07 -0400

    Merge some library UIView code for toolbar

--- app/src/main/java/org/mozilla/fenix/ext/Int.kt
+++ app/src/main/java/org/mozilla/fenix/ext/Int.kt
@@ -5,8 +5,19 @@
 package org.mozilla.fenix.ext
 
 import android.content.Context
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import org.mozilla.fenix.ThemeManager
 
+/**
+ * Returns the color resource corresponding to the attribute.
+ */
+@ColorRes
 fun Int.getColorIntFromAttr(context: Context): Int = ThemeManager.resolveAttribute(this, context)
+
+/**
+ * Returns the color int corresponding to the attribute.
+ */
+@ColorInt
 fun Int.getColorFromAttr(context: Context): Int = ContextCompat.getColor(context, this.getColorIntFromAttr(context))
