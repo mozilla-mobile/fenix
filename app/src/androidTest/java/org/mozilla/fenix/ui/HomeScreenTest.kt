6b639b1a322ff56606cbe9124134e0b7a448e128
Sawyer Blatz <sdblatz@gmail.com>
2019-07-12 16:19:08 -0700

    For #4032: Fixes broken homescreen UI tests (#4011)

--- app/src/androidTest/java/org/mozilla/fenix/ui/HomeScreenTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/HomeScreenTest.kt
@@ -5,16 +5,11 @@
 package org.mozilla.fenix.ui
 
 import androidx.test.uiautomator.UiDevice
-import okhttp3.mockwebserver.MockWebServer
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mozilla.fenix.helpers.AndroidAssetDispatcher
 import org.mozilla.fenix.helpers.HomeActivityTestRule
 
 import androidx.test.platform.app.InstrumentationRegistry
-import org.mozilla.fenix.helpers.TestAssetHelper
 import org.mozilla.fenix.ui.robots.homeScreen
 
 /**
@@ -26,38 +21,17 @@ class HomeScreenTest {
     /* ktlint-disable no-blank-line-before-rbrace */ // This imposes unreadable grouping.
 
     private val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-    private lateinit var mockWebServer: MockWebServer
 
     @get:Rule
     val activityTestRule = HomeActivityTestRule()
 
-    @Before
-    fun setUp() {
-        mockWebServer = MockWebServer().apply {
-            setDispatcher(AndroidAssetDispatcher())
-            start()
-        }
-    }
-
-    @After
-    fun tearDown() {
-        mockWebServer.shutdown()
-    }
-
     @Test
     fun homeScreenItemsTest() {
-
-        val defaultWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
-
-        // temp work-around until FIRST_RUN pref = false
-        // is added: https://github.com/mozilla-mobile/fenix/issues/3777
-        homeScreen {
-            verifyNavigationToolbar()
-        }.completeFirstRun(defaultWebPage.url) {
-        }
+        homeScreen { }.dismissOnboarding()
 
         homeScreen {
             verifyHomeScreen()
+            verifyNavigationToolbar()
             verifyHomePrivateBrowsingButton()
             verifyHomeMenu()
             verifyHomeWordmark()
@@ -75,19 +49,20 @@ class HomeScreenTest {
 
     @Test
     fun firstRunHomeScreenItemsTest() {
-
         homeScreen {
             verifyHomeScreen()
+            verifyNavigationToolbar()
             verifyHomePrivateBrowsingButton()
             verifyHomeMenu()
             verifyHomeWordmark()
 
             verifyWelcomeHeader()
 
-            verifyAlreadyHaveAnAccountHeader()
             verifyGetTheMostHeader()
             verifyAccountsSignInButton()
             verifyGetToKnowHeader()
+
+            swipeUpToDismissFirstRun()
 
             verifyChooseThemeHeader()
             verifyChooseThemeText()
