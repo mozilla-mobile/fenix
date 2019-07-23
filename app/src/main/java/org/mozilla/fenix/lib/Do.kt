5c7ead385449d0eeff06070b47507a4c023f515e
Tiger Oakes <toakes@mozilla.com>
2019-07-23 17:15:46 -0400

    Remove redundant super calls

--- app/src/main/java/org/mozilla/fenix/lib/Do.kt
+++ app/src/main/java/org/mozilla/fenix/lib/Do.kt
@@ -5,5 +5,14 @@
 package org.mozilla.fenix.lib
 
 object Do {
+
+    /**
+     * Indicates to the linter that the following when statement should be exhaustive.
+     *
+     * @sample Do exhaustive when (bool) {
+     *     true -> Unit
+     *     false -> Unit
+     * }
+     */
     inline infix fun <reified T> exhaustive(any: T?) = any
 }
