1e08823716510ee2eb78e7ac5fdb4ddced8fa3b8
Isabel Rios <isabelrios@MackBookirios.home>
2019-07-29 11:39:25 +0200

    Fixes #4324 - l10n Add more screenshots tests

--- app/src/androidTest/java/org/mozilla/fenix/ui/screenshots/ThreeDotMenuScreenShotTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/screenshots/ThreeDotMenuScreenShotTest.kt
@@ -1,7 +1,13 @@
 package org.mozilla.fenix.ui.screenshots
 
 import android.os.SystemClock
+import android.widget.ImageView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.rule.ActivityTestRule
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.Until
 
 import org.junit.After
 import org.junit.Rule
@@ -15,19 +21,37 @@ import tools.fastlane.screengrab.Screengrab
 import tools.fastlane.screengrab.locale.LocaleTestRule
 
 import br.com.concretesolutions.kappuccino.actions.ClickActions
+import br.com.concretesolutions.kappuccino.extensions.type
+import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.Matchers
+import org.junit.Before
+import org.mozilla.fenix.helpers.AndroidAssetDispatcher
+import org.mozilla.fenix.helpers.click
 import org.mozilla.fenix.ui.robots.homeScreen
+import org.mozilla.fenix.ui.robots.navigationToolbar
 import org.mozilla.fenix.ui.robots.swipeToBottom
 
 class ThreeDotMenuScreenShotTest : ScreenshotTest() {
+
+    private lateinit var mockWebServer: MockWebServer
     @Rule @JvmField
     val localeTestRule = LocaleTestRule()
 
     @get:Rule
     var mActivityTestRule: ActivityTestRule<HomeActivity> = HomeActivityTestRule()
 
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer().apply {
+            setDispatcher(AndroidAssetDispatcher())
+            start()
+        }
+    }
+
     @After
     fun tearDown() {
         mActivityTestRule.getActivity().finishAndRemoveTask()
+        mockWebServer.shutdown()
     }
 
     @Test
@@ -45,7 +69,7 @@ class ThreeDotMenuScreenShotTest : ScreenshotTest() {
         }.openSettings { }
         Screengrab.screenshot("settings")
 
-        SystemClock.sleep(TestAssetHelper.waitingTime)
+        SystemClock.sleep(TestAssetHelper.waitingTimeShort)
         settingsAccount()
         Screengrab.screenshot("settings-sync")
         device.pressBack()
@@ -100,6 +124,79 @@ class ThreeDotMenuScreenShotTest : ScreenshotTest() {
         historyButton()
         Screengrab.screenshot("library-history")
     }
+
+    @Test
+    fun bookmarksManagementTest() {
+        homeScreen {
+        }.openThreeDotMenu {
+        }.openLibrary { }
+        bookmarksButton()
+        addBookmarkFolderButton()
+        Screengrab.screenshot("add-folder-view")
+
+        confirmAddFolderButton()
+        Screengrab.screenshot("error-empty-folder-name")
+
+        // Enter folder name and save
+        addFolderName()
+        confirmAddFolderButton()
+
+        // Open folder menu
+        bookmarkFolderMenu()
+        Screengrab.screenshot("folder-menu")
+
+        // Edit folder menu
+        editBookmarkFolder()
+        device.pressBack()
+        Screengrab.screenshot("edit-bookmark-folder-menu")
+
+        // Delete folder
+        bookmarkFolderMenu()
+        deleteBookmarkFolder()
+        Screengrab.screenshot("delete-bookmark-folder-menu")
+    }
+
+    @Test
+    fun tabMenuTest() {
+        val defaultWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
+        navigationToolbar {
+        }.enterURLAndEnterToBrowser(defaultWebPage.url) {
+        }
+        device.wait(Until.findObjects(By.res("quick_action_sheet_handle")), TestAssetHelper.waitingTimeShort)
+        openQuickActionMenu()
+        Screengrab.screenshot("browser-tab-share-bookmark")
+        device.findObject(UiSelector()
+                .instance(2)
+                .className(ImageView::class.java)).click()
+        Screengrab.screenshot("browser-tab-menu")
+
+        device.pressBack()
+        device.findObject(UiSelector()
+                .instance(1)
+                .className(ImageView::class.java)).click()
+        // Home screen with visited tabs
+        device.wait(Until.findObjects(By.text("Save to collection")), TestAssetHelper.waitingTimeShort)
+        openTabsMenu()
+        Screengrab.screenshot("open-tabs-menu")
+        device.pressBack()
+        device.wait(Until.findObjects(By.text("Save to collection")), TestAssetHelper.waitingTimeShort)
+        Screengrab.screenshot("save-collection-button")
+
+        /*
+        // Save tab to Collection -> problem pressing on Enter to get the collection saved
+        saveToCollectionButton()
+        Screengrab.screenshot("save-collection-view")
+
+        // Go back to homescreen after saving one collection
+        mDevice.wait(Until.findObjects(By.text("Save to collection")), TestAssetHelper.waitingTimeShort)
+        Screengrab.screenshot("saved-tab")
+
+        // Open Collection menu
+        collectionsButton()
+        Screengrab.screenshot("saved-collections-menu")
+        pressImeActionButton()
+        */
+    }
 }
 
 fun bookmarksButton() = ClickActions.click { text(R.string.library_bookmarks) }
@@ -112,3 +209,15 @@ fun settingsAccessibility() = ClickActions.click { text(R.string.preferences_acc
 fun settingsTp() = ClickActions.click { text(R.string.preferences_tracking_protection) }
 fun settingsRemoveData() = ClickActions.click { text(R.string.preferences_delete_browsing_data) }
 fun settingsTelemetry() = ClickActions.click { text(R.string.preferences_data_collection) }
+
+fun openTabsMenu() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.tabs_overflow_button))).click()
+fun openQuickActionMenu() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.quick_action_sheet_handle))).click()
+fun saveToCollectionButton() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.save_tab_group_button))).click()
+fun collectionsButton() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.collection_overflow_button))).click()
+fun addBookmarkFolderButton() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.add_bookmark_folder))).click()
+fun confirmAddFolderButton() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.confirm_add_folder_button))).click()
+fun addFolderName() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.bookmark_add_folder_title_edit)))
+        .type("folder")
+fun bookmarkFolderMenu() = Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.bookmark_overflow))).click()
+fun editBookmarkFolder() = ClickActions.click { text(R.string.bookmark_menu_edit_button) }
+fun deleteBookmarkFolder() = ClickActions.click { text(R.string.bookmark_menu_delete_button) }
