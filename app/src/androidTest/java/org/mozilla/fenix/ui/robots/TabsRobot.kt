b9a0a870e5181cfc416149e0c271c5e69761a8dc
Richard Pappalardo <rpappalax@gmail.com>
2019-08-06 14:20:21 -0700

    Stub out tabs tests
    Add more open tabs tests (WIP)
    
    fix: ktlint

---
+++ app/src/androidTest/java/org/mozilla/fenix/ui/robots/TabsRobot.kt
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+@file:Suppress("TooManyFunctions")
+
+package org.mozilla.fenix.ui.robots
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+
+class TabsRobot {
+
+    // Tabs functions here
+
+    class Transition {
+        private val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+        // Transition functions here
+    }
+}
+
+// Locaters here
