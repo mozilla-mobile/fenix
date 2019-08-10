4a14202c4ae9c829b7627affc3d8d99d0144dfab
Sourabh Singh Rawat <dragneelfps@gmail.com>
2019-08-10 06:46:11 +0530

    For #4399: Create LibState Controller for Search

--- app/src/main/java/org/mozilla/fenix/search/SearchInteractor.kt
+++ app/src/main/java/org/mozilla/fenix/search/SearchInteractor.kt
@@ -4,19 +4,8 @@
 
 package org.mozilla.fenix.search
 
-import android.content.Context
-import androidx.navigation.NavController
 import mozilla.components.browser.search.SearchEngine
 import mozilla.components.browser.session.Session
-import mozilla.components.support.ktx.kotlin.isUrl
-import org.mozilla.fenix.BrowserDirection
-import org.mozilla.fenix.HomeActivity
-import org.mozilla.fenix.R
-import org.mozilla.fenix.components.metrics.Event
-import org.mozilla.fenix.ext.components
-import org.mozilla.fenix.ext.metrics
-import org.mozilla.fenix.ext.nav
-import org.mozilla.fenix.ext.searchEngineManager
 import org.mozilla.fenix.search.awesomebar.AwesomeBarInteractor
 import org.mozilla.fenix.search.toolbar.ToolbarInteractor
 
@@ -25,104 +14,42 @@ import org.mozilla.fenix.search.toolbar.ToolbarInteractor
  * Provides implementations for the AwesomeBarView and ToolbarView
  */
 class SearchInteractor(
-    private val context: Context,
-    private val navController: NavController,
-    private val store: SearchStore
+    private val searchController: SearchController
 ) : AwesomeBarInteractor, ToolbarInteractor {
 
-    data class UserTypingCheck(var ranOnTextChanged: Boolean, var userHasTyped: Boolean)
-
-    private val userTypingCheck = UserTypingCheck(false, !store.state.showShortcutEnginePicker)
-
     override fun onUrlCommitted(url: String) {
-        if (url.isNotBlank()) {
-            (context as HomeActivity).openToBrowserAndLoad(
-                searchTermOrURL = url,
-                newTab = store.state.session == null,
-                from = BrowserDirection.FromSearch,
-                engine = store.state.searchEngineSource.searchEngine
-            )
-
-            val event = if (url.isUrl()) {
-                Event.EnteredUrl(false)
-            } else {
-                createSearchEvent(store.state.searchEngineSource.searchEngine, false)
-            }
-
-            context.metrics.track(event)
-        }
+        searchController.handleUrlCommitted(url)
     }
 
     override fun onEditingCanceled() {
-        navController.navigateUp()
+        searchController.handleEditingCancelled()
     }
 
     override fun onTextChanged(text: String) {
-        store.dispatch(SearchAction.UpdateQuery(text))
-
-        if (userTypingCheck.ranOnTextChanged && !userTypingCheck.userHasTyped) {
-            store.dispatch(SearchAction.ShowSearchShortcutEnginePicker(false))
-            turnOnStartedTyping()
-        }
-
-        userTypingCheck.ranOnTextChanged = true
+        searchController.handleTextChanged(text)
     }
 
     override fun onUrlTapped(url: String) {
-        (context as HomeActivity).openToBrowserAndLoad(
-            searchTermOrURL = url,
-            newTab = store.state.session == null,
-            from = BrowserDirection.FromSearch
-        )
-
-        context.metrics.track(Event.EnteredUrl(false))
+        searchController.handleUrlTapped(url)
     }
 
     override fun onSearchTermsTapped(searchTerms: String) {
-        (context as HomeActivity).openToBrowserAndLoad(
-            searchTermOrURL = searchTerms,
-            newTab = store.state.session == null,
-            from = BrowserDirection.FromSearch,
-            engine = store.state.searchEngineSource.searchEngine,
-            forceSearch = true
-        )
-
-        val event = createSearchEvent(store.state.searchEngineSource.searchEngine, true)
-        context.metrics.track(event)
+        searchController.handleSearchTermsTapped(searchTerms)
     }
 
     override fun onSearchShortcutEngineSelected(searchEngine: SearchEngine) {
-        store.dispatch(SearchAction.SearchShortcutEngineSelected(searchEngine))
-        context.metrics.track(Event.SearchShortcutSelected(searchEngine.name))
+        searchController.handleSearchShortcutEngineSelected(searchEngine)
     }
 
     override fun onClickSearchEngineSettings() {
-        val directions = SearchFragmentDirections.actionSearchFragmentToSearchEngineFragment()
-        navController.navigate(directions)
+        searchController.handleClickSearchEngineSettings()
     }
 
     fun turnOnStartedTyping() {
-        userTypingCheck.ranOnTextChanged = true
-        userTypingCheck.userHasTyped = true
+        searchController.handleTurnOnStartedTyping()
     }
 
     override fun onExistingSessionSelected(session: Session) {
-        val directions = SearchFragmentDirections.actionSearchFragmentToBrowserFragment(null)
-        navController.nav(R.id.searchFragment, directions)
-        context.components.core.sessionManager.select(session)
-    }
-
-    private fun createSearchEvent(engine: SearchEngine, isSuggestion: Boolean): Event.PerformedSearch {
-        val isShortcut = engine != context.searchEngineManager.defaultSearchEngine
-
-        val engineSource =
-            if (isShortcut) Event.PerformedSearch.EngineSource.Shortcut(engine)
-            else Event.PerformedSearch.EngineSource.Default(engine)
-
-        val source =
-            if (isSuggestion) Event.PerformedSearch.EventSource.Suggestion(engineSource)
-            else Event.PerformedSearch.EventSource.Action(engineSource)
-
-        return Event.PerformedSearch(source)
+        searchController.handleExistingSessionSelected(session)
     }
 }
