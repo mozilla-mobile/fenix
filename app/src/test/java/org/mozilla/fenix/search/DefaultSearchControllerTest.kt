22349ba6314248eb3b6d8e490f36ac440be7a939
Sourabh Singh Rawat <dragneelfps@gmail.com>
2019-08-15 22:44:46 +0530

    add unit tests for DefaultSearchController

---
+++ app/src/test/java/org/mozilla/fenix/search/DefaultSearchControllerTest.kt
@@ -0,0 +1,144 @@
+/*  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.search
+
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import mozilla.components.browser.search.SearchEngine
+import mozilla.components.browser.session.Session
+import org.junit.Before
+import org.junit.Test
+import org.mozilla.fenix.BrowserDirection
+import org.mozilla.fenix.HomeActivity
+import org.mozilla.fenix.R
+import org.mozilla.fenix.components.metrics.Event
+import org.mozilla.fenix.ext.components
+import org.mozilla.fenix.ext.metrics
+import org.mozilla.fenix.ext.nav
+import org.mozilla.fenix.ext.searchEngineManager
+
+class DefaultSearchControllerTest {
+
+    private val context: HomeActivity = mockk(relaxed = true)
+    private val store: SearchStore = mockk(relaxed = true)
+    private val navController: NavController = mockk(relaxed = true)
+    private val defaultSearchEngine: SearchEngine? = mockk(relaxed = true)
+    private val session: Session? = mockk(relaxed = true)
+    private val searchEngine: SearchEngine = mockk(relaxed = true)
+
+    private lateinit var controller: DefaultSearchController
+
+    @Before
+    fun setUp() {
+        every { store.state.showShortcutEnginePicker } returns false
+        every { context.searchEngineManager.defaultSearchEngine } returns defaultSearchEngine
+        every { store.state.session } returns session
+        every { store.state.searchEngineSource.searchEngine } returns searchEngine
+
+        controller = DefaultSearchController(
+            context = context,
+            store = store,
+            navController = navController
+        )
+    }
+
+    @Test
+    fun handleUrlCommitted() {
+        val url = "https://www.google.com/"
+
+        controller.handleUrlCommitted(url)
+
+        verify { context.openToBrowserAndLoad(
+            searchTermOrURL = url,
+            newTab = session == null,
+            from = BrowserDirection.FromSearch,
+            engine = searchEngine
+        ) }
+        verify { context.metrics.track(Event.EnteredUrl(false)) }
+    }
+
+    @Test
+    fun handleEditingCancelled() {
+        controller.handleEditingCancelled()
+
+        verify { navController.navigateUp() }
+    }
+
+    @Test
+    fun handleTextChanged() {
+        val text = "fenix"
+
+        controller.handleTextChanged(text)
+
+        verify { store.dispatch(SearchAction.UpdateQuery(text)) }
+        verify(inverse = true) {
+            store.dispatch(SearchAction.ShowSearchShortcutEnginePicker(false))
+        }
+    }
+
+    @Test
+    fun handleUrlTapped() {
+        val url = "https://www.google.com/"
+
+        controller.handleUrlTapped(url)
+
+        verify { context.openToBrowserAndLoad(
+            searchTermOrURL = url,
+            newTab = session == null,
+            from = BrowserDirection.FromSearch
+        ) }
+        verify { context.metrics.track(Event.EnteredUrl(false)) }
+    }
+
+    @Test
+    fun handleSearchTermsTapped() {
+        val searchTerms = "fenix"
+
+        controller.handleSearchTermsTapped(searchTerms)
+
+        verify { context.openToBrowserAndLoad(
+            searchTermOrURL = searchTerms,
+            newTab = session == null,
+            from = BrowserDirection.FromSearch,
+            engine = searchEngine,
+            forceSearch = true
+        ) }
+    }
+
+    @Test
+    fun handleSearchShortcutEngineSelected() {
+        val searchEngine: SearchEngine = mockk(relaxed = true)
+
+        every { searchEngine.name } returns "google"
+
+        controller.handleSearchShortcutEngineSelected(searchEngine)
+
+        verify { store.dispatch(SearchAction.SearchShortcutEngineSelected(searchEngine)) }
+        verify { context.metrics.track(Event.SearchShortcutSelected("google")) }
+    }
+
+    @Test
+    fun handleClickSearchEngineSettings() {
+        val directions: NavDirections = SearchFragmentDirections.actionSearchFragmentToSearchEngineFragment()
+
+        controller.handleClickSearchEngineSettings()
+
+        verify { navController.navigate(directions) }
+    }
+
+    @Test
+    fun handleExistingSessionSelected() {
+        val session: Session = mockk(relaxed = true)
+        val directions = SearchFragmentDirections.actionSearchFragmentToBrowserFragment(null)
+
+        controller.handleExistingSessionSelected(session)
+
+        verify { navController.nav(R.id.searchFragment, directions) }
+        verify { context.components.core.sessionManager.select(session) }
+    }
+}
