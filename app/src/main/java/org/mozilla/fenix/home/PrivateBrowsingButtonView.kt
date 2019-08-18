0054e307ba9348f3fb487c8c5be0fb41a851a595
Tiger Oakes <toakes@mozilla.com>
2019-08-18 13:22:55 -0400

    For #4744 - Extract private browsing button

---
+++ app/src/main/java/org/mozilla/fenix/home/PrivateBrowsingButtonView.kt
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.home
+
+import android.view.View
+import androidx.annotation.StringRes
+import org.mozilla.fenix.BrowsingMode
+import org.mozilla.fenix.BrowsingModeManager
+import org.mozilla.fenix.R
+
+/**
+ * Sets up the private browsing toggle button on the [HomeFragment].
+ */
+class PrivateBrowsingButtonView(
+    button: View,
+    private val browsingModeManager: BrowsingModeManager,
+    private val onClick: (BrowsingMode) -> Unit
+) : View.OnClickListener {
+
+    init {
+        button.contentDescription = button.context.getString(getContentDescription(browsingModeManager.mode))
+        button.setOnClickListener(this)
+    }
+
+    /**
+     * Calls [onClick] with the new [BrowsingMode] and updates the [browsingModeManager].
+     */
+    override fun onClick(v: View) {
+        val invertedMode = BrowsingMode.fromBoolean(!browsingModeManager.mode.isPrivate)
+        onClick(invertedMode)
+
+        browsingModeManager.mode = invertedMode
+    }
+
+    companion object {
+
+        /**
+         * Returns the appropriate content description depending on the browsing mode.
+         */
+        @StringRes
+        private fun getContentDescription(mode: BrowsingMode) = when (mode) {
+            BrowsingMode.Normal -> R.string.content_description_private_browsing_button
+            BrowsingMode.Private -> R.string.content_description_disable_private_browsing_button
+        }
+    }
+}
