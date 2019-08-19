210868f5bcd0ba4035b97f6c82c26a11a8191429
Arturo Mejia <arturomejiamarmol@gmail.com>
2019-08-19 10:29:10 -0700

    Use Mozilla Android Components 9.0.0-SNAPSHOT. (#4750)

--- app/src/main/java/org/mozilla/fenix/components/Core.kt
+++ app/src/main/java/org/mozilla/fenix/components/Core.kt
@@ -21,7 +21,9 @@ import mozilla.components.browser.storage.sync.PlacesHistoryStorage
 import mozilla.components.concept.engine.DefaultSettings
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy
-import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy.Companion.SAFE_BROWSING_ALL
+import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy.SafeBrowsingCategory
+import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy.Companion.select
+import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy.TrackingCategory
 import mozilla.components.concept.engine.mediaquery.PreferredColorScheme
 import mozilla.components.concept.fetch.Client
 import mozilla.components.feature.media.MediaFeature
@@ -177,7 +179,10 @@ class Core(private val context: Context) {
             normalMode && privateMode -> trackingProtectionPolicy
             normalMode && !privateMode -> trackingProtectionPolicy.forRegularSessionsOnly()
             !normalMode && privateMode -> trackingProtectionPolicy.forPrivateSessionsOnly()
-            else -> TrackingProtectionPolicy.select(SAFE_BROWSING_ALL)
+            else -> select(
+                trackingCategories = arrayOf(TrackingCategory.NONE),
+                safeBrowsingCategories = arrayOf(SafeBrowsingCategory.RECOMMENDED)
+            )
         }
     }
 
