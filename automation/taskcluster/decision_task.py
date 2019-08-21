53c6d127f8c849cceab5c7e057a32a19943244ec
Mitchell Hentges <mhentges@mozilla.com>
2019-08-20 20:03:18 -0700

    Updates automation to use apk splitting and support different engine

--- automation/taskcluster/decision_task.py
+++ automation/taskcluster/decision_task.py
@@ -15,7 +15,7 @@ import re
 
 import taskcluster
 
-from lib.gradle import get_variants_for_build_type
+from lib.gradle import get_variant
 from lib.tasks import (
     fetch_mozharness_task_id,
     schedule_task_graph,
@@ -25,7 +25,6 @@ from lib.chain_of_trust import (
     populate_chain_of_trust_task_graph,
     populate_chain_of_trust_required_but_unused_files
 )
-from lib.variant import Variant
 
 REPO_URL = os.environ.get('MOBILE_HEAD_REPOSITORY')
 COMMIT = os.environ.get('MOBILE_HEAD_REV')
@@ -61,10 +60,10 @@ def pr():
     signing_tasks = {}
     other_tasks = {}
 
-    for variant in get_variants_for_build_type('debug'):
-        assemble_task_id = taskcluster.slugId()
-        build_tasks[assemble_task_id] = BUILDER.craft_assemble_pr_task(variant)
-        build_tasks[taskcluster.slugId()] = BUILDER.craft_test_pr_task(variant, True)
+    variant = get_variant('debug', 'geckoNightly')
+    assemble_task_id = taskcluster.slugId()
+    build_tasks[assemble_task_id] = BUILDER.craft_assemble_pr_task(variant)
+    build_tasks[taskcluster.slugId()] = BUILDER.craft_test_pr_task(variant)
 
     for craft_function in (
         BUILDER.craft_detekt_task,
@@ -80,12 +79,12 @@ def pr():
 def push():
     all_tasks = pr()
     other_tasks = all_tasks[-1]
-    other_tasks[taskcluster.slugId()] = BUILDER.craft_ui_tests_task() 
+    other_tasks[taskcluster.slugId()] = BUILDER.craft_ui_tests_task()
 
     if SHORT_HEAD_BRANCH == 'master':
         other_tasks[taskcluster.slugId()] = BUILDER.craft_dependencies_task()
 
-    return all_tasks 
+    return all_tasks
 
 
 def raptor(is_staging):
@@ -96,41 +95,41 @@ def raptor(is_staging):
     mozharness_task_id = fetch_mozharness_task_id()
     gecko_revision = taskcluster.Queue().task(mozharness_task_id)['payload']['env']['GECKO_HEAD_REV']
 
-    for variant in [Variant.from_values(abi, False, 'forPerformanceTest') for abi in ('aarch64', 'arm')]:
-        assemble_task_id = taskcluster.slugId()
-        build_tasks[assemble_task_id] = BUILDER.craft_assemble_raptor_task(variant)
-        signing_task_id = taskcluster.slugId()
-        signing_tasks[signing_task_id] = BUILDER.craft_raptor_signing_task(assemble_task_id, variant, is_staging)
+    variant = get_variant('forPerformanceTest', 'geckoNightly')
+    assemble_task_id = taskcluster.slugId()
+    build_tasks[assemble_task_id] = BUILDER.craft_assemble_raptor_task(variant)
+    signing_task_id = taskcluster.slugId()
+    signing_tasks[signing_task_id] = BUILDER.craft_raptor_signing_task(assemble_task_id, variant, is_staging)
 
+    for abi in ('aarch64', 'arm'):
         all_raptor_craft_functions = [
             BUILDER.craft_raptor_tp6m_cold_task(for_suite=i)
-            for i in range(1, 28)
-        ] + [
-            BUILDER.craft_raptor_youtube_playback_task,
-        ]
+                for i in range(1, 28)
+            ] + [
+                BUILDER.craft_raptor_youtube_playback_task,
+            ]
         for craft_function in all_raptor_craft_functions:
-            args = (signing_task_id, mozharness_task_id, variant, gecko_revision)
+            args = (signing_task_id, mozharness_task_id, abi, gecko_revision)
             other_tasks[taskcluster.slugId()] = craft_function(*args)
 
     return (build_tasks, signing_tasks, other_tasks)
 
 
-def release(channel, is_staging, version_name):
-    variants = get_variants_for_build_type(channel)
-    architectures = [variant.abi for variant in variants]
-    apk_paths = ["public/build/{}/target.apk".format(arch) for arch in architectures]
+def release(channel, engine, is_staging, version_name):
+    variant = get_variant('fenix' + channel.capitalize(), engine)
+    taskcluster_apk_paths = variant.upstream_artifacts()
 
     build_tasks = {}
     signing_tasks = {}
     push_tasks = {}
 
     build_task_id = taskcluster.slugId()
-    build_tasks[build_task_id] = BUILDER.craft_assemble_release_task(architectures, channel, is_staging, version_name)
+    build_tasks[build_task_id] = BUILDER.craft_assemble_release_task(variant, is_staging, version_name)
 
     signing_task_id = taskcluster.slugId()
     signing_tasks[signing_task_id] = BUILDER.craft_release_signing_task(
         build_task_id,
-        apk_paths=apk_paths,
+        taskcluster_apk_paths,
         channel=channel,
         is_staging=is_staging,
     )
@@ -138,7 +137,7 @@ def release(channel, is_staging, version_name):
     push_task_id = taskcluster.slugId()
     push_tasks[push_task_id] = BUILDER.craft_push_task(
         signing_task_id,
-        apks=apk_paths,
+        taskcluster_apk_paths,
         channel=channel,
         # TODO until org.mozilla.fenix.nightly is made public, put it on the internally-testable track
         override_google_play_track=None if channel != "nightly" else "internal",
@@ -152,10 +151,8 @@ def nightly_to_production_app(is_staging, version_name):
     # Since the Fenix nightly was launched, we've pushed it to the production app "org.mozilla.fenix" on the
     # "nightly" track. We're moving towards having each channel be published to its own app, but we need to
     # keep updating this "backwards-compatible" nightly for a while yet
-    build_type = 'nightlyLegacy'
-    variants = get_variants_for_build_type(build_type)
-    architectures = [variant.abi for variant in variants]
-    apk_paths = ["public/build/{}/target.apk".format(arch) for arch in architectures]
+    variant = get_variant('fenixNightlyLegacy', 'geckoNightly')
+    taskcluster_apk_paths = variant.upstream_artifacts()
 
     build_tasks = {}
     signing_tasks = {}
@@ -163,12 +160,12 @@ def nightly_to_production_app(is_staging, version_name):
     other_tasks = {}
 
     build_task_id = taskcluster.slugId()
-    build_tasks[build_task_id] = BUILDER.craft_assemble_release_task(architectures, build_type, is_staging, version_name)
+    build_tasks[build_task_id] = BUILDER.craft_assemble_release_task(variant, is_staging, version_name)
 
     signing_task_id = taskcluster.slugId()
     signing_tasks[signing_task_id] = BUILDER.craft_release_signing_task(
         build_task_id,
-        apk_paths=apk_paths,
+        taskcluster_apk_paths,
         channel='production',  # Since we're publishing to the "production" app, we need to sign for production
         index_channel='nightly',
         is_staging=is_staging,
@@ -177,16 +174,17 @@ def nightly_to_production_app(is_staging, version_name):
     push_task_id = taskcluster.slugId()
     push_tasks[push_task_id] = BUILDER.craft_push_task(
         signing_task_id,
-        apks=apk_paths,
+        taskcluster_apk_paths,
         channel='production',  # We're publishing to the "production" app on the "nightly" track
         override_google_play_track='nightly',
         is_staging=is_staging,
     )
 
-    nimbledroid_task_id = taskcluster.slugId()
-    other_tasks[nimbledroid_task_id] = BUILDER.craft_upload_apk_nimbledroid_task(
-        build_task_id
-    )
+    if not is_staging:
+        nimbledroid_task_id = taskcluster.slugId()
+        other_tasks[nimbledroid_task_id] = BUILDER.craft_upload_apk_nimbledroid_task(
+            build_task_id
+        )
 
     return (build_tasks, signing_tasks, push_tasks, other_tasks)
 
@@ -213,26 +211,25 @@ if __name__ == "__main__":
 
     result = parser.parse_args()
     command = result.command
-    taskcluster_queue = taskcluster.Queue({'baseUrl': 'http://taskcluster/queue/v1'})
 
-    if command in ('pull-request'):
+    if command == 'pull-request':
         ordered_groups_of_tasks = pr()
-    elif command in ('push'):
+    elif command == 'push':
         ordered_groups_of_tasks = push()
     elif command == 'raptor':
         ordered_groups_of_tasks = raptor(result.staging)
     elif command == 'nightly':
         nightly_version = datetime.datetime.now().strftime('Nightly %y%m%d %H:%M')
-        ordered_groups_of_tasks = release('nightly', result.staging, nightly_version) \
+        ordered_groups_of_tasks = release('nightly', 'geckoNightly', result.staging, nightly_version) \
             + nightly_to_production_app(result.staging, nightly_version)
     elif command == 'github-release':
         version = result.tag[1:]  # remove prefixed "v"
         beta_semver = re.compile(r'^v\d+\.\d+\.\d+-beta\.\d+$')
         production_semver = re.compile(r'^v\d+\.\d+\.\d+(-rc\.\d+)?$')
         if beta_semver.match(result.tag):
-            ordered_groups_of_tasks = release('beta', result.staging, version)
+            ordered_groups_of_tasks = release('beta', 'geckoBeta', result.staging, version)
         elif production_semver.match(result.tag):
-            ordered_groups_of_tasks = release('production', result.staging, version)
+            ordered_groups_of_tasks = release('production', 'geckoBeta', result.staging, version)
         else:
             raise ValueError('Github tag must be in semver format and prefixed with a "v", '
                              'e.g.: "v1.0.0-beta.0" (beta), "v1.0.0-rc.0" (production) or "v1.0.0" (production)')
