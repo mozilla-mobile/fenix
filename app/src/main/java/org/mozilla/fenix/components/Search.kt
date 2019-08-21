a0874702bdb88c509a6a260e13a44709f02d6a80
Colin Lee <mncolinlee@gmail.com>
2019-08-21 12:01:15 -0500

    For #3869: Switch to A-C SearchLocalizationProvider

--- app/src/main/java/org/mozilla/fenix/components/Search.kt
+++ app/src/main/java/org/mozilla/fenix/components/Search.kt
@@ -10,26 +10,9 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import mozilla.components.browser.search.SearchEngineManager
 import mozilla.components.browser.search.provider.AssetsSearchEngineProvider
-import mozilla.components.browser.search.provider.localization.SearchLocalizationProvider
+import mozilla.components.browser.search.provider.localization.LocaleSearchLocalizationProvider
 import org.mozilla.fenix.test.Mockable
 import org.mozilla.fenix.utils.Settings
-import java.util.Locale
-
-// Create our own SearchLocalizationProivder that sets the region based on the country
-// This is a temporary fix until we have a localization provider that can look up the users
-// issue
-// https://github.com/mozilla-mobile/fenix/issues/3869
-// https://github.com/mozilla-mobile/android-components/issues/3637
-class FenixLocaleSearchLocalizationProvider : SearchLocalizationProvider() {
-    override val language: String
-        get() = Locale.getDefault().language
-
-    override val country: String
-        get() = Locale.getDefault().country
-
-    override val region: String
-        get() = country
-}
 
 /**
  * Component group for all search engine integration related functionality.
@@ -41,9 +24,11 @@ class Search(private val context: Context) {
      * This component provides access to a centralized registry of search engines.
      */
     val searchEngineManager by lazy {
-        SearchEngineManager(coroutineContext = IO, providers = listOf(
-            AssetsSearchEngineProvider(FenixLocaleSearchLocalizationProvider())
-        )).apply {
+        SearchEngineManager(
+            coroutineContext = IO, providers = listOf(
+                AssetsSearchEngineProvider(LocaleSearchLocalizationProvider())
+            )
+        ).apply {
             registerForLocaleUpdates(context)
             GlobalScope.launch {
                 loadAsync(context).await()
