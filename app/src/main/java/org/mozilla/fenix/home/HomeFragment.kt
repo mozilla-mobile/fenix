d1964d6536f6e911cab96f60d5bf24547efcd1a3
Tiger Oakes <toakes@mozilla.com>
2019-08-27 16:04:03 -0400

    Fixes #4771 - Add method for sessions of type (#4782)
    Fixes share tabs to use the new method

--- app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -60,6 +60,7 @@ import org.mozilla.fenix.components.TabCollectionStorage
 import org.mozilla.fenix.components.metrics.Event
 import org.mozilla.fenix.ext.nav
 import org.mozilla.fenix.ext.requireComponents
+import org.mozilla.fenix.ext.sessionsOfType
 import org.mozilla.fenix.ext.toTab
 import org.mozilla.fenix.home.sessioncontrol.CollectionAction
 import org.mozilla.fenix.home.sessioncontrol.Mode
@@ -348,16 +349,21 @@ class HomeFragment : Fragment(), AccountObserver {
                 }
             }
             is TabAction.CloseAll -> {
-                if (pendingSessionDeletion?.deletionJob == null) removeAllTabsWithUndo(
-                    sessionManager.filteredSessions(action.private),
-                    action.private
-                ) else {
+                if (pendingSessionDeletion?.deletionJob == null) {
+                    removeAllTabsWithUndo(
+                        sessionManager.sessionsOfType(private = action.private),
+                        action.private
+                    )
+                } else {
                     pendingSessionDeletion?.deletionJob?.let {
                         viewLifecycleOwner.lifecycleScope.launch {
                             it.invoke()
                         }.invokeOnCompletion {
                             pendingSessionDeletion = null
-                            removeAllTabsWithUndo(sessionManager.filteredSessions(action.private), action.private)
+                            removeAllTabsWithUndo(
+                                sessionManager.sessionsOfType(private = action.private),
+                                action.private
+                            )
                         }
                     }
                 }
@@ -377,9 +383,10 @@ class HomeFragment : Fragment(), AccountObserver {
             }
             is TabAction.ShareTabs -> {
                 invokePendingDeleteJobs()
-                val shareTabs = sessionManager.sessions.map {
-                    ShareTab(it.url, it.title, it.id)
-                }
+                val shareTabs = sessionManager
+                    .sessionsOfType(private = (activity as HomeActivity).browsingModeManager.mode.isPrivate)
+                    .map { ShareTab(it.url, it.title, it.id) }
+                    .toList()
                 share(tabs = shareTabs)
             }
         }
@@ -587,7 +594,7 @@ class HomeFragment : Fragment(), AccountObserver {
         }
     }
 
-    private fun removeAllTabsWithUndo(listOfSessionsToDelete: List<Session>, private: Boolean) {
+    private fun removeAllTabsWithUndo(listOfSessionsToDelete: Sequence<Session>, private: Boolean) {
         val sessionManager = requireComponents.core.sessionManager
 
         getManagedEmitter<SessionControlChange>().onNext(SessionControlChange.TabsChange(listOf()))
@@ -651,9 +658,9 @@ class HomeFragment : Fragment(), AccountObserver {
     }
 
     private fun getListOfSessions(): List<Session> {
-        val isPrivate = browsingModeManager.mode.isPrivate
-        val notPendingDeletion = { session: Session -> session.id != pendingSessionDeletion?.sessionId }
-        return sessionManager.filteredSessions(isPrivate, notPendingDeletion)
+        return sessionManager.sessionsOfType(private = browsingModeManager.mode.isPrivate)
+            .filter { session: Session -> session.id != pendingSessionDeletion?.sessionId }
+            .toList()
     }
 
     private fun showCollectionCreationFragment(
@@ -843,15 +850,6 @@ class HomeFragment : Fragment(), AccountObserver {
             val string = view.context.getString(R.string.snackbar_collection_renamed)
             FenixSnackbar.make(view, Snackbar.LENGTH_LONG).setText(string).show()
         }
-    }
-
-    private fun SessionManager.filteredSessions(
-        private: Boolean,
-        sessionFilter: ((Session) -> Boolean)? = null
-    ): List<Session> {
-        return this.sessions
-            .filter { private == it.private }
-            .filter { sessionFilter?.invoke(it) ?: true }
     }
 
     private fun List<Session>.toTabs(): List<Tab> {
