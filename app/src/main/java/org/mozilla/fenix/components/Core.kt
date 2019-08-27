55fdfc85a42c707ba9dfffe23ffc36540564508e
Arturo Mejia <arturomejiamarmol@gmail.com>
2019-08-27 12:14:56 -0400

    Fix safe browsing API breaking change.

--- app/src/main/java/org/mozilla/fenix/components/Core.kt
+++ app/src/main/java/org/mozilla/fenix/components/Core.kt
@@ -21,9 +21,6 @@ import mozilla.components.browser.storage.sync.PlacesHistoryStorage
 import mozilla.components.concept.engine.DefaultSettings
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy
-import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy.SafeBrowsingCategory
-import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy.Companion.select
-import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy.TrackingCategory
 import mozilla.components.concept.engine.mediaquery.PreferredColorScheme
 import mozilla.components.concept.fetch.Client
 import mozilla.components.feature.media.MediaFeature
@@ -179,10 +176,7 @@ class Core(private val context: Context) {
             normalMode && privateMode -> trackingProtectionPolicy
             normalMode && !privateMode -> trackingProtectionPolicy.forRegularSessionsOnly()
             !normalMode && privateMode -> trackingProtectionPolicy.forPrivateSessionsOnly()
-            else -> select(
-                trackingCategories = arrayOf(TrackingCategory.NONE),
-                safeBrowsingCategories = arrayOf(SafeBrowsingCategory.RECOMMENDED)
-            )
+            else -> TrackingProtectionPolicy.none()
         }
     }
 
