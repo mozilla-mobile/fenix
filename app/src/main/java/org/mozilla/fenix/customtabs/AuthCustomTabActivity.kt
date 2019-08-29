39e9a466d1ce497c5f1f3329d9542067d3d24192
Tiger Oakes <toakes@mozilla.com>
2019-08-29 15:36:46 -0400

    Add comments to AuthCustomTabActivity

--- app/src/main/java/org/mozilla/fenix/customtabs/AuthCustomTabActivity.kt
+++ app/src/main/java/org/mozilla/fenix/customtabs/AuthCustomTabActivity.kt
@@ -6,22 +6,25 @@ package org.mozilla.fenix.customtabs
 
 import mozilla.components.concept.sync.AccountObserver
 import mozilla.components.concept.sync.OAuthAccount
-import mozilla.components.service.fxa.manager.FxaAccountManager
 import org.mozilla.fenix.ext.components
 
+/**
+ * A special custom tab for signing into a Firefox Account. The activity is closed once the user is signed in.
+ */
 class AuthCustomTabActivity : CustomTabActivity() {
-    private lateinit var accountManager: FxaAccountManager
 
-    // Navigate away from this activity when we have successful authentication
     private val accountStateObserver = object : AccountObserver {
+        /**
+         * Navigate away from this activity when we have successful authentication
+         */
         override fun onAuthenticated(account: OAuthAccount, newAccount: Boolean) {
-            this@AuthCustomTabActivity.finish()
+            finish()
         }
     }
 
     override fun onResume() {
         super.onResume()
-        accountManager = this.components.backgroundServices.accountManager
+        val accountManager = components.backgroundServices.accountManager
         accountManager.register(accountStateObserver, this, true)
     }
 }
