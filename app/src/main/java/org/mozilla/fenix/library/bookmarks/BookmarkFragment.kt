ce7c251159986615f481b1e0104abe3b684c3302
mihai-adrian <mihai.carare.dev@gmail.com>
2019-09-05 18:45:12 +0300

    For #4991,#4993:Ensured deleteSelectedBookmarks deletes all selected bookmarks
    Moved the deletion to a CoroutineScope(IO) to decouple from fragment lifecycle
    Used local val tempStorage to keep reference to bookmarkStorage of outside class
    Ran deletions with awaitAll to avoid concurrency issues on storage

--- app/src/main/java/org/mozilla/fenix/library/bookmarks/BookmarkFragment.kt
+++ app/src/main/java/org/mozilla/fenix/library/bookmarks/BookmarkFragment.kt
@@ -23,6 +23,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_bookmark.view.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -30,6 +32,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.awaitAll
 import mozilla.appservices.places.BookmarkRoot
 import mozilla.components.concept.storage.BookmarkNode
 import mozilla.components.concept.storage.BookmarkNodeType
@@ -275,8 +278,14 @@ class BookmarkFragment : LibraryPageFragment<BookmarkNode>(), BackHandler, Accou
     }
 
     private suspend fun deleteSelectedBookmarks(selected: Set<BookmarkNode>) {
-        selected.forEach {
-            context?.bookmarkStorage()?.deleteNode(it.guid)
+        CoroutineScope(IO).launch {
+            val tempStorage = context?.bookmarkStorage()
+            val deferreds = selected.map {
+                async {
+                    tempStorage?.deleteNode(it.guid)
+                }
+            }
+            deferreds.awaitAll()
         }
     }
 
