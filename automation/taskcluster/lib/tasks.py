4ae8d960a88c5149fd3f6ad4f7f5c7641524130c
Johan Lorenzo <jlorenzo@mozilla.com>
2019-09-24 17:20:49 +0200

    Bug 1580778 - Raptor: put ytp exceptions back

--- automation/taskcluster/lib/tasks.py
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-# Bug 1558456 - Stop tracking youtube-playback-test on motoG5 for >1080p cases
-ARM_RAPTOR_URL_PARAMS = [
-    "exclude=1,2,9,10,17,18,21,22,26,28,30,32,39,40,47,"
-    "48,55,56,63,64,71,72,79,80,83,84,89,90,95,96",
-]
