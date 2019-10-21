857a3b2e238f4bc9f387a3d833b78051229e8146
Vlad Filippov <vlad.filippov@gmail.com>
2019-10-21 12:16:34 -0400

    Closes #5913: Add new sessionToken scope

--- app/src/main/java/org/mozilla/fenix/components/BackgroundServices.kt
+++ app/src/main/java/org/mozilla/fenix/components/BackgroundServices.kt
@@ -32,6 +32,7 @@ import mozilla.components.service.fxa.ServerConfig
 import mozilla.components.service.fxa.SyncConfig
 import mozilla.components.service.fxa.SyncEngine
 import mozilla.components.service.fxa.manager.FxaAccountManager
+import mozilla.components.service.fxa.manager.SCOPE_SESSION
 import mozilla.components.service.fxa.manager.SCOPE_SYNC
 import mozilla.components.service.fxa.sync.GlobalSyncableStoreProvider
 import mozilla.components.service.sync.logins.SyncableLoginsStore
@@ -176,7 +177,10 @@ class BackgroundServices(
             // during authentication unless we explicitly specify it below.
             // This is a good example of an information leak at the API level.
             // See https://github.com/mozilla-mobile/android-components/issues/3732
-            SCOPE_SYNC
+            SCOPE_SYNC,
+            // Necessary to enable "Manage Account" functionality and ability to generate OAuth
+            // codes for certain scopes.
+            SCOPE_SESSION
         )
     ).also { accountManager ->
         // TODO this needs to change once we have a SyncManager
