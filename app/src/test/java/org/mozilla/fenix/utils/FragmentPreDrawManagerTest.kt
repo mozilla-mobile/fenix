e29999a73976fe6b1d83ed04352cdbab9031d530
obarzpaw <pawelobarzanek@gmail.com>
2019-10-21 08:32:27 +0200

    Closes mozilla-mobile#4828 verify if addOnPreDrawListener was called

--- app/src/test/java/org/mozilla/fenix/utils/FragmentPreDrawManagerTest.kt
+++ app/src/test/java/org/mozilla/fenix/utils/FragmentPreDrawManagerTest.kt
@@ -1,8 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.fenix.utils
 
+import androidx.core.view.OneShotPreDrawListener
 import androidx.fragment.app.Fragment
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import io.mockk.*
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
@@ -16,18 +22,18 @@ import org.robolectric.annotation.Config
 @RunWith(AndroidJUnit4::class)
 @Config(application = TestApplication::class)
 class FragmentPreDrawManagerTest {
-
-    private fun doNothing() {  }
+    private fun doNothing() { /*noop*/ }
 
     @Test
     fun `execute doOnPreDraw`() {
         runBlockingTest {
             val fragmentMock = mockk<Fragment>(relaxed = true)
             val fragmentPreDrawManager = FragmentPreDrawManager(fragmentMock)
+            val listener = OneShotPreDrawListener.add(fragmentMock.view!!) { mockk() }
 
             verify { fragmentMock.postponeEnterTransition() }
             fragmentPreDrawManager.execute { doNothing() }
-            verify { fragmentMock.startPostponedEnterTransition() }
+            verify { fragmentMock.view?.viewTreeObserver?.addOnPreDrawListener(listener) }
         }
     }
 }
