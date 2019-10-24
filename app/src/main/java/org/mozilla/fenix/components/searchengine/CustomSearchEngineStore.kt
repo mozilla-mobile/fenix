773daf7bc1c04451c3d77dce20a56eaf4c15a99c
Jeff Boek <jeff@jeffboek.com>
2019-10-24 08:09:48 -0700

    For #5577 - Adds custom engine store

---
+++ app/src/main/java/org/mozilla/fenix/components/searchengine/CustomSearchEngineStore.kt
@@ -0,0 +1,71 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.components.searchengine
+
+import android.content.Context
+import mozilla.components.browser.icons.IconRequest
+import mozilla.components.browser.search.SearchEngine
+import mozilla.components.browser.search.SearchEngineParser
+import org.mozilla.fenix.ext.components
+import java.lang.Exception
+
+object CustomSearchEngineStore {
+    class EngineNameAlreadyExists: Exception()
+
+    suspend fun addSearchEngine(context: Context, engineName: String, searchQuery: String) {
+        val prefs = pref(context)
+        if (prefs.contains(engineName)) { throw EngineNameAlreadyExists() }
+
+        val icon = context.components.core.icons.loadIcon(IconRequest(searchQuery)).await()
+        val searchEngineXml = SearchEngineWriter.buildSearchEngineXML(engineName, searchQuery, icon.bitmap)
+        val engines = pref(context)
+            .getStringSet(PREF_KEY_CUSTOM_SEARCH_ENGINES, emptySet()) ?: emptySet()
+        engines.add(engineName)
+
+        pref(context)
+            .edit()
+            .putStringSet(PREF_KEY_CUSTOM_SEARCH_ENGINES, engines)
+            .putString(engineName, searchEngineXml)
+            .apply()
+    }
+
+    fun removeSearchEngine(context: Context, engineId: String) {
+        val customEngines = pref(context).getStringSet(PREF_KEY_CUSTOM_SEARCH_ENGINES, emptySet())
+        val enginesEditor = pref(context).edit()
+        enginesEditor.remove(engineId)
+
+        enginesEditor.putStringSet(
+            PREF_KEY_CUSTOM_SEARCH_ENGINES,
+            customEngines?.filterNot { it == engineId }?.toSet() ?: emptySet()
+        )
+
+        enginesEditor.apply()
+    }
+
+    fun isCustomSearchEngine(engineId: String, context: Context): Boolean {
+        return loadCustomSearchEngines(context).firstOrNull { it.identifier == engineId } != null
+    }
+
+    fun loadCustomSearchEngines(context: Context): List<SearchEngine> {
+        val parser = SearchEngineParser()
+        val prefs = pref(context)
+        val engines = prefs.getStringSet(PREF_KEY_CUSTOM_SEARCH_ENGINES, emptySet()) ?: emptySet()
+
+        return engines.map {
+            val engineInputStream = prefs.getString(it, "")!!.byteInputStream().buffered()
+            parser.load(it, engineInputStream)
+        }
+    }
+
+    private fun pref(context: Context) =
+        context.getSharedPreferences(PREF_FILE_SEARCH_ENGINES, Context.MODE_PRIVATE)
+
+    const val ENGINE_TYPE_CUSTOM = "custom"
+    const val ENGINE_TYPE_BUNDLED = "bundled"
+
+    private const val REMOVED_ENGINES = "removed_engine_ids"
+    private const val PREF_KEY_CUSTOM_SEARCH_ENGINES = "pref_custom_search_engines"
+    private const val PREF_FILE_SEARCH_ENGINES = "custom-search-engines"
+}
