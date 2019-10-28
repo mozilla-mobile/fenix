1c3cf55b158c2bedfe2773f2d481080bc0e908d7
Severin Rudie <srudie@mozilla.com>
2019-10-28 14:31:21 -0700

    For #4281: small ToolbarMenu refactor
    This makes it easier to see how items are ordered in the menuItems list

--- app/src/main/java/org/mozilla/fenix/components/toolbar/DefaultToolbarMenu.kt
+++ app/src/main/java/org/mozilla/fenix/components/toolbar/DefaultToolbarMenu.kt
@@ -97,139 +97,143 @@ class DefaultToolbarMenu(
     }
 
     private val menuItems by lazy {
-        val items = mutableListOf(
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_help),
-                R.drawable.ic_help,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.Help)
-            },
+        val browsingModeIsNormal = (context.asActivity() as? HomeActivity)
+            ?.browsingModeManager?.mode == BrowsingMode.Normal
+        val shouldDeleteDataOnQuit = Settings.getInstance(context)
+            .shouldDeleteBrowsingDataOnQuit
 
-            BrowserMenuHighlightableItem(
-                label = context.getString(R.string.browser_menu_settings),
-                imageResource = R.drawable.ic_settings,
-                iconTintColorResource = if (hasAccountProblem)
-                    R.color.sync_error_text_color else
-                    ThemeManager.resolveAttribute(R.attr.primaryText, context),
-                textColorResource = if (hasAccountProblem)
-                    R.color.sync_error_text_color else
-                    ThemeManager.resolveAttribute(R.attr.primaryText, context),
-                highlight = if (hasAccountProblem) {
-                    BrowserMenuHighlightableItem.Highlight(
-                        endImageResource = R.drawable.ic_alert,
-                        backgroundResource = R.drawable.sync_error_background_with_ripple,
-                        colorResource = R.color.sync_error_background_color
-                    )
-                } else null
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.Settings)
-            },
-
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_your_library),
-                R.drawable.ic_library,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.Library)
-            },
-
-            BrowserMenuImageSwitch(
-                R.drawable.ic_desktop,
-                context.getString(R.string.browser_menu_desktop_site),
-                requestDesktopStateProvider
-            ) { checked ->
-                onItemTapped.invoke(ToolbarMenu.Item.RequestDesktop(checked))
-            },
-
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_add_to_homescreen),
-                R.drawable.ic_add_to_homescreen,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.AddToHomeScreen)
-            }.apply {
-                visible = ::shouldShowAddToHomescreen
-            },
-
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_find_in_page),
-                R.drawable.mozac_ic_search,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.FindInPage)
-            },
-
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_private_tab),
-                R.drawable.ic_private_browsing,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.NewPrivateTab)
-            },
-
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_new_tab),
-                R.drawable.ic_new,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.NewTab)
-            },
-
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_share),
-                R.drawable.mozac_ic_share,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.Share)
-            },
-
-            BrowserMenuImageText(
-                context.getString(R.string.browser_menu_report_issue),
-                R.drawable.ic_report_issues,
-                ThemeManager.resolveAttribute(R.attr.primaryText, context)
-            ) {
-                onItemTapped.invoke(ToolbarMenu.Item.ReportIssue)
-            }
-        )
-
-        if ((context.asActivity() as? HomeActivity)?.browsingModeManager?.mode == BrowsingMode.Normal) {
-            items.add(
-                BrowserMenuImageText(
-                    context.getString(R.string.browser_menu_save_to_collection),
-                    R.drawable.ic_tab_collection,
-                    ThemeManager.resolveAttribute(R.attr.primaryText, context)
-                ) {
-                    onItemTapped.invoke(ToolbarMenu.Item.SaveToCollection)
-                }
-            )
-        }
-
-        if (Settings.getInstance(context).shouldDeleteBrowsingDataOnQuit) {
-            items.add(
-                BrowserMenuImageText(
-                    context.getString(R.string.delete_browsing_data_on_quit_action),
-                    R.drawable.ic_exit,
-                    ThemeManager.resolveAttribute(R.attr.primaryText, context)
-                ) {
-                    onItemTapped.invoke(ToolbarMenu.Item.Quit)
-                }
-            )
-        }
-
-        items.add(
-            BrowserMenuDivider()
-        )
-
-        items.add(
+        listOfNotNull(
+            help,
+            settings,
+            library,
+            desktopMode,
+            addToHomescreen,
+            findInPage,
+            privateTab,
+            newTab,
+            share,
+            reportIssue,
+            if (browsingModeIsNormal) saveToCollection else null,
+            if (shouldDeleteDataOnQuit) deleteDataOnQuit else null,
+            BrowserMenuDivider(),
             menuToolbar
         )
+    }
 
-        items
+    private val help = BrowserMenuImageText(
+        context.getString(R.string.browser_menu_help),
+        R.drawable.ic_help,
+        primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.Help)
+    }
+
+    private val settings = BrowserMenuHighlightableItem(
+        label = context.getString(R.string.browser_menu_settings),
+        imageResource = R.drawable.ic_settings,
+        iconTintColorResource = if (hasAccountProblem)
+            R.color.sync_error_text_color else
+            primaryTextColor(),
+        textColorResource = if (hasAccountProblem)
+            R.color.sync_error_text_color else
+            primaryTextColor(),
+        highlight = if (hasAccountProblem) {
+            BrowserMenuHighlightableItem.Highlight(
+                endImageResource = R.drawable.ic_alert,
+                backgroundResource = R.drawable.sync_error_background_with_ripple,
+                colorResource = R.color.sync_error_background_color
+            )
+        } else null
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.Settings)
+    }
+
+    private val library = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_your_library),
+        imageResource = R.drawable.ic_library,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.Library)
+    }
+
+    private val desktopMode = BrowserMenuImageSwitch(
+        imageResource = R.drawable.ic_desktop,
+        label = context.getString(R.string.browser_menu_desktop_site),
+        initialState = requestDesktopStateProvider
+    ) { checked ->
+        onItemTapped.invoke(ToolbarMenu.Item.RequestDesktop(checked))
+    }
+
+    private val addToHomescreen = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_add_to_homescreen),
+        imageResource = R.drawable.ic_add_to_homescreen,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.AddToHomeScreen)
+    }.apply {
+        visible = ::shouldShowAddToHomescreen
     }
 
     private fun shouldShowAddToHomescreen(): Boolean {
         return context.components.useCases.webAppUseCases.isPinningSupported() &&
                 context.components.core.sessionManager.selectedSession != null
     }
+
+    private val findInPage = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_find_in_page),
+        imageResource = R.drawable.mozac_ic_search,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.FindInPage)
+    }
+
+    private val privateTab = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_private_tab),
+        imageResource = R.drawable.ic_private_browsing,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.NewPrivateTab)
+    }
+
+    private val newTab = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_new_tab),
+        imageResource = R.drawable.ic_new,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.NewTab)
+    }
+
+    private val share = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_share),
+        imageResource = R.drawable.mozac_ic_share,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.Share)
+    }
+
+    private val reportIssue = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_report_issue),
+        imageResource = R.drawable.ic_report_issues,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.ReportIssue)
+    }
+
+    private val saveToCollection = BrowserMenuImageText(
+        label = context.getString(R.string.browser_menu_save_to_collection),
+        imageResource = R.drawable.ic_tab_collection,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.SaveToCollection)
+    }
+
+    private val deleteDataOnQuit = BrowserMenuImageText(
+        label = context.getString(R.string.delete_browsing_data_on_quit_action),
+        imageResource = R.drawable.ic_exit,
+        iconTintColorResource = primaryTextColor()
+    ) {
+        onItemTapped.invoke(ToolbarMenu.Item.Quit)
+    }
+
+    private fun primaryTextColor() = ThemeManager.resolveAttribute(R.attr.primaryText, context)
 }
