b84287365a6217de476c27eb461d128002f0fb06
Severin Rudie <srudie@mozilla.com>
2019-10-31 11:28:27 -0700

    For 4281: fix test compilation after QAB removal
    Tests still need to be expanded to include added functionality

--- app/src/test/java/org/mozilla/fenix/components/toolbar/BrowserInteractorTest.kt
+++ app/src/test/java/org/mozilla/fenix/components/toolbar/BrowserInteractorTest.kt
@@ -15,7 +15,6 @@ import org.mozilla.fenix.components.metrics.Event
 import org.mozilla.fenix.components.metrics.MetricController
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.metrics
-import org.mozilla.fenix.quickactionsheet.QuickActionSheetController
 
 class BrowserInteractorTest {
 
@@ -23,7 +22,6 @@ class BrowserInteractorTest {
     lateinit var context: Context
     lateinit var browserStore: BrowserFragmentStore
     lateinit var browserToolbarController: BrowserToolbarController
-    lateinit var quickActionSheetController: QuickActionSheetController
     lateinit var readerModeController: ReaderModeController
     lateinit var session: Session
     lateinit var interactor: BrowserInteractor
@@ -34,14 +32,12 @@ class BrowserInteractorTest {
         context = mockk()
         browserStore = mockk(relaxed = true)
         browserToolbarController = mockk(relaxed = true)
-        quickActionSheetController = mockk(relaxed = true)
         readerModeController = mockk(relaxed = true)
         session = mockk()
         interactor = BrowserInteractor(
             context,
             browserStore,
             browserToolbarController,
-            quickActionSheetController,
             readerModeController,
             session
         )
@@ -84,87 +80,5 @@ class BrowserInteractorTest {
         interactor.onBrowserToolbarMenuItemTapped(item)
 
         verify { browserToolbarController.handleToolbarItemInteraction(item) }
-    }
-
-    @Test
-    fun onQuickActionSheetOpened() {
-        every { metrics.track(Event.QuickActionSheetOpened) } just Runs
-
-        interactor.onQuickActionSheetOpened()
-
-        verify { metrics.track(Event.QuickActionSheetOpened) }
-    }
-
-    @Test
-    fun onQuickActionSheetClosed() {
-        every { metrics.track(Event.QuickActionSheetClosed) } just Runs
-
-        interactor.onQuickActionSheetClosed()
-
-        verify { metrics.track(Event.QuickActionSheetClosed) }
-    }
-
-    @Test
-    fun onQuickActionSheetSharePressed() {
-        interactor.onQuickActionSheetSharePressed()
-
-        verify { quickActionSheetController.handleShare() }
-    }
-
-    @Test
-    fun onQuickActionSheetDownloadPressed() {
-        every { metrics.track(Event.QuickActionSheetDownloadTapped) } just Runs
-
-        interactor.onQuickActionSheetDownloadPressed()
-
-        verify { quickActionSheetController.handleDownload() }
-    }
-
-    @Test
-    fun onQuickActionSheetBookmarkPressed() {
-        interactor.onQuickActionSheetBookmarkPressed()
-
-        verify { quickActionSheetController.handleBookmark() }
-    }
-
-    @Test
-    fun onQuickActionSheetReadPressed() {
-        every { session.readerMode } returns false
-        every { metrics.track(Event.QuickActionSheetOpened) } just Runs
-
-        interactor.onQuickActionSheetReadPressed()
-
-        verify { metrics.track(Event.QuickActionSheetOpened) }
-        verify { readerModeController.showReaderView() }
-    }
-
-    @Test
-    fun onQuickActionSheetReadPressedWithActiveReaderMode() {
-        every { session.readerMode } returns true
-        every { metrics.track(Event.QuickActionSheetClosed) } just Runs
-
-        interactor.onQuickActionSheetReadPressed()
-
-        verify { metrics.track(Event.QuickActionSheetClosed) }
-        verify { readerModeController.hideReaderView() }
-    }
-
-    @Test
-    fun onQuickActionSheetOpenLinkPressed() {
-        interactor.onQuickActionSheetOpenLinkPressed()
-
-        verify { quickActionSheetController.handleOpenLink() }
-    }
-
-    @Test
-    fun onQuickActionSheetAppearancePressed() {
-        every { metrics.track(Event.ReaderModeAppearanceOpened) } just Runs
-
-        interactor.onQuickActionSheetAppearancePressed()
-
-        verify {
-            metrics.track(Event.ReaderModeAppearanceOpened)
-            readerModeController.showControls()
-        }
     }
 }
