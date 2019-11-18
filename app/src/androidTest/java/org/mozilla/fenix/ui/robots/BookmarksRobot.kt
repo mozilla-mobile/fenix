e6d218768fbd5740758fcc045855fef9a663af86
Patrick Martin <patrickmartin88@gmail.com>
2019-11-18 13:50:55 -0800

    For #2294 - History URL is cut with a large font set

--- app/src/androidTest/java/org/mozilla/fenix/ui/robots/BookmarksRobot.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/robots/BookmarksRobot.kt
@@ -129,7 +129,6 @@ private fun assertBookmarkFavicon() = bookmarkFavicon().check(
 )
 
 private fun assertBookmarkURL(expectedURL: Uri) = bookmarkURL()
-//    .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
     .check(matches(ViewMatchers.isCompletelyDisplayed()))
     .check(matches(withText(containsString(expectedURL.toString()))))
 
