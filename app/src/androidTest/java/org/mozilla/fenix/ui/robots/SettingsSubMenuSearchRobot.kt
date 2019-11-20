dba2186e86c0d480a2a1490650893ab92134dba7
ekager <emilykager@gmail.com>
2019-11-20 07:58:02 -0800

    Add scroll to item

--- app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsSubMenuSearchRobot.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsSubMenuSearchRobot.kt
@@ -6,11 +6,16 @@
 
 package org.mozilla.fenix.ui.robots
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
 import org.hamcrest.CoreMatchers
@@ -40,8 +45,10 @@ class SettingsSubMenuSearchRobot {
     }
 }
 
-private fun assertDefaultSearchEngineHeader() = onView(ViewMatchers.withText("Default search engine"))
-    .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertDefaultSearchEngineHeader() =
+    onView(ViewMatchers.withText("Default search engine"))
+        .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+
 private fun assertSearchEngineList() {
     onView(ViewMatchers.withText("Google"))
         .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
@@ -59,17 +66,55 @@ private fun assertSearchEngineList() {
         .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
 }
 
-private fun assertShowSearchSuggestions() = onView(ViewMatchers.withText("Show search suggestions"))
-    .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertShowSearchSuggestions() {
+    onView(withId(androidx.preference.R.id.recycler_view)).perform(
+        RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+            hasDescendant(withText("Show search suggestions"))
+        )
+    )
+    onView(ViewMatchers.withText("Show search suggestions"))
+        .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+}
 
-private fun assertShowSearchShortcuts() = onView(ViewMatchers.withText("Show search shortcuts"))
-    .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
-private fun assertShowClipboardSuggestions() = onView(ViewMatchers.withText("Show clipboard suggestions"))
-    .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
-private fun assertSearchBrowsingHistory() = onView(ViewMatchers.withText("Search browsing history"))
-    .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
-private fun assertSearchBookmarks() = onView(ViewMatchers.withText("Search bookmarks"))
-    .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertShowSearchShortcuts() {
+    onView(withId(androidx.preference.R.id.recycler_view)).perform(
+        RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+            hasDescendant(withText("Show search shortcuts"))
+        )
+    )
+    onView(ViewMatchers.withText("Show search shortcuts"))
+        .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+}
+
+private fun assertShowClipboardSuggestions() {
+    onView(withId(androidx.preference.R.id.recycler_view)).perform(
+        RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+            hasDescendant(withText("Show clipboard suggestions"))
+        )
+    )
+    onView(ViewMatchers.withText("Show clipboard suggestions"))
+        .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+}
+
+private fun assertSearchBrowsingHistory() {
+    onView(withId(androidx.preference.R.id.recycler_view)).perform(
+        RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+            hasDescendant(withText("Search browsing history"))
+        )
+    )
+    onView(ViewMatchers.withText("Search browsing history"))
+        .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+}
+
+private fun assertSearchBookmarks() {
+    onView(withId(androidx.preference.R.id.recycler_view)).perform(
+        RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+            hasDescendant(withText("Search bookmarks"))
+        )
+    )
+    onView(ViewMatchers.withText("Search bookmarks"))
+        .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+}
 
 private fun goBackButton() =
     onView(CoreMatchers.allOf(ViewMatchers.withContentDescription("Navigate up")))
