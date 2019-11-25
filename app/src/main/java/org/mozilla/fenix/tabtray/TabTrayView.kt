a11e7b7f02f5d5f0b2ff44629cbf9dcf3da432ef
David Walsh <davidwalsh83@gmail.com>
2019-11-25 13:21:02 -0600

    Implement basic RecyclerView to display tab items

--- app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
@@ -3,11 +3,51 @@ package org.mozilla.fenix.tabtray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.component_tab_tray.view.*
+import kotlinx.android.synthetic.main.tab_tray_list_item.view.*
 import org.mozilla.fenix.R
-import org.mozilla.fenix.ext.logDebug
+
+class TabItemViewHolder(
+    private val view: View
+) : RecyclerView.ViewHolder(view) {
+
+
+    fun bind(tab: Tab) {
+        view.title.text = tab.title
+        view.url.text= tab.url
+    }
+
+    companion object {
+        const val LAYOUT_ID = R.layout.tab_tray_list_item
+    }
+}
+
+class TabTrayAdapter: RecyclerView.Adapter<TabItemViewHolder>() {
+    private var tabs = listOf<Tab>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TabItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(TabItemViewHolder.LAYOUT_ID, parent, false)
+        return TabItemViewHolder(view)
+    }
+
+    override fun getItemCount() = tabs.size
+
+    override fun onBindViewHolder(holder: TabItemViewHolder, position: Int) {
+        holder.bind(tabs[position])
+    }
+
+    fun updateTabs(tabs: List<Tab>) {
+        this.tabs = tabs
+        notifyDataSetChanged()
+    }
+}
 
 class TabTrayView(val container: ViewGroup) : LayoutContainer {
+
+    val tabTrayAdapter = TabTrayAdapter()
 
     override val containerView: View?
         get() = container
@@ -15,7 +55,14 @@ class TabTrayView(val container: ViewGroup) : LayoutContainer {
     val view: View = LayoutInflater.from(container.context)
         .inflate(R.layout.component_tab_tray, container, true)
 
+    init {
+        view.tab_tray_list.apply {
+            adapter = tabTrayAdapter
+            (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+        }
+    }
+
     fun update(state: TabTrayFragmentState) {
-        logDebug("boek", state.toString())
+        tabTrayAdapter.updateTabs(state.tabs)
     }
 }
