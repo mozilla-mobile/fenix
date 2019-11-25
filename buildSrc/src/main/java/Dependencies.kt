d69db50968aba571658664d4cbdbad47c138c733
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-11-25 18:28:50 +0100

    Update the Lifecycle dependency to 2.2.0-rc02 (#6780)
    The changelog [lives here](https://developer.android.com/jetpack/androidx/releases/lifecycle#2.2.0-rc02).
    This is another attempt to fix the oddities being detected by
    Glean in Fenix: 'baseline' ping are not being sent for a certain
    subset of users, 'metrics' pings are.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -24,7 +24,7 @@ object Versions {
     const val androidx_preference = "1.1.0"
     const val androidx_legacy = "1.0.0"
     const val androidx_annotation = "1.1.0"
-    const val androidx_lifecycle = "2.2.0-beta01"
+    const val androidx_lifecycle = "2.2.0-rc02"
     const val androidx_fragment = "1.2.0-beta02"
     const val androidx_navigation = "2.2.0-beta01"
     const val androidx_recyclerview = "1.1.0-beta04"
