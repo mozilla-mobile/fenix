4aae84bf77a72d6d5f19f6a8c1d4a1b1f08df67c
Kate Glazko <katglazko@gmail.com>
2019-12-04 12:40:49 -0800

    changes

--- app/src/test/java/org/mozilla/fenix/ext/SessionTest.kt
+++ app/src/test/java/org/mozilla/fenix/ext/SessionTest.kt
@@ -4,25 +4,35 @@
 
 package org.mozilla.fenix.ext
 
-import org.junit.Assert.assertEquals
+import androidx.navigation.fragment.NavHostFragment
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.spyk
+import mozilla.components.support.test.robolectric.testContext
+import org.mozilla.fenix.TestApplication
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mozilla.fenix.TestApplication
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import mozilla.components.support.test.robolectric.testContext
+import org.junit.Assert.assertEquals
 import mozilla.components.browser.session.Session
-import mozilla.components.feature.media.state.MediaState
 import org.mozilla.fenix.home.sessioncontrol.Tab
+import java.util.UUID
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = TestApplication::class)
-class SessionTest() {
+
+class SessionTest {
+    val id: String = "c3ba04cb-e5ee-4768-b40c-159595205554"
+    val url: String = "https://www.mozilla.org/en-US/"
+    val urlTrimmedToHost:String = "mozilla.org"
 
     @Test
     fun `GIVEN session WHEN converting to tab THEN send back a correctly-populated tab`() {
-        val session = Session("https://www.mozilla.org/en-US/")
-        val tabToCompare = Tab(session.id, session.url, session.url.urlToTrimmedHost(testContext), session.title, null, null, session.icon)
+        mockkStatic(UUID::class)
+        every { UUID.randomUUID().toString() } returns "7777"
+        val session = spyk(Session("https://www.mozilla.org/en-US/"))
+        val tabToCompare = Tab("7777", url, urlTrimmedToHost, "", null, null, null)
         val tabResult = session.toTab(testContext, null, null)
         assertEquals(tabToCompare, tabResult)
     }
