88c25c43da5b021f82fd19607ba703ad1d7ab20e
David Walsh <davidwalsh83@gmail.com>
2019-12-04 08:32:25 -0600

    WIP - Close all

--- app/src/main/java/org/mozilla/fenix/tabtray/TabTrayFragment.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayFragment.kt
@@ -11,29 +11,44 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_tab_tray.view.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import mozilla.components.browser.session.Session
 import mozilla.components.browser.session.SessionManager
 import mozilla.components.concept.engine.prompt.ShareData
+import mozilla.components.feature.media.ext.getSession
+import mozilla.components.feature.media.state.MediaStateMachine
 import mozilla.components.lib.state.ext.consumeFrom
 import org.mozilla.fenix.HomeActivity
 
 import org.mozilla.fenix.R
 import org.mozilla.fenix.components.StoreProvider
+import org.mozilla.fenix.components.metrics.Event
 import org.mozilla.fenix.ext.logDebug
 import org.mozilla.fenix.ext.nav
 import org.mozilla.fenix.ext.requireComponents
 import org.mozilla.fenix.ext.sessionsOfType
+import org.mozilla.fenix.ext.toTab
 import org.mozilla.fenix.home.BrowserSessionsObserver
 import org.mozilla.fenix.home.HomeFragmentDirections
+import org.mozilla.fenix.home.sessioncontrol.SessionControlChange
+import org.mozilla.fenix.mvi.getManagedEmitter
+import org.mozilla.fenix.utils.allowUndo
 
 
 class TabTrayFragment : Fragment(), TabTrayInteractor {
 
     private lateinit var tabTrayView: TabTrayView
     private lateinit var tabTrayStore: TabTrayFragmentStore
+
+    private var pendingSessionDeletion: PendingSessionDeletion? = null
+    data class PendingSessionDeletion(val deletionJob: (suspend () -> Unit), val sessionId: String)
+
+    var deleteAllSessionsJob: (suspend () -> Unit)? = null
 
     private val sessionManager: SessionManager
         get() = requireComponents.core.sessionManager
@@ -123,7 +138,25 @@ class TabTrayFragment : Fragment(), TabTrayInteractor {
                 true
             }
             R.id.close_menu_item -> {
-                logDebug("davidwalsh", "CLOSE!")
+                val private = (activity as HomeActivity).browsingModeManager.mode.isPrivate
+                if (pendingSessionDeletion?.deletionJob == null) {
+                    removeAllTabsWithUndo(
+                        sessionManager.sessionsOfType(private = private),
+                        private
+                    )
+                } else {
+                    pendingSessionDeletion?.deletionJob?.let {
+                        viewLifecycleOwner.lifecycleScope.launch {
+                            it.invoke()
+                        }.invokeOnCompletion {
+                            pendingSessionDeletion = null
+                            removeAllTabsWithUndo(
+                                sessionManager.sessionsOfType(private = private),
+                                private
+                            )
+                        }
+                    }
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -150,5 +183,61 @@ class TabTrayFragment : Fragment(), TabTrayInteractor {
         tabTrayStore.dispatch(
             TabTrayFragmentAction.DeselectTab(item)
         )
+    }
+
+    private fun removeAllTabsWithUndo(listOfSessionsToDelete: Sequence<Session>, private: Boolean) {
+        val sessionManager = requireComponents.core.sessionManager
+
+        getManagedEmitter<SessionControlChange>().onNext(SessionControlChange.TabsChange(listOf()))
+
+        val deleteOperation: (suspend () -> Unit) = {
+            listOfSessionsToDelete.forEach {
+                sessionManager.remove(it)
+            }
+        }
+        deleteAllSessionsJob = deleteOperation
+
+        val snackbarMessage = if (private) {
+            getString(R.string.snackbar_private_tabs_closed)
+        } else {
+            getString(R.string.snackbar_tabs_closed)
+        }
+
+        viewLifecycleOwner.lifecycleScope.allowUndo(
+            view!!,
+            snackbarMessage,
+            getString(R.string.snackbar_deleted_undo), {
+                if (private) {
+                    requireComponents.analytics.metrics.track(Event.PrivateBrowsingSnackbarUndoTapped)
+                }
+                deleteAllSessionsJob = null
+                emitSessionChanges()
+            },
+            operation = deleteOperation,
+            anchorView = bottom_bar
+        )
+    }
+
+    private fun emitSessionChanges() {
+        getManagedEmitter<SessionControlChange>().onNext(
+            SessionControlChange.TabsChange(
+                getListOfSessions().toTabs()
+            )
+        )
+    }
+
+    private fun List<Session>.toTabs(): List<org.mozilla.fenix.home.sessioncontrol.Tab> {
+        val selected = sessionManager.selectedSession
+        val mediaStateSession = MediaStateMachine.state.getSession()
+
+        return this.map {
+            val mediaState = if (mediaStateSession?.id == it.id) {
+                MediaStateMachine.state
+            } else {
+                null
+            }
+
+            it.toTab(requireContext(), it == selected, mediaState)
+        }
     }
 }
