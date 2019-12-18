31247898b4b0a5a88e16681e7ef19cb5d192b21f
Jeff Boek <jeff@jeffboek.com>
2019-12-18 12:27:05 -0800

    For #1063 - Refactors TabTrayFragment to use an interactor and controller

---
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayController.kt
@@ -0,0 +1,102 @@
+package org.mozilla.fenix.tabtray
+
+import androidx.navigation.NavController
+import mozilla.components.browser.session.Session
+import mozilla.components.browser.session.SessionManager
+import mozilla.components.feature.media.ext.pauseIfPlaying
+import mozilla.components.feature.media.ext.playIfPaused
+import mozilla.components.feature.media.state.MediaStateMachine
+import org.mozilla.fenix.R
+import org.mozilla.fenix.browser.browsingmode.BrowsingMode
+import org.mozilla.fenix.browser.browsingmode.BrowsingModeManager
+import org.mozilla.fenix.collections.SaveCollectionStep
+import org.mozilla.fenix.ext.sessionsOfType
+
+interface TabTrayController {
+    fun closeTab(tab: Tab)
+    fun closeAllTabs()
+    fun pauseMedia()
+    fun playMedia()
+    fun openTab(tab: Tab)
+    fun selectTab(tab: Tab)
+    fun deselectTab(tab: Tab)
+    fun navigateToCollectionCreator()
+    fun newTab()
+    fun enterPrivateBrowsingMode()
+    fun exitPrivateBrowsingMode()
+}
+
+class DefaultTabTrayController(
+    private val navController: NavController,
+    private val sessionManager: SessionManager,
+    private val tabTrayFragmentStore: TabTrayFragmentStore,
+    private val browsingModeManager: BrowsingModeManager,
+    private val tabCloser: (Sequence<Session>, Boolean) -> Unit,
+    private val onModeChange: (BrowsingMode) -> Unit
+): TabTrayController {
+    override fun enterPrivateBrowsingMode() {
+        val newMode = BrowsingMode.Private
+
+        browsingModeManager.mode = newMode
+        onModeChange(newMode)
+    }
+
+    override fun exitPrivateBrowsingMode() {
+        val newMode = BrowsingMode.Normal
+
+        browsingModeManager.mode = newMode
+        onModeChange(newMode)
+    }
+
+    override fun newTab() {
+        val directions = TabTrayFragmentDirections.actionTabTrayFragmentToSearchFragment(null)
+        navController.navigate(directions)
+    }
+
+    override fun navigateToCollectionCreator() {
+        if (navController.currentDestination?.id == R.id.collectionCreationFragment) return
+        if (tabTrayFragmentStore.state.mode is TabTrayFragmentState.Mode.Normal) return
+
+        val tabIds = tabTrayFragmentStore.state.mode.selectedTabs.map { it.sessionId }.toTypedArray()
+
+
+        val directions = TabTrayFragmentDirections.actionTabTrayFragmentToCreateCollectionFragment(
+            tabIds = tabIds,
+            previousFragmentId = R.id.tabTrayFragment,
+            saveCollectionStep = SaveCollectionStep.SelectCollection,
+            selectedTabIds = tabIds,
+            selectedTabCollectionId = -1
+        )
+
+        navController.navigate(directions)
+    }
+
+    override fun closeTab(tab: Tab) {
+        val sessionToDelete = sessionManager.findSessionById(tab.sessionId) ?: return
+        tabCloser(sequenceOf(sessionToDelete), browsingModeManager.mode.isPrivate)
+    }
+
+    override fun closeAllTabs() {
+        val isPrivate = browsingModeManager.mode.isPrivate
+        val tabs = sessionManager.sessionsOfType(isPrivate)
+        tabCloser(tabs, isPrivate)
+    }
+
+    override fun pauseMedia() { MediaStateMachine.state.pauseIfPlaying() }
+    override fun playMedia() { MediaStateMachine.state.playIfPaused() }
+
+    override fun openTab(tab: Tab) {
+        val session = sessionManager.findSessionById(tab.sessionId) ?: return
+        sessionManager.select(session)
+        val directions = TabTrayFragmentDirections.actionTabTrayFragmentToBrowserFragment(null)
+        navController.navigate(directions)
+    }
+
+    override fun selectTab(tab: Tab) {
+        tabTrayFragmentStore.dispatch(TabTrayFragmentAction.SelectTab(tab))
+    }
+
+    override fun deselectTab(tab: Tab) {
+        tabTrayFragmentStore.dispatch(TabTrayFragmentAction.DeselectTab(tab))
+    }
+}
--- app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
@@ -10,14 +10,17 @@ import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.core.view.isVisible
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.component_tab_tray.view.*
-import kotlinx.android.synthetic.main.tab_list_row.*
 import kotlinx.android.synthetic.main.tab_tray_list_item.view.*
 import mozilla.components.feature.media.state.MediaState
 import org.mozilla.fenix.R
+import org.mozilla.fenix.browser.browsingmode.BrowsingMode
+import org.mozilla.fenix.components.FenixSnackbar
 import org.mozilla.fenix.components.metrics.Event
 import org.mozilla.fenix.components.ui.SelectableListItemView
 import org.mozilla.fenix.components.ui.SelectionHolder
@@ -25,16 +28,21 @@ import org.mozilla.fenix.components.ui.SelectionInteractor
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.increaseTapArea
 import org.mozilla.fenix.library.LibraryPageView
+import org.mozilla.fenix.utils.allowUndo
 
-interface TabTrayInteractor : SelectionInteractor<Tab> {
+interface TabTrayViewInteractor : SelectionInteractor<Tab> {
     fun closeButtonTapped(tab: Tab)
     fun onPauseMediaClicked()
     fun onPlayMediaClicked()
+    fun privateModeButtonTapped()
+    fun normalModeButtonTapped()
+    fun closeAllTabsTapped()
+    fun newTabTapped()
 }
 
 class TabItemViewHolder(
     private val view: SelectableListItemView,
-    private val interactor: TabTrayInteractor,
+    private val interactor: TabTrayViewInteractor,
     private val selectionHolder: SelectionHolder<Tab>
 ) : RecyclerView.ViewHolder(view) {
     private var tab: Tab? = null
@@ -103,7 +111,7 @@ class TabItemViewHolder(
 }
 
 class TabTrayAdapter(
-    private val interactor: TabTrayInteractor
+    private val interactor: TabTrayViewInteractor
 ) : RecyclerView.Adapter<TabItemViewHolder>(), SelectionHolder<Tab> {
     private var state = TabTrayFragmentState(listOf(), TabTrayFragmentState.Mode.Normal)
 
@@ -132,10 +140,10 @@ class TabTrayAdapter(
 
 class TabTrayView(
     val container: ViewGroup,
-    val interactor: TabTrayInteractor
+    val interactor: TabTrayViewInteractor
 ) : LibraryPageView(container), LayoutContainer {
-
-    val tabTrayAdapter = TabTrayAdapter(interactor)
+    private val tabTrayAdapter = TabTrayAdapter(interactor)
+    private var snackbar: FenixSnackbar? = null
 
     val view: View = LayoutInflater.from(container.context)
         .inflate(R.layout.component_tab_tray, container, true)
@@ -145,9 +153,44 @@ class TabTrayView(
             adapter = tabTrayAdapter
             (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
         }
+
+        view.private_browsing_button.setOnClickListener { interactor.privateModeButtonTapped() }
+        view.tab_tray_close_all.setOnClickListener { interactor.closeAllTabsTapped() }
+        view.tab_tray_open_new_tab.setOnClickListener { interactor.newTabTapped() }
     }
 
-    fun update(state: TabTrayFragmentState) {
+    fun update(state: TabTrayFragmentState, browsingMode: BrowsingMode) {
         tabTrayAdapter.updateState(state)
+        view.tab_tray_controls.isVisible = !state.mode.isEditing
+        view.tab_tray_empty_view.isVisible = state.tabs.isEmpty()
+        if (state.tabs.isEmpty()) {
+            view.announceForAccessibility(view.context.getString(R.string.no_open_tabs_description))
+        }
+
+        view.private_browsing_button.setOnClickListener {
+            if (browsingMode.isPrivate) {
+                interactor.normalModeButtonTapped()
+            } else {
+                interactor.privateModeButtonTapped()
+            }
+        }
+    }
+
+    fun showUndoSnackbar(
+        message: String,
+        onCompletion: suspend () -> Unit,
+        onUndo: suspend () -> Unit,
+        lifecycleOwner: LifecycleOwner) {
+        snackbar = lifecycleOwner.lifecycleScope.allowUndo(
+            view,
+            message,
+            view.context.getString(R.string.snackbar_deleted_undo),
+            onUndo,
+            onCompletion
+        )
+    }
+
+    fun hideSnackbar() {
+        snackbar?.dismiss()
     }
 }
