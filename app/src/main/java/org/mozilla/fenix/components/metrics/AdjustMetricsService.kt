1f5bfff0257166c22125c60eec06cc9d9d3830b4
Sebastian Kaspari <s.kaspari@gmail.com>
2020-01-10 13:52:27 +0100

    Issue #6898: Allow empty adjust token for fennecNightly builds.

--- app/src/main/java/org/mozilla/fenix/components/metrics/AdjustMetricsService.kt
+++ app/src/main/java/org/mozilla/fenix/components/metrics/AdjustMetricsService.kt
@@ -13,14 +13,15 @@ import com.adjust.sdk.AdjustConfig
 import com.adjust.sdk.LogLevel
 import org.mozilla.fenix.BuildConfig
 import org.mozilla.fenix.Config
+import org.mozilla.fenix.ReleaseChannel
 import org.mozilla.fenix.ext.settings
 
 class AdjustMetricsService(private val application: Application) : MetricsService {
     override fun start() {
-        if ((BuildConfig.ADJUST_TOKEN.isNullOrEmpty())) {
+        if ((BuildConfig.ADJUST_TOKEN.isNullOrBlank())) {
             Log.i(LOGTAG, "No adjust token defined")
 
-            if (Config.channel.isReleased) {
+            if (Config.channel.isReleased && Config.channel != ReleaseChannel.FennecNightly) {
                 throw IllegalStateException("No adjust token defined for release build")
             }
 
