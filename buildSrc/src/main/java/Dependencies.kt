dd701233f5d9e58dd6cce1e23a308d101362ba0b
Vlad Filippov <vlad.filippov@gmail.com>
2020-01-13 13:53:56 -0500

    Bump a-s dependency to 0.48.2
    Changelog: https://github.com/mozilla/application-services/releases/tag/v0.48.2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -38,7 +38,7 @@ object Versions {
     // that we depend on directly for the fenix-megazord (and for it's
     // forUnitTest variant), and it's important that it be kept in
     // sync with the version used by android-components above.
-    const val mozilla_appservices = "0.48.1"
+    const val mozilla_appservices = "0.48.2"
 
     const val mozilla_glean = "23.0.0"
 
