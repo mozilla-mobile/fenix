7f81b62e7703655e4f20a51e225b3c82f2a34c8e
Jonathan Almeida <jalmeida@mozilla.com>
2020-01-15 00:59:08 -0500

    For #7092: Add initial Migration UI

--- app/src/migration/java/org/mozilla/fenix/MigrationService.kt
+++ app/src/migration/java/org/mozilla/fenix/MigrationService.kt
@@ -6,11 +6,12 @@ package org.mozilla.fenix
 
 import mozilla.components.support.migration.AbstractMigrationService
 import mozilla.components.support.migration.state.MigrationStore
+import org.mozilla.fenix.ext.components
 
 /**
  * Background service for running the migration from legacy Firefox for Android (Fennec).
  */
 class MigrationService : AbstractMigrationService() {
     override val migrator by lazy { getMigratorFromApplication() }
-    override val store: MigrationStore by lazy { getMigrationStoreFromApplication() }
+    override val store: MigrationStore by lazy { components.migrationStore }
 }
