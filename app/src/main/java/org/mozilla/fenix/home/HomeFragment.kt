01ccb9a9ea7449a83e37b2bc3525cba1dfecb28d
mcarare <mihai.carare.dev@gmail.com>
2020-01-16 12:37:23 +0200

    For #7029: Navigate to Search Fragment when pressing Start browsing.

--- app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -187,7 +187,7 @@ class HomeFragment : Fragment() {
                 closeTab = ::closeTab,
                 closeAllTabs = ::closeAllTabs,
                 getListOfTabs = ::getListOfTabs,
-                hideOnboarding = ::hideOnboarding,
+                hideOnboarding = ::hideOnboardingAndOpenSearch,
                 invokePendingDeleteJobs = ::invokePendingDeleteJobs,
                 registerCollectionStorageObserver = ::registerCollectionStorageObserver,
                 scrollToTheTop = ::scrollToTheTop,
@@ -279,10 +279,7 @@ class HomeFragment : Fragment() {
         view.add_tab_button.setOnClickListener {
             invokePendingDeleteJobs()
             hideOnboardingIfNeeded()
-            val directions = HomeFragmentDirections.actionHomeFragmentToSearchFragment(
-                sessionId = null
-            )
-            nav(R.id.homeFragment, directions)
+            navigateToSearch()
         }
 
         PrivateBrowsingButtonView(
@@ -481,13 +478,23 @@ class HomeFragment : Fragment() {
     }
 
     private fun hideOnboardingIfNeeded() {
-        if (!onboarding.userHasBeenOnboarded()) hideOnboarding()
+        if (!onboarding.userHasBeenOnboarded()) {
+            onboarding.finish()
+            homeFragmentStore.dispatch(
+                HomeFragmentAction.ModeChange(currentMode.getCurrentMode()))
+        }
     }
 
-    private fun hideOnboarding() {
-        onboarding.finish()
-        homeFragmentStore.dispatch(
-            HomeFragmentAction.ModeChange(currentMode.getCurrentMode()))
+    private fun hideOnboardingAndOpenSearch() {
+        hideOnboardingIfNeeded()
+        navigateToSearch()
+    }
+
+    private fun navigateToSearch() {
+        val directions = HomeFragmentDirections.actionHomeFragmentToSearchFragment(
+            sessionId = null
+        )
+        nav(R.id.homeFragment, directions)
     }
 
     private fun setupHomeMenu() {
