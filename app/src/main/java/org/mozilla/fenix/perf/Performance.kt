01e08467059ea609d35d0062aae5aea9f28b081f
Michael Comella <michael.l.comella@gmail.com>
2020-01-16 16:58:12 -0800

    For perf-frontend-issues#50: instrument imperfect reportFullyDrawn.
    We don't capture the second RV draw yet. Unfortunately, I don't know if
    it's the best use of my time to capture that as well.

--- app/src/main/java/org/mozilla/fenix/perf/Performance.kt
+++ app/src/main/java/org/mozilla/fenix/perf/Performance.kt
@@ -4,9 +4,37 @@
 
 package org.mozilla.fenix.perf
 
+import androidx.core.view.doOnPreDraw
+import kotlinx.android.synthetic.main.activity_home.*
+import org.mozilla.fenix.HomeActivity
+
 /**
  * A collection of objects related to app performance.
  */
 object Performance {
     const val TAG = "FenixPerf"
+
+    /**
+     * Instruments cold startup time for use with our internal measuring system, FNPRMS. This may
+     * also appear in Google Play Vitals dashboards.
+     *
+     * This will need to be rewritten if any parts of the UI are changed to be displayed
+     * asynchronously.
+     *
+     * In the current implementation, we only intend to instrument cold startup to the homescreen.
+     * To save implementation time, we ignore the fact that the RecyclerView draws twice if the user
+     * has tabs, collections, etc. open: the "No tabs" placeholder and a tab list. This
+     * instrumentation will only capture the "No tabs" draw.
+     */
+    fun instrumentColdStartupToHomescreenTime(activity: HomeActivity) {
+        // For greater accuracy, we could add an onDrawListener instead of a preDrawListener but:
+        // - single use onDrawListeners are not built-in and it's non-trivial to write one
+        // - the difference is timing is minimal (< 7ms on Pixel 2)
+        // - if we compare against another app using a preDrawListener, it should be comparable
+        //
+        // Unfortunately, this is tightly coupled to the root view of HomeActivity's view hierarchy
+        activity.rootContainer.doOnPreDraw {
+            activity.reportFullyDrawn()
+        }
+    }
 }
