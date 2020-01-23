55b85912d4ca2625ede6c88ce5397aa56a1e3af7
Arturo Mejia <arturomejiamarmol@gmail.com>
2020-01-22 22:27:34 -0500

    Use the AddonsManagerAdapter class from AC (#7872)

--- app/src/main/java/org/mozilla/fenix/addons/AddonsManagementFragment.kt
+++ app/src/main/java/org/mozilla/fenix/addons/AddonsManagementFragment.kt
@@ -8,12 +8,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.StringRes
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,15 +17,14 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_add_ons_management.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import mozilla.components.feature.addons.Addon
 import mozilla.components.feature.addons.AddonManagerException
+import mozilla.components.feature.addons.ui.AddonsManagerAdapter
+import mozilla.components.feature.addons.ui.AddonsManagerAdapterDelegate
 import mozilla.components.feature.addons.ui.PermissionsDialogFragment
 import mozilla.components.feature.addons.ui.translate
 import org.mozilla.fenix.R
-import org.mozilla.fenix.addons.AddonsManagementFragment.CustomViewHolder.AddonViewHolder
-import org.mozilla.fenix.addons.AddonsManagementFragment.CustomViewHolder.SectionViewHolder
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.showToolbar
 
@@ -37,7 +32,7 @@ import org.mozilla.fenix.ext.showToolbar
  * Fragment use for managing add-ons.
  */
 @Suppress("TooManyFunctions")
-class AddonsManagementFragment : Fragment(), View.OnClickListener {
+class AddonsManagementFragment : Fragment(), AddonsManagerAdapterDelegate {
     private lateinit var recyclerView: RecyclerView
     private val scope = CoroutineScope(Dispatchers.IO)
 
@@ -66,6 +61,18 @@ class AddonsManagementFragment : Fragment(), View.OnClickListener {
         }
     }
 
+    override fun onAddonItemClicked(addon: Addon) {
+        if (addon.isInstalled()) {
+            showInstalledAddonDetailsFragment(addon)
+        } else {
+            showDetailsFragment(addon)
+        }
+    }
+
+    override fun onInstallAddonButtonClicked(addon: Addon) {
+        showPermissionDialog(addon)
+    }
+
     private fun bindRecyclerView(view: View) {
         recyclerView = view.findViewById(R.id.add_ons_list)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -74,7 +81,8 @@ class AddonsManagementFragment : Fragment(), View.OnClickListener {
                 val addons = requireContext().components.addonManager.getAddons()
 
                 scope.launch(Dispatchers.Main) {
-                    val adapter = AddonsAdapter(
+                    val adapter = AddonsManagerAdapter(
+                        requireContext().components.addonCollectionProvider,
                         this@AddonsManagementFragment,
                         addons
                     )
@@ -84,181 +92,6 @@ class AddonsManagementFragment : Fragment(), View.OnClickListener {
                 scope.launch(Dispatchers.Main) {
                     Toast.makeText(activity, "Failed to query Add-ons!", Toast.LENGTH_SHORT).show()
                 }
-            }
-        }
-    }
-
-    /**
-     * An adapter for displaying add-on items.
-     */
-    inner class AddonsAdapter(
-        private val clickListener: View.OnClickListener,
-        addons: List<Addon>
-    ) : RecyclerView.Adapter<CustomViewHolder>() {
-        private val items: List<Any>
-
-        init {
-            items = createListWithSections(addons)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-            return if (viewType == VIEW_HOLDER_TYPE_ADDON) {
-                createAddonViewHolder(parent)
-            } else {
-                createSectionViewHolder(parent)
-            }
-        }
-
-        private fun createSectionViewHolder(parent: ViewGroup): CustomViewHolder {
-            val context = parent.context
-            val inflater = LayoutInflater.from(context)
-            val view = inflater.inflate(R.layout.addons_section_item, parent, false)
-            val titleView = view.findViewById<TextView>(R.id.title)
-
-            return SectionViewHolder(view, titleView)
-        }
-
-        private fun createAddonViewHolder(parent: ViewGroup): AddonViewHolder {
-            val context = parent.context
-            val inflater = LayoutInflater.from(context)
-            val view = inflater.inflate(R.layout.add_ons_item, parent, false)
-            val iconView = view.findViewById<ImageView>(R.id.add_on_icon)
-            val titleView = view.findViewById<TextView>(R.id.add_on_name)
-            val summaryView = view.findViewById<TextView>(R.id.add_on_description)
-            val ratingView = view.findViewById<RatingBar>(R.id.rating)
-            val userCountView = view.findViewById<TextView>(R.id.users_count)
-            val addButton = view.findViewById<ImageView>(R.id.add_button)
-            return AddonViewHolder(
-                view,
-                iconView,
-                titleView,
-                summaryView,
-                ratingView,
-                userCountView,
-                addButton
-            )
-        }
-
-        override fun getItemCount() = items.size
-
-        override fun getItemViewType(position: Int): Int {
-            val isSection = items[position] !is Addon
-            return if (isSection) VIEW_HOLDER_TYPE_SECTION else VIEW_HOLDER_TYPE_ADDON
-        }
-
-        override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-            val item = items[position]
-
-            when (holder) {
-                is SectionViewHolder -> bindSection(holder, item as Section)
-                is AddonViewHolder -> bindAddon(holder, item as Addon)
-            }
-        }
-
-        private fun bindSection(holder: SectionViewHolder, section: Section) {
-            holder.titleView.setText(section.title)
-        }
-
-        private fun bindAddon(holder: AddonViewHolder, addon: Addon) {
-            val context = holder.itemView.context
-            addon.rating?.let {
-                val userCount = context.getString(R.string.mozac_feature_addons_user_rating_count)
-                val ratingContentDescription =
-                    context.getString(R.string.mozac_feature_addons_rating_content_description)
-                holder.ratingView.contentDescription =
-                    String.format(ratingContentDescription, it.average)
-                holder.ratingView.rating = it.average
-                holder.userCountView.text = String.format(userCount, getFormattedAmount(it.reviews))
-            }
-
-            holder.titleView.text = addon.translatableName.translate()
-            holder.summaryView.text = addon.translatableSummary.translate()
-            holder.itemView.tag = addon
-            holder.itemView.setOnClickListener(clickListener)
-            holder.addButton.isVisible = !addon.isInstalled()
-            val listener = if (!addon.isInstalled()) {
-                clickListener
-            } else {
-                null
-            }
-            holder.addButton.setOnClickListener(listener)
-
-            scope.launch {
-                val iconBitmap = context.components.addonCollectionProvider.getAddonIconBitmap(addon)
-
-                iconBitmap?.let {
-                    MainScope().launch {
-                        holder.iconView.setImageBitmap(it)
-                    }
-                }
-            }
-        }
-
-        private fun createListWithSections(addons: List<Addon>): List<Any> {
-            // We want to have the installed add-ons first in the list.
-            val sortedAddons = addons.sortedBy { !it.isInstalled() }
-
-            val itemsWithSections = ArrayList<Any>()
-            val shouldAddInstalledSection = sortedAddons.first().isInstalled()
-            var isRecommendedSectionAdded = false
-
-            if (shouldAddInstalledSection) {
-                itemsWithSections.add(Section(R.string.mozac_feature_addons_installed_section))
-            }
-
-            sortedAddons.forEach { addon ->
-                if (!isRecommendedSectionAdded && !addon.isInstalled()) {
-                    itemsWithSections.add(Section(R.string.mozac_feature_addons_recommended_section))
-                    isRecommendedSectionAdded = true
-                }
-
-                itemsWithSections.add(addon)
-            }
-            return itemsWithSections
-        }
-    }
-
-    /**
-     * A base view holder.
-     */
-    sealed class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        /**
-         * A view holder for displaying section items.
-         */
-        class SectionViewHolder(
-            view: View,
-            val titleView: TextView
-        ) : CustomViewHolder(view)
-
-        /**
-         * A view holder for displaying add-on items.
-         */
-        class AddonViewHolder(
-            view: View,
-            val iconView: ImageView,
-            val titleView: TextView,
-            val summaryView: TextView,
-            val ratingView: RatingBar,
-            val userCountView: TextView,
-            val addButton: ImageView
-        ) : CustomViewHolder(view)
-    }
-
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.add_button -> {
-                val addon = (((view.parent) as View).tag as Addon)
-                showPermissionDialog(addon)
-            }
-            R.id.add_on_item -> {
-                val addon = view.tag as Addon
-                if (addon.isInstalled()) {
-                    showInstalledAddonDetailsFragment(addon)
-                } else {
-                    showDetailsFragment(addon)
-                }
-            }
-            else -> {
             }
         }
     }
