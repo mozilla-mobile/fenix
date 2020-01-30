e8f40a2c1d26a48bfe3e4347f84ed23e46dc7535
Gabriel Luong <gabriel.luong@gmail.com>
2020-01-29 21:45:01 -0500

    Add KDoc for the Extensions helper functions

--- app/src/main/java/org/mozilla/fenix/addons/Extensions.kt
+++ app/src/main/java/org/mozilla/fenix/addons/Extensions.kt
@@ -9,10 +9,21 @@ import org.mozilla.fenix.components.FenixSnackbar
 import java.text.NumberFormat
 import java.util.Locale
 
+/**
+ * Get the formatted number amount for the current default locale.
+ *
+ * @param amount The number of addons to be formatted for the current default locale..
+ */
 internal fun getFormattedAmount(amount: Int): String {
     return NumberFormat.getNumberInstance(Locale.getDefault()).format(amount)
 }
 
+/**
+ * Shows the Fenix Snackbar in the given view along with the provided text.
+ *
+ * @param view A [View] used to determine a parent for the [FenixSnackbar].
+ * @param text The text to display in the [FenixSnackbar].
+ */
 internal fun showSnackBar(view: View, text: String) {
     FenixSnackbar.make(view, FenixSnackbar.LENGTH_SHORT)
         .setText(text)
