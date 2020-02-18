c5913932cbc11dd58a2f72f5fea9a9dd5b2afa3e
Andrew Gaul <andrew@gaul.org>
2020-02-18 05:51:45 -0800

    Upgrade to detekt 1.6.0
    Changelog:
    
    https://arturbosch.github.io/detekt/changelog.html
    
    Fixes #7360.  Also use more exact test glob.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -11,7 +11,7 @@ object Versions {
     const val leanplum = "5.2.3"
     const val osslicenses_plugin = "0.9.5"
     const val osslicenses_library = "17.0.0"
-    const val detekt = "1.0.0-RC16"
+    const val detekt = "1.6.0"
 
     const val androidx_appcompat = "1.2.0-alpha03"
     const val androidx_biometric = "1.0.1"
