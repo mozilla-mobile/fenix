3469b8dce846a63b56c1a27da814f3c852a234a7
mcarare <mihai.carare.dev@gmail.com>
2020-02-20 13:45:39 +0200

    For #8565: Use custom updater to correctly update DropDownPreference

--- app/src/main/java/org/mozilla/fenix/settings/DropDownListPreference.kt
+++ app/src/main/java/org/mozilla/fenix/settings/DropDownListPreference.kt
@@ -7,6 +7,7 @@ package org.mozilla.fenix.settings
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.preference.DropDownPreference
 import org.mozilla.fenix.R
 
@@ -21,5 +22,15 @@ class DropDownListPreference @JvmOverloads constructor(
 
     override fun createAdapter(): ArrayAdapter<Any> {
         return ArrayAdapter(context, R.layout.etp_dropdown_item)
+    }
+}
+
+fun DropDownListPreference.findEntriesValue(newValue: Any?): CharSequence? {
+    val newValueString = newValue as? String ?: return null
+    val index = this.findIndexOfValue(newValueString)
+    return if (index != Spinner.INVALID_POSITION) {
+        this.entries[index] ?: null
+    } else {
+        null
     }
 }
