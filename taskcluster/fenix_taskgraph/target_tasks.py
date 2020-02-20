5e0516d8c7f7bac09d2b8fb9c6a276b41a83e714
Mihai Tabara <mtabara@mozilla.com>
2020-02-20 14:01:34 +0000

    Let fennec-nightly ride along vanilla fenix-nightlies

--- taskcluster/fenix_taskgraph/target_tasks.py
+++ taskcluster/fenix_taskgraph/target_tasks.py
@@ -30,23 +30,13 @@ def target_tasks_nightly(full_task_graph, parameters, graph_config):
     """Select the set of tasks required for a nightly build."""
 
     def filter(task, parameters):
-        return (
-            task.attributes.get("nightly", False) and
-            not _filter_fennec("nightly", task, parameters)
-        )
+        return task.attributes.get("nightly", False)
 
     return [l for l, t in full_task_graph.tasks.iteritems() if filter(t, parameters)]
 
 
 def _filter_fennec(fennec_type, task, parameters):
     return task.attributes.get("build-type", "") == "fennec-{}".format(fennec_type)
-
-
-@_target_task("fennec-nightly")
-def target_tasks_fennec_nightly(full_task_graph, parameters, graph_config):
-    """Select the set of tasks required for a nightly build signed with the fennec key."""
-
-    return [l for l, t in full_task_graph.tasks.iteritems() if _filter_fennec("nightly", t, parameters)]
 
 
 @_target_task("fennec-beta")
