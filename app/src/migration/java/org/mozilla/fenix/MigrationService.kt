680249f724b6d86d673dee7e4f193c09692ef5d4
Sparky93 <andrei.a.lazar528@gmail.com>
2020-02-25 14:00:02 +0200

    Fix for #8722 - Update complete notification is not routing to onboarding screen
    Now handling migration completed and clear states as routes to home screen.

--- app/src/migration/java/org/mozilla/fenix/MigrationService.kt
+++ app/src/migration/java/org/mozilla/fenix/MigrationService.kt
@@ -7,7 +7,6 @@ package org.mozilla.fenix
 import mozilla.components.support.migration.AbstractMigrationService
 import mozilla.components.support.migration.state.MigrationStore
 import org.mozilla.fenix.ext.components
-import org.mozilla.fenix.migration.MigrationProgressActivity
 
 /**
  * Background service for running the migration from legacy Firefox for Android (Fennec).
@@ -15,5 +14,5 @@ import org.mozilla.fenix.migration.MigrationProgressActivity
 class MigrationService : AbstractMigrationService() {
     override val migrator by lazy { getMigratorFromApplication() }
     override val store: MigrationStore by lazy { components.migrationStore }
-    override val migrationActivity = MigrationProgressActivity::class.java
+    override val migrationActivity = MigrationDecisionActivity::class.java
 }
