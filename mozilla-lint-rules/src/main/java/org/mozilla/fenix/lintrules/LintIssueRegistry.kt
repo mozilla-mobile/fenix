414e905b7673ff15f27fd2ed13334b6a9f45ca17
ekager <ekager@mozilla.com>
2020-03-03 10:44:54 -0800

    Closes #8944 - Adds custom lint checks

---
+++ mozilla-lint-rules/src/main/java/org/mozilla/fenix/lintrules/LintIssueRegistry.kt
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.lintrules
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.Issue
+
+/**
+ * Registry which provides a list of our custom lint checks to be performed on an Android project.
+ */
+@Suppress("unused")
+class LintIssueRegistry : IssueRegistry() {
+    override val api: Int = com.android.tools.lint.detector.api.CURRENT_API
+    override val issues: List<Issue> = listOf(
+        AndroidSrcXmlDetector.ISSUE_XML_SRC_USAGE,
+        TextViewAndroidSrcXmlDetector.ISSUE_XML_SRC_USAGE,
+        ImageViewAndroidTintXmlDetector.ISSUE_XML_SRC_USAGE
+    )
+}
