4701ee88af0c6ad8bca4b84e71cdb9c27b03aad4
Severin Rudie <srudie@mozilla.com>
2020-03-10 20:00:27 -0700

    For #9091: fix test failure

--- app/src/test/java/org/mozilla/fenix/components/TestCore.kt
+++ app/src/test/java/org/mozilla/fenix/components/TestCore.kt
@@ -5,16 +5,20 @@
 package org.mozilla.fenix.components
 
 import android.content.Context
+import io.mockk.every
 import io.mockk.mockk
-import mozilla.components.browser.engine.gecko.GeckoEngine
 import mozilla.components.browser.session.SessionManager
 import mozilla.components.browser.state.store.BrowserStore
+import mozilla.components.concept.engine.Engine
+import mozilla.components.concept.engine.Settings
 import mozilla.components.concept.fetch.Client
 import mozilla.components.feature.pwa.WebAppShortcutManager
 
 class TestCore(context: Context) : Core(context) {
 
-    override val engine = mockk<GeckoEngine>(relaxed = true)
+    override val engine = mockk<Engine>(relaxed = true) {
+        every { this@mockk getProperty "settings" } returns mockk<Settings>()
+    }
     override val sessionManager = SessionManager(engine)
     override val store = mockk<BrowserStore>()
     override val client = mockk<Client>()
