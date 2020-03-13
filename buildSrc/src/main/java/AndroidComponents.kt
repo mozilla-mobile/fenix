8ddba69b9447376b615e27e1286dfe749dc3944f
Sebastian Kaspari <s.kaspari@gmail.com>
2020-03-13 15:39:55 +0100

    Use pinned Android Components Nightly version instead of snapshots.

---
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -0,0 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+object AndroidComponents {
+    const val VERSION = "37.0.20200316130158"
+}
+
