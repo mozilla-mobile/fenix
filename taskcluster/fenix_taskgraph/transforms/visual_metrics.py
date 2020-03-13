6c21f9f3431d1a24c054bd67d74bbfcfc0c01270
gmierz <gmierz1@live.ca>
2020-03-13 11:31:11 -0400

    Use spaces instead of tabs, and resolve visual-metric nits.

--- taskcluster/fenix_taskgraph/transforms/visual_metrics.py
+++ taskcluster/fenix_taskgraph/transforms/visual_metrics.py
@@ -13,72 +13,74 @@ from taskgraph.transforms.base import TransformSequence
 
 transforms = TransformSequence()
 
-SYMBOL = "%(groupSymbol)s(%(symbol)s-vismet)"
+SYMBOL = "{groupSymbol}({symbol}-vismet)"
 # the test- prefix makes the task SETA-optimized.
-LABEL = "test-vismet-%(platform)s-%(label)s"
+LABEL = "test-vismet-{platform}-{label}"
 
 
 @transforms.add
 def make_label(config, jobs):
-	""" Generate a sane label for a new task constructed from a dependency
-	Using attributes from the dependent job and the current task kind"""
-	for job in jobs:
-		dep_job = job['primary-dependency']
-		attr = dep_job.attributes.get
+    """ Generate a sane label for a new task constructed from a dependency
+    Using attributes from the dependent job and the current task kind"""
+    for job in jobs:
+        dep_job = job['primary-dependency']
+        attr = dep_job.attributes.get
 
-		if attr('locale', job.get('locale')):
-			template = "{kind}-{locale}-{build_platform}/{build_type}"
-		elif attr('l10n_chunk'):
-			template = "{kind}-{build_platform}-{l10n_chunk}/{build_type}"
-		elif config.kind.startswith("release-eme-free") or \
-				config.kind.startswith("release-partner-repack"):
-			suffix = job.get("extra", {}).get("repack_suffix", None) or \
-					 job.get("extra", {}).get("repack_id", None)
-			template = "{kind}-{build_platform}"
-			if suffix:
-				template += "-{}".format(suffix.replace('/', '-'))
-		else:
-			template = "{kind}-{build_platform}/{build_type}"
-		job['label'] = template.format(
-			kind=config.kind,
-			build_platform=attr('build_platform'),
-			build_type=attr('build_type'),
-			locale=attr('locale', job.get('locale', '')),  # Locale can be absent
-			l10n_chunk=attr('l10n_chunk', '')  # Can be empty
-		)
+        if attr('locale', job.get('locale')):
+            template = "{kind}-{locale}-{build_platform}/{build_type}"
+        elif attr('l10n_chunk'):
+            template = "{kind}-{build_platform}-{l10n_chunk}/{build_type}"
+        elif config.kind.startswith("release-eme-free") or \
+                config.kind.startswith("release-partner-repack"):
+            suffix = job.get("extra", {}).get("repack_suffix", None) or \
+                     job.get("extra", {}).get("repack_id", None)
+            template = "{kind}-{build_platform}"
+            if suffix:
+                template += "-{}".format(suffix.replace('/', '-'))
+        else:
+            template = "{kind}-{build_platform}/{build_type}"
+        job['label'] = template.format(
+            kind=config.kind,
+            build_platform=attr('build_platform'),
+            build_type=attr('build_type'),
+            locale=attr('locale', job.get('locale', '')),  # Locale can be absent
+            l10n_chunk=attr('l10n_chunk', '')  # Can be empty
+        )
 
-		yield job
+        yield job
 
 
 @transforms.add
 def run_visual_metrics(config, jobs):
-	for job in jobs:
-		dep_job = job.pop('primary-dependency', None)
-		if dep_job is not None:
-			platform = dep_job.task['extra']['treeherder-platform']
-			job['dependencies'] = {dep_job.label: dep_job.label}
+    for job in jobs:
+        dep_job = job.pop('primary-dependency', None)
+        if dep_job is not None:
+            platform = dep_job.task['extra']['treeherder-platform']
+            job['dependencies'] = {dep_job.label: dep_job.label}
 
-			# Add the artifact to be processed as a fetches artifact
-			job['fetches'][dep_job.label] = []
-			job['fetches'][dep_job.label].append({
-				'artifact': 'browsertime-results.tgz',
-				'extract': True
-			})
+            # Add the artifact to be processed as a fetches artifact
+            job['fetches'][dep_job.label] = [{
+                'artifact': 'browsertime-results.tgz',
+                'extract': True
+            }]
 
-			# Set the artifact prefix for the browsertime results
-			job.setdefault('attributes', {})
-			job['attributes']['artifact_prefix'] = 'public/test_info'
+            # Set the artifact prefix for the browsertime results
+            job.setdefault('attributes', {})
+            job['attributes']['artifact_prefix'] = 'public/test_info'
 
-			# vismet runs on Linux but we want to have it displayed
-			# alongside the job it was triggered by to make it easier for
-			# people to find it back.
-			job['label'] = LABEL % {'platform': platform, 'label': dep_job.label}
-			treeherder_info = dict(dep_job.task['extra']['treeherder'])
-			job['treeherder']['symbol'] = SYMBOL % treeherder_info
-			job['treeherder']['platform'] = platform
+            # vismet runs on Linux but we want to have it displayed
+            # alongside the job it was triggered by to make it easier for
+            # people to find it back.
+            job['label'] = LABEL.format(platform=platform, label=dep_job.label)
+            treeherder_info = dict(dep_job.task['extra']['treeherder'])
+            job['treeherder']['platform'] = platform
+            job['treeherder']['symbol'] = SYMBOL.format(
+                groupSymbol=treeherder_info['groupSymbol'],
+                symbol=treeherder_info['symbol']
+            )
 
-			# run-on-projects needs to be set based on the dependent task
-			attributes = dict(dep_job.attributes)
-			job['run-on-projects'] = attributes['run_on_projects']
+            # run-on-projects needs to be set based on the dependent task
+            attributes = dict(dep_job.attributes)
+            job['run-on-projects'] = attributes['run_on_projects']
 
-			yield job
+            yield job
