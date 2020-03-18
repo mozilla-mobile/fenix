9f3bbf1fb7da7fb93f67d51a00cd48c0ea35caed
Mugurell <Mugurell@users.noreply.github.com>
2020-03-18 18:12:04 +0200

    For #8651 - Add HistoryInteractor/Controller unit/ui tests

---
+++ app/src/test/java/org/mozilla/fenix/library/history/HistoryInteractorTest.kt
@@ -0,0 +1,124 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.library.history
+
+import assertk.assertThat
+import assertk.assertions.isTrue
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verifyAll
+import org.junit.Test
+import org.mozilla.fenix.browser.browsingmode.BrowsingMode
+
+class HistoryInteractorTest {
+    private val historyItem = HistoryItem(0, "title", "url", 0.toLong())
+    val controller: HistoryController = mockk(relaxed = true)
+    val interactor = HistoryInteractor(controller)
+
+    @Test
+    fun onOpen() {
+        interactor.open(historyItem)
+
+        verifyAll {
+            controller.handleOpen(historyItem)
+        }
+    }
+
+    @Test
+    fun onSelect() {
+        interactor.select(historyItem)
+
+        verifyAll {
+            controller.handleSelect(historyItem)
+        }
+    }
+
+    @Test
+    fun onDeselect() {
+        interactor.deselect(historyItem)
+
+        verifyAll {
+            controller.handleDeselect(historyItem)
+        }
+    }
+
+    @Test
+    fun onBackPressed() {
+        every {
+            controller.handleBackPressed()
+        } returns true
+
+        val backpressHandled = interactor.onBackPressed()
+
+        verifyAll {
+            controller.handleBackPressed()
+        }
+        assertThat(backpressHandled).isTrue()
+    }
+
+    @Test
+    fun onModeSwitched() {
+        interactor.onModeSwitched()
+
+        verifyAll {
+            controller.handleModeSwitched()
+        }
+    }
+
+    @Test
+    fun onCopyPressed() {
+        interactor.onCopyPressed(historyItem)
+
+        verifyAll {
+            controller.handleCopyUrl(historyItem)
+        }
+    }
+
+    @Test
+    fun onSharePressed() {
+        interactor.onSharePressed(historyItem)
+
+        verifyAll {
+            controller.handleShare(historyItem)
+        }
+    }
+
+    @Test
+    fun onOpenInNormalTab() {
+        interactor.onOpenInNormalTab(historyItem)
+
+        verifyAll {
+            controller.handleOpen(historyItem, BrowsingMode.Normal)
+        }
+    }
+
+    @Test
+    fun onOpenInPrivateTab() {
+        interactor.onOpenInPrivateTab(historyItem)
+
+        verifyAll {
+            controller.handleOpen(historyItem, BrowsingMode.Private)
+        }
+    }
+
+    @Test
+    fun onDeleteAll() {
+        interactor.onDeleteAll()
+
+        verifyAll {
+            controller.handleDeleteAll()
+        }
+    }
+
+    @Test
+    fun onDeleteSome() {
+        val items = setOf(historyItem)
+
+        interactor.onDeleteSome(items)
+        verifyAll {
+            controller.handleDeleteSome(items)
+        }
+    }
+}
