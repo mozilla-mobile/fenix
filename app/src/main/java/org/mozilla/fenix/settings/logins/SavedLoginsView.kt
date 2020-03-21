38a97cd1e07127171c4f9d06abf27e45a1f0d5fe
mcarare <mihai.carare.dev@gmail.com>
2020-03-21 12:48:54 +0200

    For #9252: Add progress bar to saved logins view

--- app/src/main/java/org/mozilla/fenix/settings/logins/SavedLoginsView.kt
+++ app/src/main/java/org/mozilla/fenix/settings/logins/SavedLoginsView.kt
@@ -14,6 +14,7 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.component_saved_logins.view.*
+import kotlinx.android.synthetic.main.component_saved_logins.view.progress_bar
 import org.mozilla.fenix.R
 
 /**
@@ -69,7 +70,10 @@ class SavedLoginsView(
     }
 
     fun update(state: SavedLoginsFragmentState) {
-        if (!state.isLoading) {
+        if (state.isLoading) {
+            view.progress_bar.isVisible = true
+        } else {
+            view.progress_bar.isVisible = false
             view.saved_logins_list.isVisible = state.items.isNotEmpty()
             view.saved_passwords_empty_view.isVisible = state.items.isEmpty()
         }
