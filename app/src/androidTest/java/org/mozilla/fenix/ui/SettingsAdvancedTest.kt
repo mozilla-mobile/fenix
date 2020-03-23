677a9da5eca17524c1ce4e206f90f7174f744a84
mcarare <mihai.carare.dev@gmail.com>
2020-03-23 18:48:46 +0200

    For #8485 Update UI tests after settings reorg

---
+++ app/src/androidTest/java/org/mozilla/fenix/ui/SettingsAdvancedTest.kt
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.ui
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.Rule
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import org.mozilla.fenix.helpers.AndroidAssetDispatcher
+import org.mozilla.fenix.helpers.HomeActivityIntentTestRule
+import org.mozilla.fenix.ui.robots.homeScreen
+
+/**
+ *  Tests for verifying the advanced section in Settings
+ *
+ */
+
+class SettingsAdvancedTest {
+    /* ktlint-disable no-blank-line-before-rbrace */ // This imposes unreadable grouping.
+
+    private val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    private lateinit var mockWebServer: MockWebServer
+
+    @get:Rule
+    val activityIntentTestRule = HomeActivityIntentTestRule()
+
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer().apply {
+            setDispatcher(AndroidAssetDispatcher())
+            start()
+        }
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    // Walks through settings menu and sub-menus to ensure all items are present
+    fun settingsAboutItemsTest() {
+        // ADVANCED
+        homeScreen {
+        }.openThreeDotMenu {
+        }.openSettings {
+            // ADVANCED
+            verifyAdvancedHeading()
+            verifyAddons()
+            verifyRemoteDebug()
+            verifyLeakCanaryButton()
+        }
+    }
+}
