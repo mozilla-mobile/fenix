f2acef66b59d24e4943e97e3707ae9643ea35289
Michael Comella <michael.l.comella@gmail.com>
2020-04-01 12:52:32 -0700

    For #9605: remove unnecessary @RunWith(AndroidJUnit4) in unit tests.
    In unit tests, this annotation annotations defer to robolectric, non-trivially
    increasing test runtime so it's important to remove them when they're
    unnecessary.

--- app/src/test/java/org/mozilla/fenix/utils/FragmentPreDrawManagerTest.kt
+++ app/src/test/java/org/mozilla/fenix/utils/FragmentPreDrawManagerTest.kt
@@ -6,21 +6,15 @@ package org.mozilla.fenix.utils
 
 import androidx.core.view.OneShotPreDrawListener
 import androidx.fragment.app.Fragment
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mozilla.fenix.TestApplication
-import org.robolectric.annotation.Config
 
 @ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
-@Config(application = TestApplication::class)
 class FragmentPreDrawManagerTest {
     private fun doNothing() { /*noop*/ }
 
