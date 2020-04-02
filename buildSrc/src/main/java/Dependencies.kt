d90379199249b440500aacbcabc1b0edc4f2cffa
Michael Comella <michael.l.comella@gmail.com>
2020-04-02 15:38:08 -0700

    No issue: upgrade test deps to 1.3.0-alpha05.
    ReaderViewTest failed on the 1.2.0 stable release so I'm upgrading to a
    newer version (of which only alpha is available) to see if it's fixed.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -185,19 +185,20 @@ object Deps {
     //   https://developer.android.com/jetpack/androidx/releases/test
     // For the full IDs of these test dependencies, see:
     //   https://developer.android.com/training/testing/set-up-project#android-test-dependencies
-    private const val androidx_test_shared_version = "1.2.0" // this appears to be shared with many deps.
+    private const val androidx_test_shared_version = "1.3.0-alpha05" // this appears to be shared with many deps.
     const val androidx_test_core = "androidx.test:core:$androidx_test_shared_version"
-    private const val androidx_espresso_version = "3.2.0"
+    private const val androidx_espresso_version = "3.3.0-alpha05"
     const val espresso_core = "androidx.test.espresso:espresso-core:$androidx_espresso_version"
     const val espresso_contrib = "androidx.test.espresso:espresso-contrib:$androidx_espresso_version"
     const val espresso_idling_resources = "androidx.test.espresso:espresso-idling-resource:$androidx_espresso_version"
     const val espresso_intents = "androidx.test.espresso:espresso-intents:$androidx_espresso_version"
-    const val androidx_junit = "androidx.test.ext:junit:1.1.1"
+    const val androidx_junit = "androidx.test.ext:junit:1.1.2-alpha05"
     // Monitor is unused
     const val orchestrator = "androidx.test:orchestrator:$androidx_test_shared_version"
     const val tools_test_runner = "androidx.test:runner:$androidx_test_shared_version"
     const val tools_test_rules = "androidx.test:rules:$androidx_test_shared_version"
     // Truth is unused
+    // Test services is unused
     // --- END AndroidX test dependencies --- //
 
     const val mockwebserver = "com.squareup.okhttp3:mockwebserver:${Versions.mockwebserver}"
