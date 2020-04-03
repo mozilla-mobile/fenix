441c1c3ed8325ad4deeedc1433ea2725d93e632a
Sawyer Blatz <sdblatz@gmail.com>
2020-04-03 15:08:33 -0700

    Created generic mozilla button

--- app/src/main/java/org/mozilla/fenix/uicomponents/MozillaButton.kt
+++ app/src/main/java/org/mozilla/fenix/uicomponents/MozillaButton.kt
@@ -3,32 +3,60 @@ package org.mozilla.fenix.uicomponents
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
-import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.mozilla_button.view.*
 import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.increaseTapArea
+import org.mozilla.fenix.theme.ThemeManager
 
 class MozillaButton(
     context: Context,
     attrs: AttributeSet? = null
-): MaterialButton(context, attrs) {
+): FrameLayout(context, attrs) {
     var style : ButtonStyle = ButtonStyle.NEUTRAL
 
+    var textString: String? = null
+    var drawableRes: Int? = null
+
     init {
+        View.inflate(
+            context,
+            R.layout.mozilla_button,
+            this
+        )
 
-        Log.d("Sawyer", "init: $attrs")
-
-        context.withStyledAttributes(attrs, R.styleable.MozillaButton, R.style.Widget_MaterialComponents_Button_TextButton, 0) {
+        context.withStyledAttributes(attrs, R.styleable.MozillaButton, 0, 0) {
             val styleId = getInt(
-                R.styleable.MozillaButton_type,
+                R.styleable.MozillaButton_moz_button_type,
+                0
+            )
+
+            textString = getString(
+                R.styleable.MozillaButton_moz_button_text
+            )
+
+            drawableRes = getResourceId(
+                R.styleable.MozillaButton_moz_button_image,
                 0
             )
 
             setStyle(styleId)
         }
+
+        mozilla_button.increaseTapArea(12)
+
+        mozilla_button.setOnClickListener {
+            Log.d("Sawyer", "buttonClick")
+        }
     }
+
 
         //View.inflate(context, R.layout.mozilla_button, this)
 
+    // TODO: How to better map this enum?
     private fun setStyle(id: Int) {
         Log.d("Sawyer", "setting style: $id")
         style = when (id) {
@@ -38,7 +66,36 @@ class MozillaButton(
             else -> ButtonStyle.NEUTRAL
         }
 
+        updateButtonStyles()
+
         // Set certain layouts based on that
+    }
+
+    private fun updateButtonStyles() {
+        when (style) {
+            ButtonStyle.NEUTRAL -> {
+                mozilla_button.backgroundTintList =  ContextCompat.getColorStateList(
+                    context,
+                    R.color.grey_button_color
+                )
+            }
+            ButtonStyle.POSITIVE -> {
+                mozilla_button.backgroundTintList =  ContextCompat.getColorStateList(
+                    context,
+                    ThemeManager.resolveAttribute(R.attr.accent, context)
+                )
+            }
+            ButtonStyle.DESTRUCTIVE -> {
+                mozilla_button.backgroundTintList =  ContextCompat.getColorStateList(
+                    context,
+                    R.color.grey_button_color
+                )
+            }
+        }
+
+        mozilla_button.text = textString
+
+        mozilla_button.icon = (ContextCompat.getDrawable(context, drawableRes!!))
     }
 
 
