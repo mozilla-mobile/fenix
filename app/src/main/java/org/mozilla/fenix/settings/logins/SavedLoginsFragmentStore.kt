24ba9f2fc8691ebde8b6a9e178b172bc83434c87
ValentinTimisica <valentin.timisica@softvision.ro>
2020-04-08 15:21:33 +0300

    For #9505: Adds possibility to sort saved logins
    Currently we support sorting by name and by last used. Also, the selected
    option is saved in shared preferences so that the last option chosen by
    the user is properly displayed even after the app was restarted.

--- app/src/main/java/org/mozilla/fenix/settings/logins/SavedLoginsFragmentStore.kt
+++ app/src/main/java/org/mozilla/fenix/settings/logins/SavedLoginsFragmentStore.kt
@@ -15,13 +15,16 @@ import mozilla.components.lib.state.Store
  * @property url Site of the saved login
  * @property userName Username that's saved for this site
  * @property password Password that's saved for this site
+ * @property id The unique identifier for this login entry
+ * @property timeLastUsed Time of last use in milliseconds from the unix epoch.
  */
 @Parcelize
 data class SavedLoginsItem(
     val url: String,
     val userName: String?,
     val password: String?,
-    val id: String
+    val id: String,
+    val timeLastUsed: Long
 ) :
     Parcelable
 
@@ -40,17 +43,25 @@ class SavedLoginsFragmentStore(initialState: SavedLoginsFragmentState) :
 sealed class SavedLoginsFragmentAction : Action {
     data class FilterLogins(val newText: String?) : SavedLoginsFragmentAction()
     data class UpdateLogins(val list: List<SavedLoginsItem>) : SavedLoginsFragmentAction()
+    data class SortLogins(val sortingStrategy: SortingStrategy) : SavedLoginsFragmentAction()
 }
 
 /**
  * The state for the Saved Logins Screen
+ * @property isLoading State to know when to show loading
  * @property items Source of truth of list of logins
- * @property items Filtered (or not) list of logins to display
+ * @property filteredItems Filtered (or not) list of logins to display
+ * @property searchedForText String used by the user to filter logins
+ * @property sortingStrategy sorting strategy selected by the user (Currently we support
+ * sorting alphabetically and by last used)
  */
 data class SavedLoginsFragmentState(
     val isLoading: Boolean = false,
     val items: List<SavedLoginsItem>,
-    val filteredItems: List<SavedLoginsItem>
+    val filteredItems: List<SavedLoginsItem>,
+    val searchedForText: String?,
+    val sortingStrategy: SortingStrategy,
+    val highlightedItem: SavedLoginsSortingStrategyMenu.Item
 ) : State
 
 /**
@@ -61,21 +72,75 @@ private fun savedLoginsStateReducer(
     action: SavedLoginsFragmentAction
 ): SavedLoginsFragmentState {
     return when (action) {
-        is SavedLoginsFragmentAction.UpdateLogins -> state.copy(
+        is SavedLoginsFragmentAction.UpdateLogins -> {
+            filterItems(
+                state.searchedForText, state.sortingStrategy, state.copy(
+                    isLoading = false,
+                    items = action.list,
+                    filteredItems = emptyList()
+                )
+            )
+        }
+        is SavedLoginsFragmentAction.FilterLogins ->
+            filterItems(
+                action.newText,
+                state.sortingStrategy,
+                state
+            )
+        is SavedLoginsFragmentAction.SortLogins ->
+            filterItems(
+                state.searchedForText,
+                action.sortingStrategy,
+                state
+            )
+    }
+}
+
+/**
+ * @return [SavedLoginsFragmentState] containing a new [SavedLoginsFragmentState.filteredItems]
+ * with filtered [SavedLoginsFragmentState.items]
+ *
+ * @param searchedForText based on which [SavedLoginsFragmentState.items] will be filtered.
+ * @param sortingStrategy based on which [SavedLoginsFragmentState.items] will be sorted.
+ * @param state previous [SavedLoginsFragmentState] containing all the other properties
+ * with which a new state will be created
+ */
+private fun filterItems(
+    searchedForText: String?,
+    sortingStrategy: SortingStrategy,
+    state: SavedLoginsFragmentState
+): SavedLoginsFragmentState {
+    return if (searchedForText.isNullOrBlank()) {
+        state.copy(
             isLoading = false,
-            items = action.list,
-            filteredItems = action.list
+            sortingStrategy = sortingStrategy,
+            highlightedItem = sortingStrategyToMenuItem(sortingStrategy),
+            searchedForText = searchedForText,
+            filteredItems = sortingStrategy(state.items)
         )
-        is SavedLoginsFragmentAction.FilterLogins -> {
-            if (action.newText.isNullOrBlank()) {
-                state.copy(
-                    isLoading = false,
-                    filteredItems = state.items)
-            } else {
-                state.copy(
-                    isLoading = false,
-                    filteredItems = state.items.filter { it.url.contains(action.newText) })
+    } else {
+        state.copy(
+            isLoading = false,
+            sortingStrategy = sortingStrategy,
+            highlightedItem = sortingStrategyToMenuItem(sortingStrategy),
+            searchedForText = searchedForText,
+            filteredItems = sortingStrategy(state.items).filter {
+                it.url.contains(
+                    searchedForText
+                )
             }
+        )
+    }
+}
+
+private fun sortingStrategyToMenuItem(sortingStrategy: SortingStrategy): SavedLoginsSortingStrategyMenu.Item {
+    return when (sortingStrategy) {
+        is SortingStrategy.Alphabetically -> {
+            SavedLoginsSortingStrategyMenu.Item.AlphabeticallySort
+        }
+
+        is SortingStrategy.LastUsed -> {
+            SavedLoginsSortingStrategyMenu.Item.LastUsedSort
         }
     }
 }
