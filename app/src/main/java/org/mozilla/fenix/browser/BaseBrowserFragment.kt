e7bea421ffdb44e1da791ada042fc17f69a66ae9
ekager <ekager@mozilla.com>
2020-04-13 12:08:02 -0700

    For #9703 - Exit full screen on pause if pip not entered

--- app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
+++ app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
@@ -135,6 +135,7 @@ abstract class BaseBrowserFragment : Fragment(), UserInteractionHandler, Session
 
     // We need this so we don't accidentally remove all external sessions on back press
     private var sessionRemoved = false
+    private var enteredPip = false
 
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
@@ -574,6 +575,11 @@ abstract class BaseBrowserFragment : Fragment(), UserInteractionHandler, Session
     @CallSuper
     final override fun onPause() {
         super.onPause()
+        // If we didn't enter PiP, exit full screen on pause
+        if (!enteredPip) {
+            fullScreenFeature.onBackPressed()
+        }
+        enteredPip = false
         if (findNavController().currentDestination?.id != R.id.searchFragment) {
             view?.hideKeyboard()
         }
@@ -798,6 +804,7 @@ abstract class BaseBrowserFragment : Fragment(), UserInteractionHandler, Session
 
     override fun onHomePressed(): Boolean {
         if (pipFeature?.onHomePressed() == true) {
+            enteredPip = true
             return true
         }
         return false
