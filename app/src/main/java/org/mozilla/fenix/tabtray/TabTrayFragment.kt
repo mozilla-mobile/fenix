f472d424a4078775f170cb279890bc4a4b0f343b
David Walsh <davidwalsh83@gmail.com>
2020-04-14 13:11:40 -0500

    Progress

--- app/src/main/java/org/mozilla/fenix/tabtray/TabTrayFragment.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayFragment.kt
@@ -6,17 +6,24 @@ package org.mozilla.fenix.tabtray
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
 
 @SuppressWarnings("TooManyFunctions", "LargeClass")
 class TabTrayFragment : Fragment() {
 
+    var tabTrayMenu: Menu? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        updateTitle()
 
         // More
     }
@@ -28,12 +35,41 @@ class TabTrayFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_tab_tray, container, false)
 
+        updateTitle()
+
         return view
     }
 
     override fun onResume() {
         super.onResume()
+        updateTitle()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.tab_tray_menu, menu)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        this.tabTrayMenu = menu
+        updateMenuItems()
+    }
+
+    private fun updateTitle() {
+        // TODO:  Why isn't this displaying?
         activity?.title = requireContext().getString(R.string.tab_header_label)
         (activity as AppCompatActivity).supportActionBar?.show()
+    }
+
+    private fun updateMenuItems() {
+        val inPrivateMode = (activity as HomeActivity).browsingModeManager.mode.isPrivate
+
+        // Hide all icons when in selection mode with nothing selected
+
+        // TODO:  Use store
+        val showAnyOverflowIcons = true // tabTrayStore.state.tabs.isNotEmpty()
+        this.tabTrayMenu?.findItem(R.id.tab_tray_select_to_save_menu_item)?.isVisible =
+            showAnyOverflowIcons && !inPrivateMode
+        this.tabTrayMenu?.findItem(R.id.tab_tray_share_menu_item)?.isVisible = showAnyOverflowIcons
+        this.tabTrayMenu?.findItem(R.id.tab_tray_close_menu_item)?.isVisible = showAnyOverflowIcons
     }
 }
