c805b29cda3157c80d86619f0f7aeb1a6b6f1a95
isnirp <pramankwah@gmail.com>
2020-04-15 22:48:12 +0200

    site permissions exceptions robot

---
+++ app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsSubMenuSitePermissionsExceptionsRobot.kt
@@ -0,0 +1,34 @@
+package org.mozilla.fenix.ui.robots
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import org.hamcrest.CoreMatchers
+import org.mozilla.fenix.R
+import org.mozilla.fenix.helpers.click
+
+class SettingsSubMenuSitePermissionsExceptionsRobot {
+    fun verifyEmptyMessage() = assertEmptyMessage()
+
+    class Transition {
+        fun goBack(interact: SettingsSubMenuSitePermissionsRobot.() -> Unit): SettingsSubMenuSitePermissionsRobot.Transition {
+            goBackButton().click()
+
+            SettingsSubMenuSitePermissionsRobot().interact()
+            return SettingsSubMenuSitePermissionsRobot.Transition()
+        }
+    }
+}
+
+private fun goBackButton() =
+    onView(CoreMatchers.allOf(ViewMatchers.withContentDescription("Navigate up")))
+
+private fun assertEmptyMessage() = onView(withId(R.id.empty_exception_container)).check(
+    matches(
+        withText("No site exceptions")
+    )
+)
