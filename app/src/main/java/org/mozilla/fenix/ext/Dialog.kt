48064fb478dfc4030fcd2c8e87e3f5827edafcc4
mcarare <mihai.carare.dev@gmail.com>
2020-04-16 20:34:40 +0300

    For #9987: Add extension to secures dialog if parent activity is secured

---
+++ app/src/main/java/org/mozilla/fenix/ext/Dialog.kt
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.ext
+
+import android.app.Activity
+import android.app.Dialog
+import android.view.WindowManager
+
+/**
+ * Checks if activity's window has a FLAG_SECURE set and sets it to dialog
+ */
+fun Dialog.secure(activity: Activity?) {
+    this.window.apply {
+        val flags = activity?.window?.attributes?.flags
+        if (flags != null && flags and WindowManager.LayoutParams.FLAG_SECURE != 0) {
+            this?.setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE
+            )
+        }
+    }
+}
