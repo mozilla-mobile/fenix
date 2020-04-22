4fc21ef4176c8f7ea06c467560df41e2f803e952
Arturo Mejia <arturomejiamarmol@gmail.com>
2020-04-22 10:50:26 -0400

    Update Android Components version

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "39.0.20200421130115"
+    const val VERSION = "39.0.20200422131146"
 }
