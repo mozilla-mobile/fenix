7993f1017d696520f2631023544b19fa27c87479
Christian Sadilek <christian.sadilek@gmail.com>
2020-05-12 18:17:39 -0400

    Migrate feature-accounts to browser-state

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "41.0.20200513130106"
+    const val VERSION = "42.0.20200513162226"
 }
