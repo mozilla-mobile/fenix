011a9e7d96f088684098266140d0e140727947ff
Sawyer Blatz <sdblatz@gmail.com>
2020-05-14 10:47:13 -0700

    For #1216: Add voice search

--- app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
+++ app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
@@ -33,7 +33,6 @@ import mozilla.components.browser.session.Session
 import mozilla.components.browser.session.SessionManager
 import mozilla.components.browser.session.runWithSessionIdOrSelected
 import mozilla.components.concept.engine.prompt.ShareData
-import mozilla.components.feature.accounts.FxaCapability
 import mozilla.components.feature.accounts.FxaWebChannelFeature
 import mozilla.components.feature.app.links.AppLinksFeature
 import mozilla.components.feature.contextmenu.ContextMenuCandidate
@@ -482,7 +481,8 @@ abstract class BaseBrowserFragment : Fragment(), UserInteractionHandler, Session
                 view.swipeRefresh.setOnChildScrollUpCallback { _, _ -> true }
             }
 
-            @Suppress("ConstantConditionIf")
+            // @Suppress("ConstantConditionIf")
+            /*
             if (!FeatureFlags.asFeatureWebChannelsDisabled) {
                 webchannelIntegration.set(
                     feature = FxaWebChannelFeature(
@@ -498,6 +498,8 @@ abstract class BaseBrowserFragment : Fragment(), UserInteractionHandler, Session
                     view = view
                 )
             }
+
+             */
 
             initializeEngineView(toolbarHeight)
         }
