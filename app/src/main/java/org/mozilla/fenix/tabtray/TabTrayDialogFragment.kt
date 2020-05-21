85a0806da6eff31a3c82f121c3283333e185fbed
Jeff Boek <jeff@jeffboek.com>
2020-05-20 20:20:29 -0700

    For 10504 - Adds dialog style and fab

--- app/src/main/java/org/mozilla/fenix/tabtray/TabTrayDialogFragment.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayDialogFragment.kt
@@ -4,8 +4,34 @@
 
 package org.mozilla.fenix.tabtray
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.view.updatePadding
+import kotlinx.android.synthetic.main.fragment_tab_tray_dialog.view.*
+import org.mozilla.fenix.R
 
 class TabTrayDialogFragment : AppCompatDialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.TabTrayDialogStyle)
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_tab_tray_dialog, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.tabLayout.setOnApplyWindowInsetsListener { v, insets ->
+            v.updatePadding(
+                bottom = v.paddingBottom + insets.systemWindowInsetBottom
+            )
+            insets
+        }
+    }
 }
