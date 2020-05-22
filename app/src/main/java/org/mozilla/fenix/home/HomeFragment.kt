f94c13df3562f9fa4d440b48f679dbaa7f57dbb3
Sawyer Blatz <sdblatz@gmail.com>
2020-05-22 16:06:48 -0700

    For #9488: Add search widget CFR

--- app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -26,6 +26,7 @@ import androidx.constraintlayout.widget.ConstraintSet.PARENT_ID
 import androidx.constraintlayout.widget.ConstraintSet.TOP
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.doOnLayout
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
@@ -78,6 +79,7 @@ import org.mozilla.fenix.addons.runIfFragmentIsAttached
 import org.mozilla.fenix.browser.BrowserAnimator.Companion.getToolbarNavOptions
 import org.mozilla.fenix.browser.browsingmode.BrowsingMode
 import org.mozilla.fenix.collections.SaveCollectionStep
+import org.mozilla.fenix.cfr.SearchWidgetCFR
 import org.mozilla.fenix.components.FenixSnackbar
 import org.mozilla.fenix.components.PrivateShortcutCreateManager
 import org.mozilla.fenix.components.StoreProvider
@@ -378,6 +380,14 @@ class HomeFragment : Fragment() {
                 homeFragmentStore.dispatch(
                     HomeFragmentAction.ModeChange(Mode.fromBrowsingMode(newMode))
                 )
+            }
+        }
+
+        // We call this onLayout so that the bottom bar width is correctly set for us to center
+        // the CFR in.
+        view.toolbar_wrapper.doOnLayout {
+            if (!browsingModeManager.mode.isPrivate) {
+                SearchWidgetCFR(view.context) { view.toolbar_wrapper }.displayIfNecessary()
             }
         }
     }
