546ab633a0dda7fb1a31fb2a7fccd358b364040c
Christian Sadilek <christian.sadilek@gmail.com>
2020-05-27 15:44:54 -0400

    Update Android Components version to 44.0.20200528190114
    Pass store to download manager and service to address API changes.

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "44.0.20200528130107"
+    const val VERSION = "44.0.20200528190114"
 }
