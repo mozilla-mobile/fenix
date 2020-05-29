3946ec11de16034c4f3df09a7714a44a19c99dd0
Jeff Boek <jeff@jeffboek.com>
2020-05-29 15:09:12 -0700

    Polish homescreen (#11065)
    * For #10505 - Adjusts wordmark margins
    
    * For #10505 - Removes topsite header, fixes collections header size and removes divider
    
    * For #10505 - Restyle the top site items on the homescreen

--- app/src/main/java/org/mozilla/fenix/home/sessioncontrol/SessionControlAdapter.kt
+++ app/src/main/java/org/mozilla/fenix/home/sessioncontrol/SessionControlAdapter.kt
@@ -30,7 +30,6 @@ import org.mozilla.fenix.home.sessioncontrol.viewholders.SaveTabGroupViewHolder
 import org.mozilla.fenix.home.sessioncontrol.viewholders.TabHeaderViewHolder
 import org.mozilla.fenix.home.sessioncontrol.viewholders.TabInCollectionViewHolder
 import org.mozilla.fenix.home.sessioncontrol.viewholders.TabViewHolder
-import org.mozilla.fenix.home.sessioncontrol.viewholders.TopSiteHeaderViewHolder
 import org.mozilla.fenix.home.sessioncontrol.viewholders.TopSiteViewHolder
 import org.mozilla.fenix.home.sessioncontrol.viewholders.onboarding.OnboardingAutomaticSignInViewHolder
 import org.mozilla.fenix.home.sessioncontrol.viewholders.onboarding.OnboardingFinishViewHolder
@@ -75,7 +74,6 @@ sealed class AdapterItem(@LayoutRes val viewType: Int) {
         }
     }
 
-    object TopSiteHeader : AdapterItem(TopSiteHeaderViewHolder.LAYOUT_ID)
     data class TopSiteList(val topSites: List<TopSite>) : AdapterItem(TopSiteViewHolder.LAYOUT_ID)
 
     object SaveTabGroup : AdapterItem(SaveTabGroupViewHolder.LAYOUT_ID)
@@ -170,7 +168,6 @@ class SessionControlAdapter(
         return when (viewType) {
             ButtonTipViewHolder.LAYOUT_ID -> ButtonTipViewHolder(view, interactor)
             TabHeaderViewHolder.LAYOUT_ID -> TabHeaderViewHolder(view, interactor)
-            TopSiteHeaderViewHolder.LAYOUT_ID -> TopSiteHeaderViewHolder(view)
             TabViewHolder.LAYOUT_ID -> TabViewHolder(view, interactor)
             TopSiteViewHolder.LAYOUT_ID -> TopSiteViewHolder(view, interactor)
             SaveTabGroupViewHolder.LAYOUT_ID -> SaveTabGroupViewHolder(view, interactor)
