2837f42b7d8db439753b7f2155ca32f77c0416f1
Tiger Oakes <toakes@mozilla.com>
2020-06-15 16:36:46 -0700

    Add tests for addon details

--- app/src/main/java/org/mozilla/fenix/addons/AddonDetailsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/addons/AddonDetailsFragment.kt
@@ -7,13 +7,10 @@ package org.mozilla.fenix.addons
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
 import android.view.View
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
-import kotlinx.android.synthetic.main.fragment_add_on_details.view.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
@@ -21,108 +18,35 @@ import kotlinx.coroutines.withContext
 import mozilla.components.feature.addons.Addon
 import mozilla.components.feature.addons.ui.showInformationDialog
 import mozilla.components.feature.addons.ui.translatedName
-import mozilla.components.feature.addons.ui.translatedDescription
 import mozilla.components.feature.addons.update.DefaultAddonUpdater.UpdateAttemptStorage
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.showToolbar
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 /**
  * A fragment to show the details of an add-on.
  */
-class AddonDetailsFragment : Fragment(R.layout.fragment_add_on_details) {
-    private val updateAttemptStorage: UpdateAttemptStorage by lazy {
-        UpdateAttemptStorage(requireContext())
-    }
+class AddonDetailsFragment : Fragment(R.layout.fragment_add_on_details), AddonDetailsInteractor {
 
+    private val updateAttemptStorage by lazy { UpdateAttemptStorage(requireContext()) }
     private val args by navArgs<AddonDetailsFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bind(args.addon, view)
+
+        showToolbar(title = args.addon.translatedName)
+        AddonDetailsView(view, interactor = this).bind(args.addon)
     }
 
-    private fun bind(addon: Addon, view: View) {
-        val title = addon.translatedName
-        showToolbar(title)
-
-        bindDetails(addon, view)
-        bindAuthors(addon, view)
-        bindVersion(addon, view)
-        bindLastUpdated(addon, view)
-        bindWebsite(addon, view)
-        bindRating(addon, view)
+    override fun openWebsite(addonSiteUrl: Uri) {
+        startActivity(Intent(Intent.ACTION_VIEW, addonSiteUrl))
     }
 
-    private fun bindRating(addon: Addon, view: View) {
-        addon.rating?.let {
-            val ratingView = view.rating_view
-            val userCountView = view.users_count
-
-            val ratingContentDescription =
-                getString(R.string.mozac_feature_addons_rating_content_description)
-            ratingView.contentDescription = String.format(ratingContentDescription, it.average)
-            ratingView.rating = it.average
-
-            userCountView.text = getFormattedAmount(it.reviews)
-        }
-    }
-
-    private fun bindWebsite(addon: Addon, view: View) {
-        view.home_page_label.setOnClickListener {
-            val intent =
-                Intent(Intent.ACTION_VIEW).setData(Uri.parse(addon.siteUrl))
-            startActivity(intent)
-        }
-    }
-
-    private fun bindLastUpdated(addon: Addon, view: View) {
-        view.last_updated_text.text = formatDate(addon.updatedAt)
-    }
-
-    private fun bindVersion(addon: Addon, view: View) {
-        view.version_text.text =
-            addon.installedState?.version?.ifEmpty { addon.version } ?: addon.version
-        if (addon.isInstalled()) {
-            view.version_text.setOnLongClickListener {
-                showUpdaterDialog(addon)
-                true
+    override fun showUpdaterDialog(addon: Addon) {
+        viewLifecycleOwner.lifecycleScope.launch(Main) {
+            val updateAttempt = withContext(IO) {
+                updateAttemptStorage.findUpdateAttemptBy(addon.id)
             }
+            updateAttempt?.showInformationDialog(requireContext())
         }
-    }
-
-    private fun showUpdaterDialog(addon: Addon) {
-        viewLifecycleOwner.lifecycleScope.launch(IO) {
-            val updateAttempt = updateAttemptStorage.findUpdateAttemptBy(addon.id)
-            updateAttempt?.let {
-                withContext(Main) {
-                    it.showInformationDialog(requireContext())
-                }
-            }
-        }
-    }
-
-    private fun bindAuthors(addon: Addon, view: View) {
-        view.author_text.text = addon.authors.joinToString { author ->
-            author.name
-        }.trim()
-    }
-
-    private fun bindDetails(addon: Addon, view: View) {
-        val detailsView = view.details
-        val detailsText = addon.translatedDescription
-
-        val parsedText = detailsText.replace("\n", "<br/>")
-        val text = HtmlCompat.fromHtml(parsedText, HtmlCompat.FROM_HTML_MODE_COMPACT)
-
-        detailsView.text = text
-        detailsView.movementMethod = LinkMovementMethod.getInstance()
-    }
-
-    private fun formatDate(text: String): String {
-        val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
-        return DateFormat.getDateInstance().format(formatter.parse(text)!!)
     }
 }
