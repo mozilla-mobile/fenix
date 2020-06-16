48e5d6b1f326a697f9ef5681c9872f62db9abfe9
Hakkı Kaan Çalışkan <caliskanhkaan@gmail.com>
2020-06-16 21:13:17 +0300

    Update app/src/main/java/org/mozilla/fenix/components/FxaServer.kt
    Co-authored-by: Tiger Oakes <contact@tigeroakes.com>

--- app/src/main/java/org/mozilla/fenix/components/FxaServer.kt
+++ app/src/main/java/org/mozilla/fenix/components/FxaServer.kt
@@ -15,7 +15,7 @@ import org.mozilla.fenix.ext.settings
 object FxaServer {
     private const val CLIENT_ID = "a2270f727f45f648"
 
-    fun redirectUrl() = "urn:ietf:wg:oauth:2.0:oob:oauth-redirect-webchannel"
+    const val REDIRECT_URL = "urn:ietf:wg:oauth:2.0:oob:oauth-redirect-webchannel"
 
     fun config(context: Context): ServerConfig {
         val serverOverride = context.settings().overrideFxAServer
