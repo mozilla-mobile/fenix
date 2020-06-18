51af79b7287f30255db830f2e5ee1558db0d77cb
Marc Leclair <marc.leclair0113@gmail.com>
2020-06-17 23:25:34 -0400

    Draft for removing black screen flicker

--- app/src/migration/java/org/mozilla/fenix/MigrationDecisionActivity.kt
+++ app/src/migration/java/org/mozilla/fenix/MigrationDecisionActivity.kt
@@ -4,9 +4,8 @@
 
 package org.mozilla.fenix
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
 import mozilla.components.support.migration.state.MigrationProgress
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.migration.MigrationProgressActivity
@@ -15,30 +14,28 @@ import org.mozilla.fenix.migration.MigrationProgressActivity
  * The purpose of this activity, when launched, is to decide whether we want to show the migration
  * screen ([MigrationProgressActivity]) or launch the browser normally ([HomeActivity]).
  */
-class MigrationDecisionActivity : Activity() {
+class MigrationDecisionActivity : HomeActivity() {
     private val store by lazy { components.migrationStore }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
 
         val intent = if (intent != null) intent else Intent()
 
-        val activity = when (store.state.progress) {
-            MigrationProgress.NONE, MigrationProgress.COMPLETED -> HomeActivity::class.java
-            MigrationProgress.MIGRATING -> MigrationProgressActivity::class.java
+        // If we don't have the migrating state, we continue with our regular start up flow.
+        if (store.state.progress == MigrationProgress.MIGRATING) {
+            intent.setClass(applicationContext, MigrationProgressActivity::class.java)
+            intent.putExtra(HomeActivity.OPEN_TO_BROWSER, false)
+
+            base.startActivity(intent)
+            finish()
+
+            // We are disabling animations here when switching activities because this results in a
+            // perceived faster launch. This activity will start immediately with a solid background
+            // and then we switch to the actual activity without an animation. This visually looks like
+            // a faster start than launching this activity invisibly and switching to the actual
+            // activity after that.
+            overridePendingTransition(0, 0)
         }
-
-        intent.setClass(applicationContext, activity)
-        intent.putExtra(HomeActivity.OPEN_TO_BROWSER, false)
-
-        startActivity(intent)
-        finish()
-
-        // We are disabling animations here when switching activities because this results in a
-        // perceived faster launch. This activity will start immediately with a solid background
-        // and then we switch to the actual activity without an animation. This visually looks like
-        // a faster start than launching this activity invisibly and switching to the actual
-        // activity after that.
-        overridePendingTransition(0, 0)
     }
 }
