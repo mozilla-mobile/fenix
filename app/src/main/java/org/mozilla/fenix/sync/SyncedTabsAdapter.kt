db8fd83da4382392efe6aad1d6bfc96b45bb5653
Mihai Branescu <branescu.mihai@gmail.com>
2020-06-25 10:49:35 +0300

    For #11498 - removed SignIn ViewHolder and unified with the Error one

--- app/src/main/java/org/mozilla/fenix/sync/SyncedTabsAdapter.kt
+++ app/src/main/java/org/mozilla/fenix/sync/SyncedTabsAdapter.kt
@@ -12,7 +12,6 @@ import androidx.recyclerview.widget.ListAdapter
 import mozilla.components.browser.storage.sync.SyncedDeviceTabs
 import org.mozilla.fenix.sync.SyncedTabsViewHolder.DeviceViewHolder
 import org.mozilla.fenix.sync.SyncedTabsViewHolder.ErrorViewHolder
-import org.mozilla.fenix.sync.SyncedTabsViewHolder.SignInViewHolder
 import org.mozilla.fenix.sync.SyncedTabsViewHolder.TabViewHolder
 import mozilla.components.browser.storage.sync.Tab as SyncTab
 import mozilla.components.concept.sync.Device as SyncDevice
@@ -28,7 +27,6 @@ class SyncedTabsAdapter(
             DeviceViewHolder.LAYOUT_ID -> DeviceViewHolder(itemView)
             TabViewHolder.LAYOUT_ID -> TabViewHolder(itemView)
             ErrorViewHolder.LAYOUT_ID -> ErrorViewHolder(itemView)
-            SignInViewHolder.LAYOUT_ID -> SignInViewHolder(itemView)
             else -> throw IllegalStateException()
         }
     }
@@ -38,10 +36,9 @@ class SyncedTabsAdapter(
     }
 
     override fun getItemViewType(position: Int) = when (getItem(position)) {
-        is AdapterItem.Device -> DeviceViewHolder.LAYOUT_ID
-        is AdapterItem.Tab -> TabViewHolder.LAYOUT_ID
-        is AdapterItem.Error -> ErrorViewHolder.LAYOUT_ID
-        is AdapterItem.SignIn -> SignInViewHolder.LAYOUT_ID
+            is AdapterItem.Device -> DeviceViewHolder.LAYOUT_ID
+            is AdapterItem.Tab -> TabViewHolder.LAYOUT_ID
+            is AdapterItem.Error -> ErrorViewHolder.LAYOUT_ID
     }
 
     fun updateData(syncedTabs: List<SyncedDeviceTabs>) {
@@ -62,7 +59,7 @@ class SyncedTabsAdapter(
             when (oldItem) {
                 is AdapterItem.Device ->
                     newItem is AdapterItem.Device && oldItem.device.id == newItem.device.id
-                is AdapterItem.Tab, AdapterItem.Error, AdapterItem.SignIn ->
+                is AdapterItem.Tab, is AdapterItem.Error ->
                     oldItem == newItem
             }
 
@@ -74,7 +71,9 @@ class SyncedTabsAdapter(
     sealed class AdapterItem {
         data class Device(val device: SyncDevice) : AdapterItem()
         data class Tab(val tab: SyncTab) : AdapterItem()
-        data class SignIn(val navController: NavController) : AdapterItem()
-        data class Error(val errorResId: Int) : AdapterItem()
+        data class Error(
+            val descriptionResId: Int,
+            val navController: NavController? = null
+        ) : AdapterItem()
     }
 }
--- app/src/main/java/org/mozilla/fenix/sync/SyncedTabsLayout.kt
+++ app/src/main/java/org/mozilla/fenix/sync/SyncedTabsLayout.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.widget.FrameLayout
 import androidx.annotation.StringRes
 import androidx.fragment.app.findFragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.component_sync_tabs.view.*
@@ -19,6 +20,7 @@ import kotlinx.coroutines.launch
 import mozilla.components.browser.storage.sync.SyncedDeviceTabs
 import mozilla.components.feature.syncedtabs.view.SyncedTabsView
 import org.mozilla.fenix.R
+import java.lang.IllegalStateException
 
 class SyncedTabsLayout @JvmOverloads constructor(
     context: Context,
@@ -45,10 +47,17 @@ class SyncedTabsLayout @JvmOverloads constructor(
             // We may still be displaying a "loading" spinner, hide it.
             stopLoading()
 
-            sync_tabs_status.text = context.getText(stringResourceForError(error))
+            val navController: NavController? = try {
+                findFragment<SyncedTabsFragment>().findNavController()
+            } catch (exception: IllegalStateException) {
+                null
+            }
 
-            synced_tabs_list.visibility = View.GONE
-            sync_tabs_status.visibility = View.VISIBLE
+            val descriptionResId = stringResourceForError(error)
+            val errorItem = getErrorItem(navController, error, descriptionResId)
+
+            val errorList: List<SyncedTabsAdapter.AdapterItem> = listOf(errorItem)
+            adapter.submitList(errorList)
 
             synced_tabs_pull_to_refresh.isEnabled = pullToRefreshEnableState(error)
         }
@@ -56,17 +65,11 @@ class SyncedTabsLayout @JvmOverloads constructor(
 
     override fun displaySyncedTabs(syncedTabs: List<SyncedDeviceTabs>) {
         coroutineScope.launch {
-            synced_tabs_list.visibility = View.VISIBLE
-            sync_tabs_status.visibility = View.GONE
-
             adapter.updateData(syncedTabs)
         }
     }
 
     override fun startLoading() {
-        synced_tabs_list.visibility = View.VISIBLE
-        sync_tabs_status.visibility = View.GONE
-
         synced_tabs_pull_to_refresh.isRefreshing = true
     }
 
@@ -80,7 +83,8 @@ class SyncedTabsLayout @JvmOverloads constructor(
     }
 
     companion object {
-        internal fun pullToRefreshEnableState(error: SyncedTabsView.ErrorType) = when (error) {
+
+        private fun pullToRefreshEnableState(error: SyncedTabsView.ErrorType) = when (error) {
             // Disable "pull-to-refresh" when we clearly can't sync tabs, and user needs to take an
             // action within the app.
             SyncedTabsView.ErrorType.SYNC_UNAVAILABLE,
@@ -93,12 +97,26 @@ class SyncedTabsLayout @JvmOverloads constructor(
             SyncedTabsView.ErrorType.NO_TABS_AVAILABLE -> true
         }
 
-        internal fun stringResourceForError(error: SyncedTabsView.ErrorType) = when (error) {
+        private fun stringResourceForError(error: SyncedTabsView.ErrorType) = when (error) {
             SyncedTabsView.ErrorType.MULTIPLE_DEVICES_UNAVAILABLE -> R.string.synced_tabs_connect_another_device
             SyncedTabsView.ErrorType.SYNC_ENGINE_UNAVAILABLE -> R.string.synced_tabs_enable_tab_syncing
             SyncedTabsView.ErrorType.SYNC_UNAVAILABLE -> R.string.synced_tabs_sign_in_message
             SyncedTabsView.ErrorType.SYNC_NEEDS_REAUTHENTICATION -> R.string.synced_tabs_reauth
             SyncedTabsView.ErrorType.NO_TABS_AVAILABLE -> R.string.synced_tabs_no_tabs
+        }
+
+        private fun getErrorItem(
+            navController: NavController?,
+            error: SyncedTabsView.ErrorType,
+            @StringRes stringResId: Int
+        ): SyncedTabsAdapter.AdapterItem = when (error) {
+            SyncedTabsView.ErrorType.MULTIPLE_DEVICES_UNAVAILABLE,
+            SyncedTabsView.ErrorType.SYNC_ENGINE_UNAVAILABLE,
+            SyncedTabsView.ErrorType.SYNC_NEEDS_REAUTHENTICATION,
+            SyncedTabsView.ErrorType.NO_TABS_AVAILABLE -> SyncedTabsAdapter.AdapterItem
+                .Error(descriptionResId = stringResId)
+            SyncedTabsView.ErrorType.SYNC_UNAVAILABLE -> SyncedTabsAdapter.AdapterItem
+                .Error(descriptionResId = stringResId, navController = navController)
         }
     }
 }
