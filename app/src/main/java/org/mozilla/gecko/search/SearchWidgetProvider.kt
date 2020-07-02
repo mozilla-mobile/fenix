4d234c74f3de2d9796d01a33382feab1e1d81375
Sawyer Blatz <sdblatz@gmail.com>
2020-07-02 10:48:35 -0700

    Revert "For #11183: Redirect user to homescreen after widget added"
    This reverts commit a8ed913654ce40c7e65c14e9358c2955e6ed31a4.

--- app/src/main/java/org/mozilla/gecko/search/SearchWidgetProvider.kt
+++ app/src/main/java/org/mozilla/gecko/search/SearchWidgetProvider.kt
@@ -4,7 +4,6 @@
 
 package org.mozilla.gecko.search
 
-import android.app.ActivityManager
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH
@@ -37,21 +36,6 @@ class SearchWidgetProvider : AppWidgetProvider() {
 
     override fun onEnabled(context: Context) {
         context.settings().addSearchWidgetInstalled(1)
-        if (isAppInForeground(context)) {
-            val goHomeOnWidgetAdded = Intent(Intent.ACTION_MAIN)
-            goHomeOnWidgetAdded.addCategory(Intent.CATEGORY_HOME)
-            goHomeOnWidgetAdded.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            context.startActivity(goHomeOnWidgetAdded)
-        }
-    }
-
-    // We need this because user can not add widget via launcher app without this
-    private fun isAppInForeground(context: Context): Boolean {
-        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val runningAppProcesses =
-            activityManager.runningAppProcesses ?: return false
-        return runningAppProcesses.any { it.processName == context.packageName &&
-            it.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND }
     }
 
     override fun onDeleted(context: Context, appWidgetIds: IntArray) {
