8826f99ef7ce67b20e3e4458514860690188fc17
mcarare <mihai.carare.dev@gmail.com>
2020-07-15 16:14:29 +0300

    For #9722: Refactor onboarding buttons with text.
    Add title and description to button text, remove extra Textviews.

---
+++ app/src/main/java/org/mozilla/fenix/ext/SpannableString.kt
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.ext
+
+import android.content.Context
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
+import androidx.core.content.ContextCompat
+import mozilla.components.support.ktx.android.util.dpToPx
+
+fun SpannableString.setTextSize(context: Context, textSize: Int) =
+    this.setSpan(
+        AbsoluteSizeSpan(textSize.dpToPx(context.resources.displayMetrics)),
+        0,
+        this.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+
+fun SpannableString.setTextColor(context: Context, colorResId: Int) =
+    this.setSpan(
+        ForegroundColorSpan(
+            ContextCompat.getColor(
+                context,
+                colorResId
+            )
+        ),
+        0,
+        this.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
