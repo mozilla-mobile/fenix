13949d6968006dc78556818c662375d2dd89e51e
Tiger Oakes <toakes@mozilla.com>
2020-07-17 14:35:13 -0700

    Test migration classes (#12677)

--- app/src/main/java/org/mozilla/fenix/migration/MigrationProgressActivity.kt
+++ app/src/main/java/org/mozilla/fenix/migration/MigrationProgressActivity.kt
@@ -5,28 +5,15 @@
 package org.mozilla.fenix.migration
 
 import android.content.Intent
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.DimenRes
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_migration.*
-import kotlinx.android.synthetic.main.migration_list_item.view.*
 import mozilla.components.support.base.log.logger.Logger
 import mozilla.components.support.ktx.android.content.getColorFromAttr
 import mozilla.components.support.migration.AbstractMigrationProgressActivity
 import mozilla.components.support.migration.AbstractMigrationService
-import mozilla.components.support.migration.Migration
-import mozilla.components.support.migration.Migration.Bookmarks
-import mozilla.components.support.migration.Migration.History
-import mozilla.components.support.migration.Migration.Logins
-import mozilla.components.support.migration.Migration.Settings
 import mozilla.components.support.migration.MigrationResults
 import mozilla.components.support.migration.state.MigrationAction
 import mozilla.components.support.migration.state.MigrationProgress
@@ -97,91 +84,10 @@ class MigrationProgressActivity : AbstractMigrationProgressActivity() {
         migration_button.setBackgroundResource(R.drawable.migration_button_background)
         migration_button_progress_bar.visibility = View.INVISIBLE
         // Keep the results list up-to-date.
-        statusAdapter.submitList(results.toItemList())
+        statusAdapter.updateData(results)
     }
 
     override fun onMigrationStateChanged(progress: MigrationProgress, results: MigrationResults) {
-        statusAdapter.submitList(results.toItemList())
-    }
-}
-
-// These are the only items we want to show migrating in the UI.
-internal val whiteList = linkedMapOf(
-    Settings to R.string.settings_title,
-    History to R.string.preferences_sync_history,
-    Bookmarks to R.string.preferences_sync_bookmarks,
-    Logins to R.string.migration_text_passwords
-)
-
-internal fun MigrationResults.toItemList() = whiteList.keys
-    .map {
-        if (containsKey(it)) {
-            MigrationItem(it, getValue(it).success)
-        } else {
-            MigrationItem(it)
-        }
-    }
-
-internal data class MigrationItem(val migration: Migration, val status: Boolean = false)
-
-internal class MigrationStatusAdapter :
-    ListAdapter<MigrationItem, MigrationStatusAdapter.ViewHolder>(DiffCallback) {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
-
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
-    }
-
-    override fun getItemViewType(position: Int): Int = R.layout.migration_list_item
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val context = view.context
-        private val title = view.migration_item_name
-        private val status = view.migration_status_image
-
-        fun bind(item: MigrationItem) {
-            // Get the resource ID for the item.
-            val migrationText = whiteList[item.migration]?.run {
-                context.getString(this)
-            } ?: ""
-            title.text = migrationText
-            status.visibility = if (item.status) View.VISIBLE else View.INVISIBLE
-            status.contentDescription = context.getString(R.string.migration_icon_description)
-        }
-    }
-
-    private object DiffCallback : DiffUtil.ItemCallback<MigrationItem>() {
-
-        override fun areItemsTheSame(oldItem: MigrationItem, newItem: MigrationItem) =
-            oldItem.migration.javaClass.simpleName == newItem.migration.javaClass.simpleName
-
-        override fun areContentsTheSame(oldItem: MigrationItem, newItem: MigrationItem) =
-            oldItem.migration.javaClass.simpleName == newItem.migration.javaClass.simpleName &&
-                oldItem.status == newItem.status
-    }
-}
-
-internal class MigrationStatusItemDecoration(
-    @DimenRes private val spacing: Int
-) : RecyclerView.ItemDecoration() {
-
-    override fun getItemOffsets(
-        outRect: Rect,
-        view: View,
-        parent: RecyclerView,
-        state: RecyclerView.State
-    ) {
-        val position = parent.getChildViewHolder(view).adapterPosition
-        val itemCount = state.itemCount
-
-        outRect.left = spacing
-        outRect.right = spacing
-        outRect.top = spacing
-        outRect.bottom = if (position == itemCount - 1) spacing else 0
+        statusAdapter.updateData(results)
     }
 }
