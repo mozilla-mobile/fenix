af3d34e95a01f1c86a37b0ad7059d45aeb337fae
Jeff Boek <jeff@jeffboek.com>
2020-07-29 15:30:18 -0700

    No Issue - Updates AC

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "52.0.20200728130711"
+    const val VERSION = "53.0.20200729190533"
 }
