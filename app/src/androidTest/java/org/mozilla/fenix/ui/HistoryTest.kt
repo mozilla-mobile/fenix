1e806b25c14a97a2fd84bca6634f061f251d8e7c
Oana Horvath <oana.horvath@softvision.ro>
2020-08-04 20:33:52 +0300

    Closes #12893: fixed deleteAllHistoryTest (#13255)

--- app/src/androidTest/java/org/mozilla/fenix/ui/HistoryTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/HistoryTest.kt
@@ -11,7 +11,6 @@ import mozilla.components.browser.storage.sync.PlacesHistoryStorage
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.helpers.AndroidAssetDispatcher
@@ -165,7 +164,6 @@ class HistoryTest {
         }
     }
 
-    @Ignore("Failing test: https://github.com/mozilla-mobile/fenix/issues/12893")
     @Test
     fun deleteAllHistoryTest() {
         val firstWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
