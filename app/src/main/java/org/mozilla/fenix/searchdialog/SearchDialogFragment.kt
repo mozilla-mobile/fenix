8c205f4c0bdc4551dff1bfc6892c1fb8e589b8fd
Jeff Boek <jeff@jeffboek.com>
2020-08-04 13:51:10 -0700

    For #13272 - Adds the ToolbarView to the SearchDialogFragment

--- app/src/main/java/org/mozilla/fenix/searchdialog/SearchDialogFragment.kt
+++ app/src/main/java/org/mozilla/fenix/searchdialog/SearchDialogFragment.kt
@@ -9,12 +9,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDialogFragment
+import kotlinx.android.synthetic.main.fragment_search.view.*
 import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.logDebug
+import org.mozilla.fenix.ext.requireComponents
+import org.mozilla.fenix.search.toolbar.ToolbarInteractor
+import org.mozilla.fenix.search.toolbar.ToolbarView
+
+class TempSearchInteractor : ToolbarInteractor {
+    override fun onUrlCommitted(url: String) {
+        logDebug("boek", "onUrlCommitted $url")
+    }
+
+    override fun onEditingCanceled() {
+        logDebug("boek", "onEditingCanceled")
+    }
+
+    override fun onTextChanged(text: String) {
+        logDebug("boek", "onTextChanged $text")
+    }
+}
 
 class SearchDialogFragment : AppCompatDialogFragment() {
+
+    private lateinit var toolbarView: ToolbarView
+    private val tempInteractor = TempSearchInteractor()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.SearchDialogStyle)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_search_dialog, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_search_dialog, container, false)
+
+        toolbarView = ToolbarView(
+            requireContext(),
+            tempInteractor,
+            null,
+            false,
+            view.toolbar,
+            requireComponents.core.engine
+        )
+
+        return view
+    }
 }
