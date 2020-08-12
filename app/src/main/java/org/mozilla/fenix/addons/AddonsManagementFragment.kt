d8cff89632b7d61f89639f829165ec3c9e2a63fe
Mugurell <Mugurell@users.noreply.github.com>
2020-08-12 14:19:42 +0300

    For #8578 - Don't attempt to use different addons icons backgrounds
    After the change from AC #8054 specifying different addons icons backgrounds
    is not possible anymore.
    All favicons used all throughout the app will have the same background.

--- app/src/main/java/org/mozilla/fenix/addons/AddonsManagementFragment.kt
+++ app/src/main/java/org/mozilla/fenix/addons/AddonsManagementFragment.kt
@@ -120,7 +120,6 @@ class AddonsManagementFragment : Fragment(R.layout.fragment_add_ons_management) 
             addonNameTextColor = ThemeManager.resolveAttribute(R.attr.primaryText, context),
             addonSummaryTextColor = ThemeManager.resolveAttribute(R.attr.secondaryText, context),
             sectionsTypeFace = ResourcesCompat.getFont(context, R.font.metropolis_semibold),
-            addonBackgroundIconColor = ThemeManager.resolveAttribute(R.attr.inset, requireContext()),
             addonAllowPrivateBrowsingLabelDrawableRes = R.drawable.ic_add_on_private_browsing_label
         )
     }
