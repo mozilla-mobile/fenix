99fab556f4c0d5c0000c7bb45665585f5782be94
Jonathan Almeida <jalmeida@mozilla.com>
2020-08-18 18:19:11 -0400

    For #12287: Address review comments

--- app/src/main/java/org/mozilla/fenix/tabtray/SyncedTabsController.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/SyncedTabsController.kt
@@ -6,13 +6,21 @@ package org.mozilla.fenix.tabtray
 
 import android.view.View
 import androidx.fragment.app.FragmentManager.findFragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import mozilla.components.browser.storage.sync.SyncedDeviceTabs
 import mozilla.components.feature.syncedtabs.view.SyncedTabsView
+import mozilla.components.lib.state.ext.flowScoped
+import mozilla.components.support.ktx.kotlinx.coroutines.flow.ifChanged
 import org.mozilla.fenix.sync.ListenerDelegate
 import org.mozilla.fenix.sync.SyncedTabsAdapter
 import org.mozilla.fenix.sync.ext.toAdapterList
@@ -20,8 +28,12 @@ import org.mozilla.fenix.sync.ext.toAdapterItem
 import org.mozilla.fenix.sync.ext.toStringRes
 import kotlin.coroutines.CoroutineContext
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class SyncedTabsController(
+    lifecycleOwner: LifecycleOwner,
     private val view: View,
+    store: TabTrayDialogFragmentStore,
+    private val concatAdapter: ConcatAdapter,
     coroutineContext: CoroutineContext = Dispatchers.Main
 ) : SyncedTabsView {
     override var listener: SyncedTabsView.Listener? = null
@@ -29,6 +41,24 @@ class SyncedTabsController(
     val adapter = SyncedTabsAdapter(ListenerDelegate { listener })
 
     private val scope: CoroutineScope = CoroutineScope(coroutineContext)
+
+    init {
+        store.flowScoped(lifecycleOwner) { flow ->
+            flow.map { it.mode }
+                .ifChanged()
+                .drop(1)
+                .collect { mode ->
+                    when (mode) {
+                        is TabTrayDialogFragmentState.Mode.Normal -> {
+                            concatAdapter.addAdapter(0, adapter)
+                        }
+                        is TabTrayDialogFragmentState.Mode.MultiSelect -> {
+                            concatAdapter.removeAdapter(adapter)
+                        }
+                    }
+                }
+        }
+    }
 
     override fun displaySyncedTabs(syncedTabs: List<SyncedDeviceTabs>) {
         scope.launch {
