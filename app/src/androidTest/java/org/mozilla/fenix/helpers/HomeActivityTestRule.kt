0b2e6297748245c482c989895a5f3fff593edf0f
Oana Horvath <oana.horvath@softvision.ro>
2020-08-19 18:06:40 +0300

    Closes #13219: Changes device long-tap delay before UI tests (#13502)

--- app/src/androidTest/java/org/mozilla/fenix/helpers/HomeActivityTestRule.kt
+++ app/src/androidTest/java/org/mozilla/fenix/helpers/HomeActivityTestRule.kt
@@ -5,7 +5,9 @@
 package org.mozilla.fenix.helpers
 
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import androidx.test.uiautomator.UiDevice
 import org.mozilla.fenix.HomeActivity
 
 /**
@@ -16,7 +18,12 @@ import org.mozilla.fenix.HomeActivity
  */
 
 class HomeActivityTestRule(initialTouchMode: Boolean = false, launchActivity: Boolean = true) :
-    ActivityTestRule<HomeActivity>(HomeActivity::class.java, initialTouchMode, launchActivity)
+    ActivityTestRule<HomeActivity>(HomeActivity::class.java, initialTouchMode, launchActivity) {
+    override fun beforeActivityLaunched() {
+        super.beforeActivityLaunched()
+        setLongTapTimeout()
+    }
+}
 
 /**
  * A [org.junit.Rule] to handle shared test set up for tests on [HomeActivity]. This adds
@@ -26,5 +33,19 @@ class HomeActivityTestRule(initialTouchMode: Boolean = false, launchActivity: Bo
  * @param launchActivity See [IntentsTestRule]
  */
 
-class HomeActivityIntentTestRule(initialTouchMode: Boolean = false, launchActivity: Boolean = true) :
-    IntentsTestRule<HomeActivity>(HomeActivity::class.java, initialTouchMode, launchActivity)
+class HomeActivityIntentTestRule(
+    initialTouchMode: Boolean = false,
+    launchActivity: Boolean = true
+) :
+    IntentsTestRule<HomeActivity>(HomeActivity::class.java, initialTouchMode, launchActivity) {
+    override fun beforeActivityLaunched() {
+        super.beforeActivityLaunched()
+        setLongTapTimeout()
+    }
+}
+
+// changing the device preference for Touch and Hold delay, to avoid long-clicks instead of a single-click
+fun setLongTapTimeout() {
+    val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    mDevice.executeShellCommand("settings put secure long_press_timeout 3000")
+}
--- app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsSubMenuAboutRobot.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsSubMenuAboutRobot.kt
@@ -222,7 +222,7 @@ private fun assertLibrariesUsed() {
         .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
         .perform(click())
 
-    onView(withId(R.id.action_bar)).check(matches(hasDescendant(withText(containsString("Firefox Preview | OSS Libraries")))))
+    onView(withId(R.id.toolbar)).check(matches(hasDescendant(withText(containsString("Firefox Preview | OSS Libraries")))))
     Espresso.pressBack()
 }
 
