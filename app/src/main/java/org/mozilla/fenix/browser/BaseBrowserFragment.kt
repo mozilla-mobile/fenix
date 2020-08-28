2f1b903005c93f56c1d99618cf09b9eedd6750eb
mcarare <mihai.carare.dev@gmail.com>
2020-08-28 16:15:05 +0300

    For #14289: Check if toolbar is initialised before setting flags.

--- app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
+++ app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
@@ -1162,6 +1162,8 @@ abstract class BaseBrowserFragment : Fragment(), UserInteractionHandler, Session
     }
 
     override fun onAccessibilityStateChanged(enabled: Boolean) {
-        browserToolbarView.setScrollFlags(enabled)
+        if (_browserToolbarView != null) {
+            browserToolbarView.setScrollFlags(enabled)
+        }
     }
 }
