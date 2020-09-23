b1384c7781308a2b4bb5aa0902dd08207637ec0b
Grisha Kruglov <gkruglov@mozilla.com>
2020-09-23 11:43:46 -0700

    Update A-C Version

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "60.0.20200922130109"
+    const val VERSION = "61.0.20200923130632"
 }
