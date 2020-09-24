d4ab728cff0991645306205cc5171d8a1993250f
Christian Sadilek <christian.sadilek@gmail.com>
2020-09-24 17:44:22 -0400

    For #14034: Add debug preference to override AMO collection in Nightly

---
+++ app/src/test/java/org/mozilla/fenix/settings/SettingsFragmentTest.kt
@@ -0,0 +1,76 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings
+
+import androidx.fragment.app.FragmentActivity
+import androidx.preference.Preference
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.getPreferenceKey
+import org.mozilla.fenix.helpers.FenixRobolectricTestRunner
+import org.mozilla.fenix.utils.Settings
+import org.robolectric.Robolectric
+
+@RunWith(FenixRobolectricTestRunner::class)
+class SettingsFragmentTest {
+
+    @Test
+    fun `Add-on collection override pref is visible if debug menu active`() {
+        val settingsFragment = SettingsFragment()
+        val activity = Robolectric.buildActivity(FragmentActivity::class.java).create().get()
+
+        activity.supportFragmentManager.beginTransaction()
+            .add(settingsFragment, "test")
+            .commitNow()
+
+        val preferenceAmoCollectionOverride = settingsFragment.findPreference<Preference>(
+            settingsFragment.getPreferenceKey(R.string.pref_key_override_amo_collection)
+        )
+
+        settingsFragment.setupAmoCollectionOverridePreference(mockk(relaxed = true))
+        assertNotNull(preferenceAmoCollectionOverride)
+        assertFalse(preferenceAmoCollectionOverride!!.isVisible)
+
+        val settings: Settings = mockk(relaxed = true)
+        every { settings.showSecretDebugMenuThisSession } returns true
+        settingsFragment.setupAmoCollectionOverridePreference(settings)
+        assertTrue(preferenceAmoCollectionOverride.isVisible)
+    }
+
+    @Test
+    fun `Add-on collection override pref is visible if already configured`() {
+        val settingsFragment = SettingsFragment()
+        val activity = Robolectric.buildActivity(FragmentActivity::class.java).create().get()
+
+        activity.supportFragmentManager.beginTransaction()
+            .add(settingsFragment, "test")
+            .commitNow()
+
+        val preferenceAmoCollectionOverride = settingsFragment.findPreference<Preference>(
+            settingsFragment.getPreferenceKey(R.string.pref_key_override_amo_collection)
+        )
+
+        settingsFragment.setupAmoCollectionOverridePreference(mockk(relaxed = true))
+        assertNotNull(preferenceAmoCollectionOverride)
+        assertFalse(preferenceAmoCollectionOverride!!.isVisible)
+
+        val settings: Settings = mockk(relaxed = true)
+        every { settings.showSecretDebugMenuThisSession } returns false
+
+        every { settings.amoCollectionOverrideConfigured() } returns false
+        settingsFragment.setupAmoCollectionOverridePreference(settings)
+        assertFalse(preferenceAmoCollectionOverride.isVisible)
+
+        every { settings.amoCollectionOverrideConfigured() } returns true
+        settingsFragment.setupAmoCollectionOverridePreference(settings)
+        assertTrue(preferenceAmoCollectionOverride.isVisible)
+    }
+}
