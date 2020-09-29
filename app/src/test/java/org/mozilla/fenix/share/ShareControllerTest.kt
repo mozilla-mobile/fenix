4de466883b8818a10fa21023dff2684342067927
Sebastian Kaspari <s.kaspari@gmail.com>
2020-09-29 10:59:17 +0200

    Revert "For #12565: Pass bookmark storage to controller" for debug test failures.
    This reverts commit 3c22100b8495434ea8c30f9cef2bb8ce554f180b.

--- app/src/test/java/org/mozilla/fenix/share/ShareControllerTest.kt
+++ app/src/test/java/org/mozilla/fenix/share/ShareControllerTest.kt
@@ -83,13 +83,11 @@ class ShareControllerTest {
 
     @Test
     fun `handleShareToApp should start a new sharing activity and close this`() = runBlocking {
-        val appPackageName = "package"
-        val appClassName = "activity"
         val appShareOption = AppShareOption(
             name = "app",
             icon = mockk(),
-            packageName = appPackageName,
-            activityName = appClassName
+            packageName = "package",
+            activityName = "activity"
         )
         val shareIntent = slot<Intent>()
         // Our share Intent uses `FLAG_ACTIVITY_NEW_TASK` but when resolving the startActivity call
@@ -110,8 +108,8 @@ class ShareControllerTest {
         assertEquals(textToShare, shareIntent.captured.extras!![Intent.EXTRA_TEXT])
         assertEquals("text/plain", shareIntent.captured.type)
         assertEquals(Intent.FLAG_ACTIVITY_NEW_TASK, shareIntent.captured.flags)
-        assertEquals(appPackageName, shareIntent.captured.component!!.packageName)
-        assertEquals(appClassName, shareIntent.captured.component!!.className)
+        assertEquals("package", shareIntent.captured.component!!.packageName)
+        assertEquals("activity", shareIntent.captured.component!!.className)
 
         verify { recentAppStorage.updateRecentApp(appShareOption.activityName) }
         verifyOrder {
