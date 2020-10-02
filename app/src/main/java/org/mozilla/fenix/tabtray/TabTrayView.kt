e6d78cfe8545db0630c6473cd5d7dfd7f019df84
mcarare <mihai.carare.dev@gmail.com>
2020-10-02 16:20:43 +0300

    For #15632: Improve checking open tabs logic to show collection button.
    Check now considers selected tab tray mode.

--- app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
@@ -99,6 +99,14 @@ class TabTrayView(
 
     private val components = container.context.components
 
+    private val checkOpenTabs = {
+        if (isPrivateModeSelected) {
+            view.context.components.core.store.state.privateTabs.isNotEmpty()
+        } else {
+            view.context.components.core.store.state.normalTabs.isNotEmpty()
+        }
+    }
+
     init {
         components.analytics.metrics.track(Event.TabsTrayOpened)
 
@@ -213,14 +221,9 @@ class TabTrayView(
         tabTrayItemMenu =
             TabTrayItemMenu(
                 context = view.context,
-                shouldShowSaveToCollection = { tabs.isNotEmpty() && view.tab_layout.selectedTabPosition == 0 },
-                hasOpenTabs = {
-                    if (isPrivateModeSelected) {
-                        view.context.components.core.store.state.privateTabs.isNotEmpty()
-                    } else {
-                        view.context.components.core.store.state.normalTabs.isNotEmpty()
-                    }
-                }) {
+                shouldShowSaveToCollection = { checkOpenTabs.invoke() && view.tab_layout.selectedTabPosition == 0 },
+                hasOpenTabs = checkOpenTabs
+            ) {
                 when (it) {
                     is TabTrayItemMenu.Item.ShareAllTabs -> interactor.onShareTabsClicked(
                         isPrivateModeSelected
