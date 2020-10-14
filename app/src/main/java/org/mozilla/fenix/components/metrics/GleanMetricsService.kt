ad6fc610f8b9b536fd40808c47cc319141750998
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2020-10-14 13:33:20 +0300

    For #15720 - Remove unused telemetry

--- app/src/main/java/org/mozilla/fenix/components/metrics/GleanMetricsService.kt
+++ app/src/main/java/org/mozilla/fenix/components/metrics/GleanMetricsService.kt
@@ -38,7 +38,6 @@ import org.mozilla.fenix.GleanMetrics.Preferences
 import org.mozilla.fenix.GleanMetrics.PrivateBrowsingMode
 import org.mozilla.fenix.GleanMetrics.PrivateBrowsingShortcut
 import org.mozilla.fenix.GleanMetrics.ProgressiveWebApp
-import org.mozilla.fenix.GleanMetrics.QrScanner
 import org.mozilla.fenix.GleanMetrics.ReaderMode
 import org.mozilla.fenix.GleanMetrics.SearchDefaultEngine
 import org.mozilla.fenix.GleanMetrics.SearchShortcuts
@@ -229,18 +228,6 @@ private val Event.wrapper: EventWrapper<*>?
         is Event.UriOpened -> EventWrapper<NoExtraKeys>(
             { Events.totalUriCount.add(1) }
         )
-        is Event.QRScannerOpened -> EventWrapper<NoExtraKeys>(
-            { QrScanner.opened.record(it) }
-        )
-        is Event.QRScannerPromptDisplayed -> EventWrapper<NoExtraKeys>(
-            { QrScanner.promptDisplayed.record(it) }
-        )
-        is Event.QRScannerNavigationAllowed -> EventWrapper<NoExtraKeys>(
-            { QrScanner.navigationAllowed.record(it) }
-        )
-        is Event.QRScannerNavigationDenied -> EventWrapper<NoExtraKeys>(
-            { QrScanner.navigationDenied.record(it) }
-        )
         is Event.ErrorPageVisited -> EventWrapper(
             { ErrorPage.visitedError.record(it) },
             { ErrorPage.visitedErrorKeys.valueOf(it) }
@@ -269,9 +256,6 @@ private val Event.wrapper: EventWrapper<*>?
         is Event.SyncAuthOtherExternal -> EventWrapper<NoExtraKeys>(
             { SyncAuth.otherExternal.record(it) }
         )
-        is Event.SyncAuthFromSharedReuse, Event.SyncAuthFromSharedCopy -> EventWrapper<NoExtraKeys>(
-            { SyncAuth.autoLogin.record(it) }
-        )
         is Event.SyncAuthRecovered -> EventWrapper<NoExtraKeys>(
             { SyncAuth.recovered.record(it) }
         )
@@ -283,9 +267,6 @@ private val Event.wrapper: EventWrapper<*>?
         )
         is Event.SyncAccountOpened -> EventWrapper<NoExtraKeys>(
             { SyncAccount.opened.record(it) }
-        )
-        is Event.SyncAccountClosed -> EventWrapper<NoExtraKeys>(
-            { SyncAccount.closed.record(it) }
         )
         is Event.SyncAccountSyncNow -> EventWrapper<NoExtraKeys>(
             { SyncAccount.syncNow.record(it) }
@@ -375,20 +356,11 @@ private val Event.wrapper: EventWrapper<*>?
         is Event.SearchWidgetVoiceSearchPressed -> EventWrapper<NoExtraKeys>(
             { SearchWidget.voiceButton.record(it) }
         )
-        is Event.PrivateBrowsingGarbageIconTapped -> EventWrapper<NoExtraKeys>(
-            { PrivateBrowsingMode.garbageIcon.record(it) }
-        )
         is Event.PrivateBrowsingSnackbarUndoTapped -> EventWrapper<NoExtraKeys>(
             { PrivateBrowsingMode.snackbarUndo.record(it) }
         )
         is Event.PrivateBrowsingNotificationTapped -> EventWrapper<NoExtraKeys>(
             { PrivateBrowsingMode.notificationTapped.record(it) }
-        )
-        is Event.PrivateBrowsingNotificationOpenTapped -> EventWrapper<NoExtraKeys>(
-            { PrivateBrowsingMode.notificationOpen.record(it) }
-        )
-        is Event.PrivateBrowsingNotificationDeleteAndOpenTapped -> EventWrapper<NoExtraKeys>(
-            { PrivateBrowsingMode.notificationDelete.record(it) }
         )
         is Event.PrivateBrowsingCreateShortcut -> EventWrapper<NoExtraKeys>(
             { PrivateBrowsingShortcut.createShortcut.record(it) }
@@ -582,9 +554,6 @@ private val Event.wrapper: EventWrapper<*>?
             { Events.tabCounterMenuAction.record(it) },
             { Events.tabCounterMenuActionKeys.valueOf(it) }
         )
-        is Event.OnboardingWhatsNew -> EventWrapper<NoExtraKeys>(
-            { Onboarding.whatsNew.record(it) }
-        )
         is Event.OnboardingPrivateBrowsing -> EventWrapper<NoExtraKeys>(
             { Onboarding.prefToggledPrivateBrowsing.record(it) }
         )
@@ -708,6 +677,7 @@ private val Event.wrapper: EventWrapper<*>?
         is Event.AddonInstalled -> null
         is Event.SearchWidgetInstalled -> null
         is Event.ChangedToDefaultBrowser -> null
+        is Event.SyncAuthFromSharedReuse, Event.SyncAuthFromSharedCopy -> null
     }
 
 class GleanMetricsService(
