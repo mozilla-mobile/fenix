975a5b76ec93ee58585b002019ec1b24ab84c6b4
Codrut Topliceanu <60002907+codrut-topliceanu@users.noreply.github.com>
2020-10-30 01:08:31 +0200

    For #14637 - Fix sitePermissionsFeature breaking change + AC bump (#16229)
    * For #14637 - Compatibility patch for AC - 8554
    
    * Update Android Components version to 65.0.20201029143039
    
    Co-authored-by: Arturo Mejia <arturomejiamarmol@gmail.com>

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "64.0.20201027143116"
+    const val VERSION = "65.0.20201029143039"
 }
