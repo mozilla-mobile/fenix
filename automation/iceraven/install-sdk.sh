7ac63a51a15472bb22aed73f64638806ee38811a
Adam Novak <interfect@gmail.com>
2020-11-08 17:32:19 -0800

    Get unzipped sdkmanager path right

--- automation/iceraven/install-sdk.sh
+++ automation/iceraven/install-sdk.sh
@@ -19,7 +19,7 @@ popd
 export ANDROID_SDK_ROOT=$HOME/android-sdk/android-sdk-linux/
 
 # Install the weirdly missing NDK
-${ANDROID_SDK_ROOT}/cmdline-tools/tools/bin/sdkmanager "ndk;21.0.6113669"
+${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager "ndk;21.0.6113669"
 
 # Point the build at the tools
 echo "sdk.dir=${ANDROID_SDK_ROOT}" >> local.properties
