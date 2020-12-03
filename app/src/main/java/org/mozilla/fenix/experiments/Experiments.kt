3cd4ff9ac8c1ae329f96ea58b49f98b589c46058
jhugman <jhugman@users.noreply.github.com>
2020-12-03 12:24:46 +0000

    Adding menu configuration for A/A experiment and A/B experiment (#16692)

---
+++ app/src/main/java/org/mozilla/fenix/experiments/Experiments.kt
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.experiments
+
+class Experiments {
+    companion object {
+        const val A_A_NIMBUS_VALIDATION = "fenix-nimbus-validation"
+        const val BOOKMARK_ICON = "fenix-bookmark-list-icon"
+    }
+}
+
+class ExperimentBranch {
+    companion object {
+        const val TREATMENT = "treatment"
+        const val CONTROL = "control"
+        const val A1 = "A1"
+        const val A2 = "A2"
+    }
+}
