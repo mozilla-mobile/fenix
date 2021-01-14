f8359e72bf15f21ad312f8bdc250312ee1560b41
Christian Sadilek <christian.sadilek@gmail.com>
2021-01-14 11:59:37 -0500

    Closes #17443: IllegalStateException in startPostponedEnterTransition on HomeFragment

--- app/src/main/java/org/mozilla/fenix/home/HomeScreenViewModel.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeScreenViewModel.kt
@@ -4,7 +4,6 @@
 
 package org.mozilla.fenix.home
 
-import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 
 class HomeScreenViewModel : ViewModel() {
@@ -12,8 +11,6 @@ class HomeScreenViewModel : ViewModel() {
      * Used to delete a specific session once the home screen is resumed
      */
     var sessionToDelete: String? = null
-
-    var layoutManagerState: Parcelable? = null
 
     /**
      * Used to remember if we need to scroll to top of the homeFragment's recycleView (top sites) see #8561
