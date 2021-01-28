43b0b27819133fbbfd761a9c2cdd299d2894eb8e
Elise Richards <erichards@mozilla.com>
2021-01-28 14:30:59 -0600

    Update metrics when an addon is installed or enabled (#17669)

--- app/src/main/java/org/mozilla/fenix/addons/InstalledAddonDetailsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/addons/InstalledAddonDetailsFragment.kt
@@ -21,6 +21,7 @@ import kotlinx.coroutines.launch
 import mozilla.components.feature.addons.Addon
 import mozilla.components.feature.addons.AddonManagerException
 import mozilla.components.feature.addons.ui.translateName
+import org.mozilla.fenix.GleanMetrics.Addons
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.components
@@ -127,6 +128,7 @@ class InstalledAddonDetailsFragment : Fragment() {
                                     )
                                 )
                             }
+                            Addons.hasEnabledAddons.set(true)
                         }
                     },
                     onError = {
