72f977771d0eedb94ce4097ac621c0320c147862
jhugman <jhugman@users.noreply.github.com>
2021-02-01 23:50:07 +0000

    Re-enable Nimbus with Off the main thread I/O and non-blocking networking (#17684)
    * Moved Nimbus setup from Analytics to its own file
    
    * Change experiment identifiers to fit new run of the experiment
    
    * Re-enable nimbus for debug and nightly builds
    
    * ./gradlew ktlint && detekt

--- app/src/main/java/org/mozilla/fenix/components/Analytics.kt
+++ app/src/main/java/org/mozilla/fenix/components/Analytics.kt
@@ -8,17 +8,13 @@ import android.app.Application
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.os.StrictMode
 import mozilla.components.lib.crash.CrashReporter
 import mozilla.components.lib.crash.service.CrashReporterService
 import mozilla.components.lib.crash.service.GleanCrashReporterService
 import mozilla.components.lib.crash.service.MozillaSocorroService
 import mozilla.components.lib.crash.service.SentryService
 import mozilla.components.service.nimbus.NimbusApi
-import mozilla.components.service.nimbus.Nimbus as NimbusEnabled
 import mozilla.components.service.nimbus.NimbusDisabled
-import mozilla.components.service.nimbus.NimbusServerSettings
 import org.mozilla.fenix.BuildConfig
 import org.mozilla.fenix.Config
 import org.mozilla.fenix.FeatureFlags
@@ -29,6 +25,7 @@ import org.mozilla.fenix.components.metrics.AdjustMetricsService
 import org.mozilla.fenix.components.metrics.GleanMetricsService
 import org.mozilla.fenix.components.metrics.LeanplumMetricsService
 import org.mozilla.fenix.components.metrics.MetricController
+import org.mozilla.fenix.experiments.createNimbus
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.perf.lazyMonitored
@@ -109,33 +106,7 @@ class Analytics(
 
     val experiments: NimbusApi by lazyMonitored {
         if (FeatureFlags.nimbusExperiments) {
-            // Eventually we'll want to use `NimbusDisabled` when we have no NIMBUS_ENDPOINT.
-            // but we keep this here to not mix feature flags and how we configure Nimbus.
-            val url: String? = BuildConfig.NIMBUS_ENDPOINT
-            val serverSettings = if (!url.isNullOrBlank()) {
-                NimbusServerSettings(url = Uri.parse(url))
-            } else {
-                null
-            }
-
-            NimbusEnabled(context, serverSettings).apply {
-                // Global opt out state is stored in Nimbus, and shouldn't be toggled to `true`
-                // from the app unless the user does so from a UI control.
-                // However, the user may have opt-ed out of mako experiments already, so
-                // we should respect that setting here.
-                val enabled =
-                    context.components.strictMode.resetAfter(StrictMode.allowThreadDiskReads()) {
-                        context.settings().isExperimentationEnabled
-                    }
-                if (!enabled) {
-                    globalUserParticipation = enabled
-                }
-
-                // Nimbus should look after downloading experiment definitions from remote settings
-                // on another thread, and making sure we don't hit the server each time we start.
-                @Suppress("Deprecation")
-                updateExperiments()
-            }
+            createNimbus(context, BuildConfig.NIMBUS_ENDPOINT)
         } else {
             NimbusDisabled()
         }
