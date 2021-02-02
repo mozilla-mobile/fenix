04d182fd20596088b15ac482b72943dd38b308b0
Adam Novak <interfect@gmail.com>
2021-02-01 21:41:35 -0800

    Actually satisfy the linter after removing a bunch of Nimbus code

--- app/src/main/java/org/mozilla/fenix/components/Analytics.kt
+++ app/src/main/java/org/mozilla/fenix/components/Analytics.kt
@@ -8,20 +8,14 @@ import android.app.Application
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.os.StrictMode
 import mozilla.components.lib.crash.CrashReporter
 import mozilla.components.lib.crash.service.CrashReporterService
 import mozilla.components.lib.crash.service.GleanCrashReporterService
 import mozilla.components.lib.crash.service.MozillaSocorroService
 import mozilla.components.lib.crash.service.SentryService
-import mozilla.components.service.nimbus.NimbusApi
-import mozilla.components.service.nimbus.Nimbus as NimbusEnabled
 import mozilla.components.service.nimbus.NimbusDisabled
-import mozilla.components.service.nimbus.NimbusServerSettings
 import org.mozilla.fenix.BuildConfig
 import org.mozilla.fenix.Config
-import org.mozilla.fenix.FeatureFlags
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ReleaseChannel
