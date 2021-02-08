e31b4f49e93389c5ad1f45dd9e2f87491d7d6b6e
AndiAJ <andiaj@users.noreply.github.com>
2021-02-08 17:45:42 +0200

    For #17195 Fix Intermittent UI test verifyTabTrayNotShowingStateHalfExpanded

--- app/src/androidTest/java/org/mozilla/fenix/ui/TabbedBrowsingTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/TabbedBrowsingTest.kt
@@ -262,7 +262,6 @@ class TabbedBrowsingTest {
 
     @Test
     fun verifyTabTrayNotShowingStateHalfExpanded() {
-        homeScreen { }.dismissOnboarding()
 
         navigationToolbar {
         }.openTabTray {
