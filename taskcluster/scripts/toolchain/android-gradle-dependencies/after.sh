58a8c0a38f1f3957c553f348f8b4f286cd629d8b
Sebastian Kaspari <s.kaspari@gmail.com>
2021-02-09 17:06:03 +0100

    Issue #17819: Add Maven Central repository.

--- taskcluster/scripts/toolchain/android-gradle-dependencies/after.sh
+++ taskcluster/scripts/toolchain/android-gradle-dependencies/after.sh
@@ -22,6 +22,7 @@ mkdir -p android-gradle-dependencies /builds/worker/artifacts
 
 cp -R ${NEXUS_WORK}/storage/jcenter android-gradle-dependencies
 cp -R ${NEXUS_WORK}/storage/google android-gradle-dependencies
+cp -R ${NEXUS_WORK}/storage/central android-gradle-dependencies
 
 tar cf - android-gradle-dependencies | xz > /builds/worker/artifacts/android-gradle-dependencies.tar.xz
 
