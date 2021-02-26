1eb6671d018654725b1d358568d3a0bb09132e9e
isabelrios <isabelrios@gmail.com>
2021-02-26 12:58:15 +0100

    Modify taskgraph to allow running UI tests on Nightly builds

--- taskcluster/fenix_taskgraph/transforms/signing.py
+++ taskcluster/fenix_taskgraph/transforms/signing.py
@@ -18,7 +18,7 @@ transforms = TransformSequence()
 @transforms.add
 def resolve_keys(config, tasks):
     for task in tasks:
-        for key in ("index", "run-on-tasks-for", "worker-type", "worker.signing-type", "signing-format"):
+        for key in ("run-on-tasks-for",):
             resolve_keyed_by(
                 task,
                 key,
@@ -29,6 +29,50 @@ def resolve_keys(config, tasks):
                     'tasks-for': config.params["tasks_for"],
                 }
             )
+        yield task
+
+@transforms.add
+def set_worker_type(config, tasks):
+    for task in tasks:
+        worker_type = "dep-signing"
+        if (
+            str(config.params["level"]) == "3"
+            and task["attributes"]["build-type"]
+            in ("nightly", "beta", "release", "android-test-nightly")
+            and config.params["tasks_for"] in ("cron", "github-release", "action")
+        ):
+            worker_type = "signing"
+        task["worker-type"] = worker_type
+        yield task
+
+
+@transforms.add
+def set_signing_type(config, tasks):
+    for task in tasks:
+        signing_type = "dep-signing"
+        if (
+            str(config.params["level"]) == "3"
+            and config.params["tasks_for"] in ("cron", "github-release", "action")
+        ):
+            if task["attributes"]["build-type"] in ("beta", "release"):
+                signing_type = "fennec-production-signing"
+            elif task["attributes"]["build-type"] in ("nightly", "android-test-nightly"):
+                signing_type = "production-signing"
+        task.setdefault("worker", {})["signing-type"] = signing_type
+        yield task
+
+
+@transforms.add
+def set_index(config, tasks):
+    for task in tasks:
+        index = {}
+        if (
+            config.params["tasks_for"] in ("cron", "github-release", "action")
+            and task["attributes"]["build-type"]
+            in ("nightly", "debut", "nightly-simulation", "beta", "release")
+        ):
+            index["type"] = "signing"
+        task["index"] = index
         yield task
 
 
