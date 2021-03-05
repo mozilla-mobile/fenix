32d16fde0f25e54bec870337bf944e1899db3c37
Adam Novak <interfect@gmail.com>
2021-03-04 19:57:26 -0800

    Satisfy linter

--- app/src/main/java/io/github/forkmaintainers/iceraven/components/PagedAddonCollectionProvider.kt
+++ app/src/main/java/io/github/forkmaintainers/iceraven/components/PagedAddonCollectionProvider.kt
@@ -93,7 +93,11 @@ class PagedAddonCollectionProvider(
      * a connectivity problem or a timeout.
      */
     @Throws(IOException::class)
-    override suspend fun getAvailableAddons(allowCache: Boolean, readTimeoutInSeconds: Long?, language: String?): List<Addon> {
+    override suspend fun getAvailableAddons(
+        allowCache: Boolean,
+        readTimeoutInSeconds: Long?,
+        language: String?
+    ): List<Addon> {
         val cachedAddons = if (allowCache && !cacheExpired(context)) {
             readFromDiskCache()
         } else {
