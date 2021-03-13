491a11674f3b7d2f413a6505d73752b496435399
Michael Comella <michael.l.comella@gmail.com>
2021-03-12 16:40:02 -0800

    Improve navGraphProvider docs

--- app/src/main/java/org/mozilla/fenix/perf/NavGraphProvider.kt
+++ app/src/main/java/org/mozilla/fenix/perf/NavGraphProvider.kt
@@ -13,16 +13,23 @@ import kotlinx.coroutines.launch
 import org.mozilla.fenix.R
 
 /**
- * This class asynchronously loads the Navigation xml.
+ * This class asynchronously loads the navigation graph XML. This is a performance optimization:
+ * large nav graphs take ~29ms to inflate on the Moto G5 (#16900). This also seemingly prevents the
+ * HomeFragment layout XML from being unnecessarily inflated when it isn't used, improving perf by
+ * ~148ms on the Moto G5 for VIEW start up (#18245) though it was unintentional and we may wish to
+ * implement more intentional for that.
  *
- * This class is defined as an Object since it needs to be called by the NavController extension
- * function.
+ * This class is defined as an Object, rather than as a class instance in our Components, because
+ * it needs to be called by the [NavController] extension function which can't easily access Components.
  *
- * To use this class properly, NavGraphProvider.inflateNavGraphAsync must be called first before
- * blockForNavGraphInflation using the same navController.
+ * To use this class properly, [inflateNavGraphAsync] must be called first before
+ * [blockForNavGraphInflation] using the same [NavController] instance.
  */
 object NavGraphProvider {
 
+    // We want to store member state on the NavController. However, there is no way to do this.
+    // Instead, we store state as part of a map: NavController instance -> State. In order to
+    // garbage collect our data when the NavController is no longer relevant, we use a WeakHashMap.
     private val map = WeakHashMap<NavController, Job>()
 
     fun inflateNavGraphAsync(navController: NavController, lifecycleScope: LifecycleCoroutineScope) {
@@ -38,12 +45,15 @@ object NavGraphProvider {
      * The job should block the main thread if it isn't completed so that the NavGraph can be loaded
      * before any navigation is done.
      *
-     * InflateNavGraphAsync must be called before this method.
+     * [inflateNavGraphAsync] must be called before this method.
+     *
+     * @throws IllegalStateException if [inflateNavGraphAsync] wasn't called first for this [NavController]
      */
     fun blockForNavGraphInflation(navController: NavController) {
         val inflationJob = map[navController] ?: throw IllegalStateException("Expected " +
-                "`NavGraphProvider.inflateNavGraphAsync` to be called before this method with the" +
-                " same `navController`. If it was, this may be a bug in the implementation.")
+            "`NavGraphProvider.inflateNavGraphAsync` to be called before this method with the same " +
+            "`NavController` instance. If this occurred in a test, you probably need to add the " +
+            "DisableNavGraphProviderAssertionRule.")
         runBlockingIncrement { inflationJob.join() }
     }
 }
