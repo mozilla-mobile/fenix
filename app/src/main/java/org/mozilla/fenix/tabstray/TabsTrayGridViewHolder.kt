718bd09c4ae38389dcef569c4c49c2f0dc112c9e
Roger Yang <royang@mozilla.com>
2021-03-25 11:50:04 -0400

    Closes #18535: Re-add grid layout to tabs tray

---
+++ app/src/main/java/org/mozilla/fenix/tabstray/TabsTrayGridViewHolder.kt
@@ -0,0 +1,58 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.tabstray
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatImageButton
+import mozilla.components.browser.tabstray.TabsTrayStyling
+import mozilla.components.concept.base.images.ImageLoader
+import mozilla.components.concept.tabstray.Tab
+import mozilla.components.concept.tabstray.TabsTray
+import mozilla.components.support.base.observer.Observable
+import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.increaseTapArea
+import kotlin.math.max
+import kotlinx.android.synthetic.main.tab_tray_grid_item.view.tab_tray_grid_item
+
+/**
+ * A RecyclerView ViewHolder implementation for "tab" items with grid layout.
+ */
+class TabsTrayGridViewHolder(
+    parent: ViewGroup,
+    imageLoader: ImageLoader,
+    private val itemView: View =
+        LayoutInflater.from(parent.context).inflate(R.layout.tab_tray_grid_item, parent, false),
+    thumbnailSize: Int =
+        max(
+            itemView.resources.getDimensionPixelSize(R.dimen.tab_tray_grid_item_thumbnail_height),
+            itemView.resources.getDimensionPixelSize(R.dimen.tab_tray_grid_item_thumbnail_width)
+        )
+) : TabsTrayViewHolder(itemView, imageLoader, thumbnailSize) {
+
+    private val closeButton: AppCompatImageButton = itemView.findViewById(R.id.mozac_browser_tabstray_close)
+
+    override fun updateSelectedTabIndicator(showAsSelected: Boolean) {
+        itemView.tab_tray_grid_item.background = if (showAsSelected) {
+            AppCompatResources.getDrawable(itemView.context, R.drawable.tab_tray_grid_item_selected_border)
+        } else {
+            null
+        }
+        return
+    }
+
+    override fun bind(
+        tab: Tab,
+        isSelected: Boolean,
+        styling: TabsTrayStyling,
+        observable: Observable<TabsTray.Observer>
+    ) {
+        super.bind(tab, isSelected, styling, observable)
+
+        closeButton.increaseTapArea(GRID_ITEM_CLOSE_BUTTON_EXTRA_DPS)
+    }
+}
---
+++ app/src/main/java/org/mozilla/fenix/tabstray/TabsTrayListViewHolder.kt
@@ -0,0 +1,43 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.tabstray
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import mozilla.components.concept.base.images.ImageLoader
+import org.mozilla.fenix.R
+import kotlin.math.max
+
+/**
+ * A RecyclerView ViewHolder implementation for "tab" items with list layout.
+ */
+class TabsTrayListViewHolder(
+    parent: ViewGroup,
+    imageLoader: ImageLoader,
+    private val itemView: View =
+        LayoutInflater.from(parent.context).inflate(R.layout.tab_tray_item, parent, false),
+    thumbnailSize: Int =
+        max(
+            itemView.resources.getDimensionPixelSize(R.dimen.tab_tray_list_item_thumbnail_height),
+            itemView.resources.getDimensionPixelSize(R.dimen.tab_tray_list_item_thumbnail_width)
+        )
+) : TabsTrayViewHolder(itemView, imageLoader, thumbnailSize) {
+
+    override fun updateSelectedTabIndicator(showAsSelected: Boolean) {
+        val color = if (showAsSelected) {
+            R.color.tab_tray_item_selected_background_normal_theme
+        } else {
+            R.color.tab_tray_item_background_normal_theme
+        }
+        itemView.setBackgroundColor(
+            ContextCompat.getColor(
+                itemView.context,
+                color
+            )
+        )
+    }
+}
