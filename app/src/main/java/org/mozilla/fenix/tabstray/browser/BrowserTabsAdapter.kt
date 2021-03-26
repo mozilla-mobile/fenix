c21b44e0a35ed0a6781c05d4c468ba2aeba766a8
Roger Yang <royang@mozilla.com>
2021-03-26 10:33:46 -0400

    Closes #18535: Re-add grid layout to tabs tray (#18638)

--- app/src/main/java/org/mozilla/fenix/tabstray/browser/BrowserTabsAdapter.kt
+++ app/src/main/java/org/mozilla/fenix/tabstray/browser/BrowserTabsAdapter.kt
@@ -5,7 +5,6 @@
 package org.mozilla.fenix.tabstray.browser
 
 import android.content.Context
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,10 +14,10 @@ import mozilla.components.browser.thumbnails.loader.ThumbnailLoader
 import mozilla.components.concept.tabstray.TabsTray
 import mozilla.components.support.base.observer.Observable
 import mozilla.components.support.base.observer.ObserverRegistry
-import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.settings
-import org.mozilla.fenix.tabtray.TabTrayViewHolder
+import org.mozilla.fenix.tabstray.TabsTrayGridViewHolder
+import org.mozilla.fenix.tabstray.TabsTrayListViewHolder
 
 /**
  * A [RecyclerView.Adapter] for browser tabs.
@@ -42,21 +41,17 @@ class BrowserTabsAdapter(
 
     override fun getItemViewType(position: Int): Int {
         return if (context.settings().gridTabView) {
-            // ViewType.GRID.ordinal
-            R.layout.tab_tray_grid_item
+            ViewType.GRID.ordinal
         } else {
-            // ViewType.LIST.ordinal
-            R.layout.tab_tray_item
+            ViewType.LIST.ordinal
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TabViewHolder {
-        // TODO make this into separate view holders for each layout
-        // For this, we need to separate the TabTrayViewHolder as well.
-        // See https://github.com/mozilla-mobile/fenix/issues/18535
-        val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
-
-        return TabTrayViewHolder(view, imageLoader)
+        return when (viewType) {
+            ViewType.GRID.ordinal -> TabsTrayGridViewHolder(parent, imageLoader)
+            else -> TabsTrayListViewHolder(parent, imageLoader)
+        }
     }
 
     override fun onBindViewHolder(holder: TabViewHolder, position: Int) {
