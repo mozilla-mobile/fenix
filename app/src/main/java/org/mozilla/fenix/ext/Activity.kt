c6f7992fc3872ac2d4b283434aac4c1d1fccff94
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2021-03-30 13:22:53 +0300

    For #18667 - Migrated exitImmersiveModeIfNeeded to Fenix

--- app/src/main/java/org/mozilla/fenix/ext/Activity.kt
+++ app/src/main/java/org/mozilla/fenix/ext/Activity.kt
@@ -24,6 +24,19 @@ fun Activity.enterToImmersiveMode() {
             or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 }
 
+/**
+ * Attempts to come out from immersive mode using the View.
+ */
+fun Activity.exitImmersiveModeIfNeeded() {
+    if (WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON and window.attributes.flags == 0) {
+        // We left immersive mode already.
+        return
+    }
+
+    window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+}
+
 fun Activity.breadcrumb(
     message: String,
     data: Map<String, String> = emptyMap()
