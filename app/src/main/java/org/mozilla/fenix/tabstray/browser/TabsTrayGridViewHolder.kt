8ccc4648a9369e9e167c434da8f4b451a0c373cc
Jonathan Almeida <jalmeida@mozilla.com>
2021-03-30 11:40:47 -0400

    Issue #18535: Move list/grid viewholders to browser package

--- app/src/main/java/org/mozilla/fenix/tabstray/browser/TabsTrayGridViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/tabstray/browser/TabsTrayGridViewHolder.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.fenix.tabstray
+package org.mozilla.fenix.tabstray.browser
 
 import android.view.LayoutInflater
 import android.view.View
@@ -18,7 +18,7 @@ import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.increaseTapArea
 import kotlin.math.max
 import kotlinx.android.synthetic.main.tab_tray_grid_item.view.tab_tray_grid_item
-import org.mozilla.fenix.tabstray.browser.BrowserTrayInteractor
+import org.mozilla.fenix.tabstray.TabsTrayViewHolder
 
 /**
  * A RecyclerView ViewHolder implementation for "tab" items with grid layout.
