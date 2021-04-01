21c5eba31061598816aef7338898ed3e16dd918d
MickeyMoz <sebastian@mozilla.com>
2021-04-01 15:35:21 +0000

    Update Android Components version to 75.0.20210401143103.

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "75.0.20210330143044"
+    const val VERSION = "75.0.20210401143103"
 }
