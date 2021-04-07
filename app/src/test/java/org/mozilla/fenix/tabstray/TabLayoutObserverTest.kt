adc9aab3887af67bd1e45cf794b69ff6467edb9f
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2021-04-07 14:00:00 +0300

    For #18515 - Added Icon and sync functionality
    Co-authored-by: Jonathan Almeida <jalmeida@mozilla.com>

--- app/src/test/java/org/mozilla/fenix/tabstray/TabLayoutObserverTest.kt
+++ app/src/test/java/org/mozilla/fenix/tabstray/TabLayoutObserverTest.kt
@@ -8,25 +8,43 @@ import com.google.android.material.tabs.TabLayout
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import mozilla.components.support.test.libstate.ext.waitUntilIdle
+import mozilla.components.support.test.middleware.CaptureActionsMiddleware
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class TabLayoutObserverTest {
     private val interactor = mockk<TabsTrayInteractor>(relaxed = true)
+    private lateinit var store: TabsTrayStore
+    private val middleware = CaptureActionsMiddleware<TabsTrayState, TabsTrayAction>()
+
+    @Before
+    fun setup() {
+        store = TabsTrayStore(middlewares = listOf(middleware))
+    }
 
     @Test
     fun `WHEN tab is selected THEN notify the interactor`() {
-        val observer = TabLayoutObserver(interactor)
+        val observer = TabLayoutObserver(interactor, store)
         val tab = mockk<TabLayout.Tab>()
         every { tab.position } returns 1
 
         observer.onTabSelected(tab)
 
+        store.waitUntilIdle()
+
         verify { interactor.setCurrentTrayPosition(1, false) }
+
+        middleware.assertLastAction(TabsTrayAction.PageSelected::class) {
+            assertTrue(it.page == Page.PrivateTabs)
+        }
     }
 
     @Test
     fun `WHEN observer is first started THEN do not smooth scroll`() {
-        val observer = TabLayoutObserver(interactor)
+        val store = TabsTrayStore()
+        val observer = TabLayoutObserver(interactor, store)
         val tab = mockk<TabLayout.Tab>()
         every { tab.position } returns 1
 
