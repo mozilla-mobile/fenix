c4b4db215deb1cce59458cd460f15159d831010f
Arturo Mejia <arturomejiamarmol@gmail.com>
2021-04-07 13:25:33 -0400

    For #18608 made set a default browser functionality publicly available.

--- app/src/main/java/org/mozilla/fenix/ext/Activity.kt
+++ app/src/main/java/org/mozilla/fenix/ext/Activity.kt
@@ -8,6 +8,14 @@ import android.app.Activity
 import android.view.View
 import android.view.WindowManager
 import mozilla.components.concept.base.crash.Breadcrumb
+import android.app.role.RoleManager
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import androidx.core.os.bundleOf
+import org.mozilla.fenix.BrowserDirection
+import org.mozilla.fenix.HomeActivity
+import org.mozilla.fenix.settings.SupportUtils
 
 /**
  * Attempts to call immersive mode using the View to hide the status bar and navigation buttons.
@@ -48,3 +56,59 @@ fun Activity.breadcrumb(
         )
     )
 }
+
+/**
+ *  Opens Android's Manage Default Apps Settings if possible.
+ */
+fun Activity.openSetDefaultBrowserOption() {
+    when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+            getSystemService(RoleManager::class.java).also {
+                if (it.isRoleAvailable(RoleManager.ROLE_BROWSER) && !it.isRoleHeld(
+                        RoleManager.ROLE_BROWSER
+                    )
+                ) {
+                    startActivityForResult(
+                        it.createRequestRoleIntent(RoleManager.ROLE_BROWSER),
+                        REQUEST_CODE_BROWSER_ROLE
+                    )
+                } else {
+                    navigateToDefaultBrowserAppsSettings()
+                }
+            }
+        }
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> {
+            navigateToDefaultBrowserAppsSettings()
+        }
+        else -> {
+            (this as HomeActivity).openToBrowserAndLoad(
+                searchTermOrURL = SupportUtils.getSumoURLForTopic(
+                    this,
+                    SupportUtils.SumoTopic.SET_AS_DEFAULT_BROWSER
+                ),
+                newTab = true,
+                from = BrowserDirection.FromSettings
+            )
+        }
+    }
+}
+
+private fun Activity.navigateToDefaultBrowserAppsSettings() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        val intent = Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS)
+        intent.putExtra(
+            SETTINGS_SELECT_OPTION_KEY,
+            DEFAULT_BROWSER_APP_OPTION
+        )
+        intent.putExtra(
+            SETTINGS_SHOW_FRAGMENT_ARGS,
+            bundleOf(SETTINGS_SELECT_OPTION_KEY to DEFAULT_BROWSER_APP_OPTION)
+        )
+        startActivity(intent)
+    }
+}
+
+const val REQUEST_CODE_BROWSER_ROLE = 1
+const val SETTINGS_SELECT_OPTION_KEY = ":settings:fragment_args_key"
+const val SETTINGS_SHOW_FRAGMENT_ARGS = ":settings:show_fragment_args"
+const val DEFAULT_BROWSER_APP_OPTION = "default_browser"
