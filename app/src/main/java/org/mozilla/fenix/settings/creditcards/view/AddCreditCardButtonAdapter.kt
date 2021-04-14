04ca0b742eb3cdd2b2c9a0fe9926004a89c11318
Christian H <christian.hernandez.2015@gmail.com>
2021-04-13 21:17:29 -0700

    #18881 - Rebasing with master after falling behind

---
+++ app/src/main/java/org/mozilla/fenix/settings/creditcards/view/AddCreditCardButtonAdapter.kt
@@ -0,0 +1,22 @@
+package org.mozilla.fenix.settings.creditcards.view
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapter for the add credit card button
+ */
+class AddCreditCardButtonAdapter : RecyclerView.Adapter<AddCreditCardButtonViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddCreditCardButtonViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holderCreditCard: AddCreditCardButtonViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/org/mozilla/fenix/settings/creditcards/view/AddCreditCardButtonViewHolder.kt
@@ -0,0 +1,14 @@
+package org.mozilla.fenix.settings.creditcards.view
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Viewholder for Add Credit Card Button
+ */
+class AddCreditCardButtonViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind() {
+
+    }
+}
