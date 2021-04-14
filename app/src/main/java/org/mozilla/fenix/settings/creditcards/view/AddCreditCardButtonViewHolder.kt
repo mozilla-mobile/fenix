e3f14abbdfaf0f1cdf9fdaef2179cdb777ce43b8
Christian H <christian.hernandez.2015@gmail.com>
2021-04-13 23:03:33 -0700

    #18881 - Implemented AddCreditCardButtonViewHolder. Added an onclick listener to the view holder that will take the user to the credit card editor desitnation when add button is clicked. Implemented AddCreditCardButtonAdapter. I added id to the saved_cards_add_button.xml layout.

--- app/src/main/java/org/mozilla/fenix/settings/creditcards/view/AddCreditCardButtonViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/settings/creditcards/view/AddCreditCardButtonViewHolder.kt
@@ -1,14 +1,29 @@
 package org.mozilla.fenix.settings.creditcards.view
 
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import org.mozilla.fenix.R
+import org.mozilla.fenix.settings.creditcards.interactor.CreditCardsManagementInteractor
 
 /**
  * Viewholder for Add Credit Card Button
  */
-class AddCreditCardButtonViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class AddCreditCardButtonViewHolder(
+        private val view: View) : RecyclerView.ViewHolder(view) {
 
-    fun bind() {
-
+    fun bind(position : Int) {
+        view.findViewById<ConstraintLayout>(R.id.saved_cards_layout).setOnClickListener {
+            Log.e("Add Credit Card Viewhol","clicked")
+        }
     }
+
+
+    companion object {
+        const val LAYOUT_ID = R.layout.saved_cards_add_button
+    }
+
 }
