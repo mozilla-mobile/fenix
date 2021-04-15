4de9c7dc04d08900d95d5f8cbf2ba8108e6221c0
Grisha Kruglov <gkruglov@mozilla.com>
2021-04-15 11:25:46 -0700

    Fix breaking changes introduced by credit card encryption APIs

--- app/src/main/java/org/mozilla/fenix/components/BackgroundServices.kt
+++ app/src/main/java/org/mozilla/fenix/components/BackgroundServices.kt
@@ -95,8 +95,8 @@ class BackgroundServices(
     private val syncConfig = SyncConfig(supportedEngines, PeriodicSyncConfig(periodMinutes = 240)) // four hours
 
     init {
-        /* Make the "history", "bookmark", "passwords", and "tabs" stores accessible to workers
-           spawned by the sync manager. */
+        // Make the "history", "bookmark", "passwords", "tabs" stores
+        // accessible to workers spawned by the sync manager.
         GlobalSyncableStoreProvider.configureStore(SyncEngine.History to historyStorage)
         GlobalSyncableStoreProvider.configureStore(SyncEngine.Bookmarks to bookmarkStorage)
         GlobalSyncableStoreProvider.configureStore(SyncEngine.Passwords to passwordsStorage)
