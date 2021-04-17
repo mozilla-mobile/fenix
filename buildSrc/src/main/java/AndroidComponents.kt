5872d7a849cc50f99b166b1b9d571749d9745ab6
Ryan VanderMeulen <ryanvm@gmail.com>
2021-04-17 13:23:09 -0400

    Update A-C to 75.0.20210417143049 (#19097)

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "75.0.20210416190104"
+    const val VERSION = "75.0.20210417143049"
 }
