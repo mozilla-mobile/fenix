9f522e206429349f4656cfa1c53daad2c0a4fc1b
Christian H <christian.hernandez.2015@gmail.com>
2021-04-19 16:21:03 -0700

    #18881 - Added 6 dp margin to the add credit card button. Deleted log statements and cleaned up some imports

--- app/src/main/java/org/mozilla/fenix/settings/creditcards/view/AddCreditCardButtonViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/settings/creditcards/view/AddCreditCardButtonViewHolder.kt
@@ -1,13 +1,8 @@
 package org.mozilla.fenix.settings.creditcards.view
 
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import org.mozilla.fenix.R
-import kotlinx.android.synthetic.main.saved_cards_add_button.*
 import org.mozilla.fenix.settings.creditcards.interactor.CreditCardsManagementInteractor
 
 /**
@@ -18,7 +13,6 @@ class AddCreditCardButtonViewHolder(
         private val interactor: CreditCardsManagementInteractor) : RecyclerView.ViewHolder(view) {
 
     fun bind() {
-        Log.e("VIEWHOLDER VIEW",view.tag.toString())
         view.setOnClickListener {
             interactor.onAddCreditCard()
         }
