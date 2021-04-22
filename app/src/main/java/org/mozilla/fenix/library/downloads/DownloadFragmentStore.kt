c725493bea4b5d2ce2a8185f00949c14e7649772
Arturo Mejia <arturomejiamarmol@gmail.com>
2021-04-22 15:58:24 -0400

    Fix openFile breaking changes

--- app/src/main/java/org/mozilla/fenix/library/downloads/DownloadFragmentStore.kt
+++ app/src/main/java/org/mozilla/fenix/library/downloads/DownloadFragmentStore.kt
@@ -12,13 +12,16 @@ import mozilla.components.lib.state.Store
 /**
  * Class representing a downloads entry
  * @property id Unique id of the download item
+ * @property url The full url to the content that should be downloaded
  * @property fileName File name of the download item
  * @property filePath Full path of the download item
  * @property size The size in bytes of the download item
  * @property contentType The type of file the download is
+ * @property status The status that represents every state that a download can be in
  */
 data class DownloadItem(
     val id: String,
+    val url: String,
     val fileName: String?,
     val filePath: String,
     val size: String,
