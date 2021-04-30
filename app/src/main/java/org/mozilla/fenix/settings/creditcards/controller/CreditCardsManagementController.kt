8a6906e00ac824684910af070cc6fa9b31f8a94a
Christian H <christian.hernandez.2015@gmail.com>
2021-04-29 23:59:04 -0700

    #18881 - Adding extra space that will fail to locate lint issue.

--- app/src/main/java/org/mozilla/fenix/settings/creditcards/controller/CreditCardsManagementController.kt
+++ app/src/main/java/org/mozilla/fenix/settings/creditcards/controller/CreditCardsManagementController.kt
@@ -43,7 +43,8 @@ class DefaultCreditCardsManagementController(
                 )
         )
     }
-    
+
+
     override fun handleAddCreditCardButtonClicked() {
         navController.navigateBlockingForAsyncNavGraph(
             CreditCardsManagementFragmentDirections
