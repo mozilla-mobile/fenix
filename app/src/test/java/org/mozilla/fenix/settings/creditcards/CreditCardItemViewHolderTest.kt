9575d9a7da43f6eae049438e12106c31be565a14
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2021-05-20 10:17:04 +0300

    For #18270 - Fixes test for AC method used

--- app/src/test/java/org/mozilla/fenix/settings/creditcards/CreditCardItemViewHolderTest.kt
+++ app/src/test/java/org/mozilla/fenix/settings/creditcards/CreditCardItemViewHolderTest.kt
@@ -11,7 +11,6 @@ import io.mockk.verify
 import kotlinx.android.synthetic.main.credit_card_list_item.view.*
 import mozilla.components.concept.storage.CreditCard
 import mozilla.components.concept.storage.CreditCardNumber
-import mozilla.components.support.ktx.kotlin.addEllipsesToCreditCardNumber
 import mozilla.components.support.test.robolectric.testContext
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -51,7 +50,7 @@ class CreditCardItemViewHolderTest {
     fun `GIVEN a new credit card item on bind THEN set the card number and expiry date text`() {
         CreditCardItemViewHolder(view, interactor).bind(creditCard)
 
-        assertEquals(creditCard.cardNumberLast4.addEllipsesToCreditCardNumber(), view.credit_card_number.text)
+        assertEquals(creditCard.obfuscatedCardNumber, view.credit_card_number.text)
         assertEquals("0${creditCard.expiryMonth}/${creditCard.expiryYear}", view.expiry_date.text)
     }
 
