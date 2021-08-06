pocmo/phoenix

Issue #19956: Add telemetry for tab view setting changes
This differs from `tab_view_setting` which tells us what the user's tab
setting is at startup. It does not tell us if the user explicitly
changed it instead of just using the default (which was recently
changed in #19809).

app/src/main/java/org/mozilla/fenix/components/metrics/Event.kt
@@ -628,6 +628,13 @@ sealed class Event {
             get() = mapOf(Autoplay.settingChangedKeys.autoplaySetting to setting.toString().toLowerCase(Locale.ROOT))
     }
 
+    data class TabViewSettingChanged(val type: Type) : Event() {
+        enum class Type { LIST, GRID }
+
+        override val extras: Map<Events.tabViewChangedKeys, String>?
+            get() = mapOf(Events.tabViewChangedKeys.type to type.toString().toLowerCase(Locale.ROOT))
+    }
+
     sealed class Search
 
     internal open val extras: Map<*, String>?

mozilla-mobile/fenix

app/src/main/java/org/mozilla/fenix/components/metrics/Event.kt
@@ -605,6 +605,13 @@ sealed class Event {
             get() = mapOf(Autoplay.settingChangedKeys.autoplaySetting to setting.toString().lowercase(Locale.ROOT))
     }
 
+    data class TabViewSettingChanged(val type: Type) : Event() {
+        enum class Type { LIST, GRID }
+
+        override val extras: Map<Events.tabViewChangedKeys, String>?
+            get() = mapOf(Events.tabViewChangedKeys.type to type.toString().lowercase(Locale.ROOT))
+    }
+
     sealed class Search
 
     internal open val extras: Map<*, String>?