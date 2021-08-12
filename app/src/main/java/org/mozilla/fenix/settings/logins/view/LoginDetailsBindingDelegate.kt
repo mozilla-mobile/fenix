34e063669a46842254e0a1797cb0da4cf2637c67
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2021-08-12 13:20:57 +0300

    For #17917: Use View binding in Login Exceptions

--- app/src/main/java/org/mozilla/fenix/settings/logins/view/LoginDetailsBindingDelegate.kt
+++ app/src/main/java/org/mozilla/fenix/settings/logins/view/LoginDetailsBindingDelegate.kt
@@ -4,18 +4,18 @@
 
 package org.mozilla.fenix.settings.logins.view
 
-import android.view.ViewGroup
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.fragment_login_detail.*
+import org.mozilla.fenix.databinding.FragmentLoginDetailBinding
 import org.mozilla.fenix.settings.logins.LoginsListState
 
 /**
  * View that contains and configures the Login Details
  */
-class LoginDetailView(override val containerView: ViewGroup) : LayoutContainer {
+class LoginDetailsBindingDelegate(
+    private val binding: FragmentLoginDetailBinding
+) {
     fun update(login: LoginsListState) {
-        webAddressText.text = login.currentItem?.origin
-        usernameText.text = login.currentItem?.username
-        passwordText.text = login.currentItem?.password
+        binding.webAddressText.text = login.currentItem?.origin
+        binding.usernameText.text = login.currentItem?.username
+        binding.passwordText.text = login.currentItem?.password
     }
 }
