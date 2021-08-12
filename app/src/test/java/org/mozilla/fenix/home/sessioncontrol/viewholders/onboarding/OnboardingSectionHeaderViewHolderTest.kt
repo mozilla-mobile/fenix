7acd6997f6c948d3133432127f9bdbbb23638c3d
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2021-08-12 16:45:46 +0300

    For #17917: Use View binding in Onboarding Tests

--- app/src/test/java/org/mozilla/fenix/home/sessioncontrol/viewholders/onboarding/OnboardingSectionHeaderViewHolderTest.kt
+++ app/src/test/java/org/mozilla/fenix/home/sessioncontrol/viewholders/onboarding/OnboardingSectionHeaderViewHolderTest.kt
@@ -5,34 +5,32 @@
 package org.mozilla.fenix.home.sessioncontrol.viewholders.onboarding
 
 import android.view.LayoutInflater
-import android.view.View
-import kotlinx.android.synthetic.main.onboarding_section_header.view.*
 import mozilla.components.support.test.robolectric.testContext
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mozilla.fenix.databinding.OnboardingSectionHeaderBinding
 import org.mozilla.fenix.helpers.FenixRobolectricTestRunner
 
 @RunWith(FenixRobolectricTestRunner::class)
 class OnboardingSectionHeaderViewHolderTest {
 
-    private lateinit var view: View
+    private lateinit var binding: OnboardingSectionHeaderBinding
 
     @Before
     fun setup() {
-        view = LayoutInflater.from(testContext)
-            .inflate(OnboardingSectionHeaderViewHolder.LAYOUT_ID, null)
+        binding = OnboardingSectionHeaderBinding.inflate(LayoutInflater.from(testContext))
     }
 
     @Test
     fun `bind text`() {
-        val holder = OnboardingSectionHeaderViewHolder(view)
+        val holder = OnboardingSectionHeaderViewHolder(binding.root)
         holder.bind { "Hello world" }
 
         assertEquals(
             "Hello world",
-            view.section_header_text.text
+            binding.sectionHeaderText.text
         )
     }
 }
