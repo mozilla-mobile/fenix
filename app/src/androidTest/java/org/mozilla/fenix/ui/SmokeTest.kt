77e12f901416400374007783db59ac16b4d3deb9
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2021-08-19 17:58:58 +0300

    For #20716 - Disables Intermittent failing UI test

--- app/src/androidTest/java/org/mozilla/fenix/ui/SmokeTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/SmokeTest.kt
@@ -1129,6 +1129,7 @@ class SmokeTest {
     }
 
     @Test
+    @Ignore("To be re-enabled later. See https://github.com/mozilla-mobile/fenix/issues/20716")
     fun mainMenuInstallPWATest() {
         val pwaPage = "https://mozilla-mobile.github.io/testapp/"
 
