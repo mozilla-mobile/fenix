31103036cec6ab79c236d877724f2925985f9eb0
Elise Richards <erichards@mozilla.com>
2021-08-30 15:39:37 -0700

    Remove banner open in app telemetry

--- app/src/main/java/org/mozilla/fenix/components/metrics/Event.kt
+++ app/src/main/java/org/mozilla/fenix/components/metrics/Event.kt
@@ -179,10 +179,6 @@ sealed class Event {
     object HaveOpenTabs : Event()
     object HaveNoOpenTabs : Event()
 
-    object BannerOpenInAppDisplayed : Event()
-    object BannerOpenInAppDismissed : Event()
-    object BannerOpenInAppGoToSettings : Event()
-
     object ContextMenuCopyTapped : Event()
     object ContextMenuSearchTapped : Event()
     object ContextMenuSelectAllTapped : Event()
