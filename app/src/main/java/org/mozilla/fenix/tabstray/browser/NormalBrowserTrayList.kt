c28598cb723bb29c107f128bb5ead067e67f8a08
Jonathan Almeida <jalmeida@mozilla.com>
2021-10-01 17:55:45 -0400

    Issue #21641: Do not add a group of only one tab

--- app/src/main/java/org/mozilla/fenix/tabstray/browser/NormalBrowserTrayList.kt
+++ app/src/main/java/org/mozilla/fenix/tabstray/browser/NormalBrowserTrayList.kt
@@ -9,18 +9,15 @@ import android.util.AttributeSet
 import androidx.recyclerview.widget.ConcatAdapter
 import mozilla.components.browser.state.state.TabSessionState
 import mozilla.components.browser.tabstray.TabViewHolder
+import mozilla.components.concept.tabstray.Tab
+import mozilla.components.concept.tabstray.TabsTray
 import mozilla.components.feature.tabs.tabstray.TabsFeature
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.settings
+import org.mozilla.fenix.tabstray.TrayPagerAdapter.Companion.TABS_TRAY_FEATURE_NAME
 import org.mozilla.fenix.tabstray.ext.browserAdapter
 import org.mozilla.fenix.tabstray.ext.inactiveTabsAdapter
-import org.mozilla.fenix.tabstray.ext.isNormalTabActive
-import org.mozilla.fenix.tabstray.ext.isNormalTabActiveWithSearchTerm
-import org.mozilla.fenix.tabstray.ext.isNormalTabActiveWithoutSearchTerm
-import org.mozilla.fenix.tabstray.ext.isNormalTabWithoutSearchTerm
-import org.mozilla.fenix.tabstray.ext.isNormalTabWithSearchTerm
 import org.mozilla.fenix.tabstray.ext.isNormalTabInactive
-import org.mozilla.fenix.tabstray.ext.tabGroupAdapter
 import java.util.concurrent.TimeUnit
 
 /**
@@ -39,85 +36,33 @@ class NormalBrowserTrayList @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : AbstractBrowserTrayList(context, attrs, defStyleAttr) {
 
+    private val swipeDelegate = SwipeToDeleteDelegate()
     private val concatAdapter by lazy { adapter as ConcatAdapter }
-
-    override val tabsFeature by lazy {
-        val tabsAdapter = concatAdapter.browserAdapter
-        val inactiveTabsEnabled = context.settings().inactiveTabsAreEnabled
-        val searchTermTabGroupsAreEnabled = context.settings().searchTermTabGroupsAreEnabled
-        val tabFilter: (TabSessionState) -> Boolean = {
-            when {
-                searchTermTabGroupsAreEnabled && inactiveTabsEnabled ->
-                    it.isNormalTabActiveWithoutSearchTerm(maxActiveTime)
-
-                inactiveTabsEnabled -> it.isNormalTabActive(maxActiveTime)
-
-                searchTermTabGroupsAreEnabled -> it.isNormalTabWithoutSearchTerm()
-
-                else -> !it.content.private
-            }
-        }
-
-        TabsFeature(
-            tabsAdapter,
-            context.components.core.store,
-            selectTabUseCase,
-            removeTabUseCase,
-            tabFilter,
-            {}
-        )
-    }
-
-    private val searchTermFeature by lazy {
-        val store = context.components.core.store
-        val inactiveTabsEnabled = context.settings().inactiveTabsAreEnabled
-        val searchTermTabGroupsAreEnabled = context.settings().searchTermTabGroupsAreEnabled
-        val tabFilter: (TabSessionState) -> Boolean = {
-            when {
-                searchTermTabGroupsAreEnabled && inactiveTabsEnabled ->
-                    it.isNormalTabActiveWithSearchTerm(maxActiveTime)
-
-                searchTermTabGroupsAreEnabled -> it.isNormalTabWithSearchTerm()
-
-                else -> false
-            }
-        }
-        val tabsAdapter = concatAdapter.tabGroupAdapter
-
-        TabsFeature(
-            tabsAdapter,
-            store,
-            selectTabUseCase,
-            removeTabUseCase,
-            tabFilter,
-            {}
-        )
-    }
-
-    /**
-     * NB: The setup for this feature is a bit complicated without a better dependency injection
-     * solution to scope it down to just this view.
-     */
-    private val inactiveFeature by lazy {
-        val store = context.components.core.store
+    private val tabSorter by lazy { TabSorter(context, concatAdapter, context.components.core.store) }
+    private val inactiveTabsInteractor by lazy {
         val tabFilter: (TabSessionState) -> Boolean = filter@{
             if (!context.settings().inactiveTabsAreEnabled) {
                 return@filter false
             }
             it.isNormalTabInactive(maxActiveTime)
         }
-        val tabsAdapter = concatAdapter.inactiveTabsAdapter.apply {
-            inactiveTabsInteractor = DefaultInactiveTabsInteractor(
-                InactiveTabsController(store, tabFilter, this, context.components.analytics.metrics)
+        DefaultInactiveTabsInteractor(
+            InactiveTabsController(
+                context.components.core.store,
+                tabFilter,
+                concatAdapter.inactiveTabsAdapter,
+                context.components.analytics.metrics
             )
-        }
+        )
+    }
 
+    override val tabsFeature by lazy {
         TabsFeature(
-            tabsAdapter,
-            store,
+            tabSorter,
+            context.components.core.store,
             selectTabUseCase,
             removeTabUseCase,
-            tabFilter,
+            { !it.content.private },
             {}
         )
     }
@@ -135,9 +80,11 @@ class NormalBrowserTrayList @JvmOverloads constructor(
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
-        inactiveFeature.start()
-        searchTermFeature.start()
+        concatAdapter.inactiveTabsAdapter.inactiveTabsInteractor = inactiveTabsInteractor
+
         tabsFeature.start()
+
+        concatAdapter.browserAdapter.register(swipeDelegate)
 
         touchHelper.attachToRecyclerView(this)
     }
@@ -146,9 +93,22 @@ class NormalBrowserTrayList @JvmOverloads constructor(
         super.onDetachedFromWindow()
 
         tabsFeature.stop()
-        searchTermFeature.stop()
-        inactiveFeature.stop()
+
+        concatAdapter.browserAdapter.unregister(swipeDelegate)
 
         touchHelper.attachToRecyclerView(null)
+    }
+
+    /**
+     * A delegate for handling open/selected events from swipe-to-delete gestures.
+     */
+    inner class SwipeToDeleteDelegate : TabsTray.Observer {
+        override fun onTabClosed(tab: Tab) {
+            removeTabUseCase.invoke(tab.id, TABS_TRAY_FEATURE_NAME)
+        }
+
+        override fun onTabSelected(tab: Tab) {
+            selectTabUseCase.invoke(tab.id)
+        }
     }
 }
