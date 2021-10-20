623914bd8944b7fa290559fc7affbcbbefb6114a
Michael Comella <michael.l.comella@gmail.com>
2021-10-20 16:20:58 -0700

    For #22054: add marker for root view layout/measure.
    The onLayout marker may be redundant to onGlobalLayout marker but I'm not
    sure yet so let's leave them both in and observe if that's the case.
    
    Here's a profile with the markers: https://share.firefox.dev/3lZaOQb

--- app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -211,6 +211,7 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.root.profilerProvider = { components.core.engine.profiler }
         ProfilerMarkers.addListenerForOnGlobalLayout(components.core.engine, this, binding.root)
 
         // Must be after we set the content view
