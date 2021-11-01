fc0c119f7963638849ac1e24a2addc35571adbdd
Jonathan Almeida <jalmeida@mozilla.com>
2021-11-01 16:28:08 -0400

    No issue: Merge our inactive tab interactor/controllers

--- app/src/test/java/org/mozilla/fenix/tabstray/browser/InactiveTabsControllerTest.kt
+++ app/src/test/java/org/mozilla/fenix/tabstray/browser/InactiveTabsControllerTest.kt
@@ -4,8 +4,12 @@
 
 package org.mozilla.fenix.tabstray.browser
 
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.slot
+import io.mockk.spyk
 import io.mockk.verify
 import mozilla.components.browser.state.state.BrowserState
 import mozilla.components.browser.state.state.TabSessionState
@@ -17,9 +21,12 @@ import org.junit.Test
 import org.mozilla.fenix.components.AppStore
 import org.mozilla.fenix.components.metrics.Event
 import org.mozilla.fenix.components.metrics.MetricController
+import org.mozilla.fenix.utils.Settings
 
 class InactiveTabsControllerTest {
 
+    private val metrics: MetricController = mockk(relaxed = true)
+    private val settings: Settings = mockk(relaxed = true)
     private val appStore = AppStore()
 
     @Test
@@ -36,7 +43,8 @@ class InactiveTabsControllerTest {
         )
         val tray: TabsTray = mockk(relaxed = true)
         val tabsSlot = slot<List<TabSessionState>>()
-        val controller = InactiveTabsController(store, appStore, filter, tray, mockk(relaxed = true))
+        val controller =
+            InactiveTabsController(store, appStore, filter, tray, mockk(relaxed = true), settings)
 
         controller.updateCardExpansion(true)
 
@@ -48,10 +56,9 @@ class InactiveTabsControllerTest {
 
     @Test
     fun `WHEN expanded THEN track telemetry event`() {
-        val metrics: MetricController = mockk(relaxed = true)
         val store = BrowserStore(BrowserState())
         val controller = InactiveTabsController(
-            store, appStore, mockk(relaxed = true), mockk(relaxed = true), metrics
+            store, appStore, mockk(relaxed = true), mockk(relaxed = true), metrics, settings
         )
 
         controller.updateCardExpansion(true)
@@ -61,14 +68,51 @@ class InactiveTabsControllerTest {
 
     @Test
     fun `WHEN collapsed THEN track telemetry event`() {
-        val metrics: MetricController = mockk(relaxed = true)
         val store = BrowserStore(BrowserState())
         val controller = InactiveTabsController(
-            store, appStore, mockk(relaxed = true), mockk(relaxed = true), metrics
+            store, appStore, mockk(relaxed = true), mockk(relaxed = true), metrics, settings
         )
 
         controller.updateCardExpansion(false)
 
         verify { metrics.track(Event.TabsTrayInactiveTabsCollapsed) }
+    }
+
+    @Test
+    fun `WHEN close THEN update settings and refresh`() {
+        val store = BrowserStore()
+        val controller = spyk(
+            InactiveTabsController(
+                store, appStore, mockk(relaxed = true), mockk(relaxed = true), metrics, settings
+            )
+        )
+
+        every { controller.refreshInactiveTabsSection() } just Runs
+
+        controller.close()
+
+        verify { metrics.track(Event.TabsTrayAutoCloseDialogDismissed) }
+        verify { settings.hasInactiveTabsAutoCloseDialogBeenDismissed = true }
+        verify { controller.refreshInactiveTabsSection() }
+    }
+
+    @Test
+    fun `WHEN enableAutoClosed THEN update closeTabsAfterOneMonth settings and refresh`() {
+        val filter: (TabSessionState) -> Boolean = { !it.content.private }
+        val store = BrowserStore()
+        val tray: TabsTray = mockk(relaxed = true)
+        val controller =
+            spyk(InactiveTabsAutoCloseDialogController(store, settings, filter, tray, metrics))
+
+        every { controller.refreshInactiveTabsSection() } just Runs
+
+        controller.enableAutoClosed()
+
+        verify { metrics.track(Event.TabsTrayAutoCloseDialogTurnOnClicked) }
+        verify { settings.closeTabsAfterOneMonth = true }
+        verify { settings.closeTabsAfterOneWeek = false }
+        verify { settings.closeTabsAfterOneDay = false }
+        verify { settings.manuallyCloseTabs = false }
+        verify { controller.refreshInactiveTabsSection() }
     }
 }
