9bb49585ea758313b99074ed6ce95387bf13c879
Jonathan Almeida <jalmeida@mozilla.com>
2021-11-01 16:28:08 -0400

    No issue: Merge our inactive tab interactor/controllers

--- app/src/main/java/org/mozilla/fenix/tabstray/browser/InactiveTabsInteractor.kt
+++ app/src/main/java/org/mozilla/fenix/tabstray/browser/InactiveTabsInteractor.kt
@@ -4,14 +4,56 @@
 
 package org.mozilla.fenix.tabstray.browser
 
-interface InactiveTabsInteractor {
+/**
+ * Interactor for all things related to inactive tabs in the tabs tray.
+ */
+interface InactiveTabsInteractor : InactiveTabsAutoCloseDialogInteractor {
+    /**
+     * Invoked when the header is tapped on.
+     *
+     * @param activated true when the tap should expand the inactive section.
+     */
     fun onHeaderClicked(activated: Boolean)
+}
+
+/**
+ * Interactor for the auto-close dialog in the inactive tabs section.
+ */
+interface InactiveTabsAutoCloseDialogInteractor {
+
+    /**
+     * Invoked when the close button is clicked.
+     */
+    fun onCloseClicked()
+
+    /**
+     * Invoked when the dialog is clicked.
+     */
+    fun onEnabledAutoCloseClicked()
 }
 
 class DefaultInactiveTabsInteractor(
     private val controller: InactiveTabsController
 ) : InactiveTabsInteractor {
+
+    /**
+     * See [InactiveTabsInteractor.onHeaderClicked].
+     */
     override fun onHeaderClicked(activated: Boolean) {
         controller.updateCardExpansion(activated)
+    }
+
+    /**
+     * See [InactiveTabsAutoCloseDialogInteractor.onCloseClicked].
+     */
+    override fun onCloseClicked() {
+        controller.close()
+    }
+
+    /**
+     * See [InactiveTabsAutoCloseDialogInteractor.onEnabledAutoCloseClicked].
+     */
+    override fun onEnabledAutoCloseClicked() {
+        controller.enableAutoClosed()
     }
 }
