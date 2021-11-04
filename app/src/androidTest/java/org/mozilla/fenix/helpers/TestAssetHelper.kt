fd70be1ea806ce192a60022b9cfbd78b1740c512
mike a <mavduevskiy@mozilla.com>
2021-11-03 21:45:16 -0700

    For #21816: Filename in "Open" PDF dialog overflows screen
    Changing the download file name length to the max allowed by AS (251 char, won’t compile if more; max would be 260 for latest windows versions, but generally it is 255), and changing the UI test to check if the long file name is fully visible.
    Changing the downloaded dialog layout to properly display really long file names.

--- app/src/androidTest/java/org/mozilla/fenix/helpers/TestAssetHelper.kt
+++ app/src/androidTest/java/org/mozilla/fenix/helpers/TestAssetHelper.kt
@@ -17,6 +17,11 @@ object TestAssetHelper {
     @Suppress("MagicNumber")
     val waitingTime: Long = TimeUnit.SECONDS.toMillis(15)
     val waitingTimeShort: Long = TimeUnit.SECONDS.toMillis(1)
+    // File name length in most operating systems is limited to 255 bytes,
+    // including file extension.
+    // This string is 251 char long though, as Android Studio won't compile
+    // with a resource file having a name longer than 251 chars.
+    const val downloadFileName = "tAJwqaWjJsXS8AhzSninBMCfIZbHBGgcc001lx5DIdDwIcfEgQ6vE5Gb5VgAled17DFZ2A7ZDOHA0NpQPHXXFHPSD4wzCkRWiaOorNI574zLtv4Hjiz6O6T7onmUTGgUQ2YQoiQFyrCrPv8ZB9KvmtoRFRVIZh8Pg2a1THrm9gpMoLwP44nDNN5o70USUyVXFxPeTerG9OSdpZIwCWGIVM3AZmjFSaiAyZKxJp8G1oZ2md4DBQYy6F1.svg"
 
     data class TestAsset(val url: Uri, val content: String, val title: String)
 
@@ -70,7 +75,7 @@ object TestAssetHelper {
 
     fun getDownloadAsset(server: MockWebServer): TestAsset {
         val url = server.url("pages/download.html").toString().toUri()!!
-        val content = "Page content: Globe.svg"
+        val content = "Page content: $downloadFileName"
 
         return TestAsset(url, content, "")
     }
