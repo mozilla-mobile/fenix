bf007234c459d549c5775db8e56235449f1dcb66
Arturo Mejia <arturomejiamarmol@gmail.com>
2021-11-09 18:46:30 -0500

    For #22271 Improve URL accessing from the clipboard for Android 12 and above.
    (cherry picked from commit 94a543a4032feac18a90e3d1449587631a9595fe)

--- app/src/main/java/org/mozilla/fenix/utils/ClipboardHandler.kt
+++ app/src/main/java/org/mozilla/fenix/utils/ClipboardHandler.kt
@@ -7,6 +7,8 @@ package org.mozilla.fenix.utils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.os.Build
+import android.view.textclassifier.TextClassifier
 import androidx.annotation.VisibleForTesting
 import androidx.core.content.getSystemService
 import mozilla.components.support.utils.SafeUrl
@@ -21,6 +23,13 @@ private const val MIME_TYPE_TEXT_HTML = "text/html"
 class ClipboardHandler(val context: Context) {
     private val clipboard = context.getSystemService<ClipboardManager>()!!
 
+    /**
+     * Provides access to the current content of the clipboard, be aware this is a sensitive
+     * API as from Android 12 and above, accessing it will trigger a notification letting the user
+     * know the app has accessed the clipboard, make sure when you call this API that users are
+     * completely aware that we are accessing the clipboard.
+     * See for more details https://github.com/mozilla-mobile/fenix/issues/22271.
+     */
     var text: String?
         get() {
             if (!clipboard.isPrimaryClipEmpty() &&
@@ -37,13 +46,36 @@ class ClipboardHandler(val context: Context) {
             clipboard.setPrimaryClip(ClipData.newPlainText("Text", value))
         }
 
-    val url: String?
-        get() {
-            return text?.let {
-                val finder = WebURLFinder(it)
-                finder.bestWebURL()
-            }
+    /**
+     * Returns a possible URL from the actual content of the clipboard, be aware this is a sensitive
+     * API as from Android 12 and above, accessing it will trigger a notification letting the user
+     * know the app has accessed the clipboard, make sure when you call this API that users are
+     * completely aware that we are accessing the clipboard.
+     * See for more details https://github.com/mozilla-mobile/fenix/issues/22271.
+     */
+    fun extractURL(): String? {
+        return text?.let {
+            val finder = WebURLFinder(it)
+            finder.bestWebURL()
         }
+    }
+
+    @Suppress("MagicNumber")
+    internal fun containsURL(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val description = clipboard.primaryClipDescription
+            // An IllegalStateException is thrown if the url is too long.
+            val score =
+                try {
+                    description?.getConfidenceScore(TextClassifier.TYPE_URL) ?: 0F
+                } catch (e: IllegalStateException) {
+                    0F
+                }
+            score >= 0.7F
+        } else {
+            !extractURL().isNullOrEmpty()
+        }
+    }
 
     private fun ClipboardManager.isPrimaryClipPlainText() =
         primaryClipDescription?.hasMimeType(MIME_TYPE_TEXT_PLAIN) ?: false
