dc0f50b59087a3f7ad0420cdd5ccfee39a8c59db
Arturo Mejia <arturomejiamarmol@gmail.com>
2022-01-06 17:21:35 -0500

    For #22851: Load wallpapers dynamically from the assets directory.

--- app/src/main/java/org/mozilla/fenix/wallpapers/WallpaperManager.kt
+++ app/src/main/java/org/mozilla/fenix/wallpapers/WallpaperManager.kt
@@ -5,18 +5,29 @@
 package org.mozilla.fenix.wallpapers
 
 import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import mozilla.components.support.base.log.logger.Logger
 import mozilla.components.support.ktx.android.content.getColorFromAttr
+import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.asActivity
 import org.mozilla.fenix.utils.Settings
 
 /**
  * Provides access to available wallpapers and manages their states.
  */
-class WallpaperManager(private val settings: Settings) {
+class WallpaperManager(
+    private val settings: Settings,
+    private val wallpaperStorage: WallpaperStorage
+) {
     val logger = Logger("WallpaperManager")
+    var availableWallpapers: List<Wallpaper> = loadWallpapers()
+        private set
+
     var currentWallpaper: Wallpaper = getCurrentWallpaperFromSettings()
         set(value) {
             settings.currentWallpaper = value.name
@@ -27,13 +38,14 @@ class WallpaperManager(private val settings: Settings) {
      * Apply the [newWallpaper] into the [wallpaperContainer] and update the [currentWallpaper].
      */
     fun updateWallpaper(wallpaperContainer: View, newWallpaper: Wallpaper) {
-        if (newWallpaper == Wallpaper.NONE) {
-            val context = wallpaperContainer.context
-            wallpaperContainer.setBackgroundColor(context.getColorFromAttr(newWallpaper.drawable))
+        val context = wallpaperContainer.context
+        if (newWallpaper == defaultWallpaper) {
+            wallpaperContainer.setBackgroundColor(context.getColorFromAttr(DEFAULT_RESOURCE))
             logger.info("Wallpaper update to default background")
         } else {
             logger.info("Wallpaper update to ${newWallpaper.name}")
-            wallpaperContainer.setBackgroundResource(newWallpaper.drawable)
+            val bitmap = loadWallpaperFromAssets(newWallpaper, context)
+            wallpaperContainer.background = BitmapDrawable(context.resources, bitmap)
         }
         currentWallpaper = newWallpaper
 
@@ -41,7 +53,7 @@ class WallpaperManager(private val settings: Settings) {
     }
 
     private fun adjustTheme(context: Context) {
-        val mode = if (currentWallpaper != Wallpaper.NONE) {
+        val mode = if (currentWallpaper != defaultWallpaper) {
             if (currentWallpaper.isDark) {
                 updateThemePreference(useDarkTheme = true)
                 logger.info("theme changed to useDarkTheme")
@@ -79,7 +91,7 @@ class WallpaperManager(private val settings: Settings) {
      * the first available [Wallpaper] will be returned.
      */
     fun switchToNextWallpaper(): Wallpaper {
-        val values = Wallpaper.values()
+        val values = availableWallpapers
         val index = values.indexOf(currentWallpaper) + 1
 
         return if (index >= values.size) {
@@ -92,9 +104,43 @@ class WallpaperManager(private val settings: Settings) {
     private fun getCurrentWallpaperFromSettings(): Wallpaper {
         val currentWallpaper = settings.currentWallpaper
         return if (currentWallpaper.isEmpty()) {
-            Wallpaper.NONE
+            defaultWallpaper
         } else {
-            Wallpaper.valueOf(currentWallpaper)
+            availableWallpapers.find { it.name == currentWallpaper } ?: defaultWallpaper
         }
+    }
+
+    fun loadWallpaperFromAssets(wallpaper: Wallpaper, context: Context): Bitmap {
+        val path = if (isLandscape(context)) {
+            wallpaper.landscapePath
+        } else {
+            wallpaper.portraitPath
+        }
+        return context.assets.open(path).use {
+            BitmapFactory.decodeStream(it)
+        }
+    }
+
+    private fun isLandscape(context: Context): Boolean {
+        return context.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+    }
+
+    private fun loadWallpapers(): List<Wallpaper> {
+        val wallpapersFromStorage = wallpaperStorage.loadAll()
+        return if (wallpapersFromStorage.isNotEmpty()) {
+            listOf(defaultWallpaper) + wallpapersFromStorage
+        } else {
+            listOf(defaultWallpaper)
+        }
+    }
+
+    companion object {
+        const val DEFAULT_RESOURCE = R.attr.homeBackground
+        val defaultWallpaper = Wallpaper(
+            name = "default_wallpaper",
+            portraitPath = "",
+            landscapePath = "",
+            isDark = false
+        )
     }
 }
