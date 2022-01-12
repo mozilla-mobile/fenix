5e6f50e3f695e243906fc7768f6bb431436102be
Grigory Kruglov <grigory@kruglov.ca>
2022-01-11 16:01:32 -0800

    For #22795: Stop trimming login origins on sorting
    Due to the async nature (??) of the trimming code, this is causing severe performance issues
    during search.
    
    Looking back through commits, doesn't seem like there's a particularly good reason we were trimming here. All I could find is #9986 (comment) which is lacking explanation of why this is actually useful.
    
    And currently, we're dealing with an origin (not a full url when this was initially written, I think), i.e. https://accounts.firefox.com vs https://accounts.firefox.com/signin. So, the suffix stripping isn't even doing much beyond removing com in vast majority of cases.
    
    So, seems like all of this trimming stuff can be cleaned up.

--- app/src/test/java/org/mozilla/fenix/settings/logins/LoginsListControllerTest.kt
+++ app/src/test/java/org/mozilla/fenix/settings/logins/LoginsListControllerTest.kt
@@ -7,8 +7,6 @@ package org.mozilla.fenix.settings.logins
 import androidx.navigation.NavController
 import io.mockk.mockk
 import io.mockk.verifyAll
-import mozilla.components.lib.publicsuffixlist.PublicSuffixList
-import mozilla.components.support.test.robolectric.testContext
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mozilla.fenix.BrowserDirection
@@ -24,8 +22,7 @@ import org.mozilla.fenix.utils.Settings
 class LoginsListControllerTest {
     private val store: LoginsFragmentStore = mockk(relaxed = true)
     private val settings: Settings = mockk(relaxed = true)
-    private val publicSuffixList = PublicSuffixList(testContext)
-    private val sortingStrategy: SortingStrategy = SortingStrategy.Alphabetically(publicSuffixList)
+    private val sortingStrategy: SortingStrategy = SortingStrategy.Alphabetically
     private val navController: NavController = mockk(relaxed = true)
     private val browserNavigator: (String, Boolean, BrowserDirection) -> Unit = mockk(relaxed = true)
     private val metrics: MetricController = mockk(relaxed = true)
@@ -43,11 +40,7 @@ class LoginsListControllerTest {
         controller.handleSort(sortingStrategy)
 
         verifyAll {
-            store.dispatch(
-                LoginsAction.SortLogins(
-                    SortingStrategy.Alphabetically(publicSuffixList)
-                )
-            )
+            store.dispatch(LoginsAction.SortLogins(SortingStrategy.Alphabetically))
             settings.savedLoginsSortingStrategy = sortingStrategy
         }
     }
