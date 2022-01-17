12a9071fc44ab6b0ea2c7712e1e3155c8cae66b6
Christian Sadilek <christian.sadilek@gmail.com>
2022-01-17 16:57:28 -0500

    Issue #22256: Ignore intermittent editCustomSearchEngineTest

--- app/src/androidTest/java/org/mozilla/fenix/ui/SmokeTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/SmokeTest.kt
@@ -521,6 +521,7 @@ class SmokeTest {
 
     @Test
     // Verifies setting as default a customized search engine name and URL
+    @Ignore("Failing intermittently https://github.com/mozilla-mobile/fenix/issues/22256")
     fun editCustomSearchEngineTest() {
         val searchEngine = object {
             var title = "Elefant"
