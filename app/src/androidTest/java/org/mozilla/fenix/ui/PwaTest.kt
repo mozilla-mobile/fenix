bd13bf0919273a74fb8458e07cc2f93a752f17af
AndiAJ <andiaj@users.noreply.github.com>
2022-01-26 15:34:18 +0200

    Clock and Calendar PWA tests

--- app/src/androidTest/java/org/mozilla/fenix/ui/PwaTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/PwaTest.kt
@@ -13,13 +13,17 @@ import org.mozilla.fenix.helpers.HomeActivityIntentTestRule
 import org.mozilla.fenix.helpers.TestHelper.assertNativeAppOpens
 import org.mozilla.fenix.ui.robots.customTabScreen
 import org.mozilla.fenix.ui.robots.navigationToolbar
+import org.mozilla.fenix.ui.robots.pwaScreen
 
 class PwaTest {
     private val featureSettingsHelper = FeatureSettingsHelper()
     private val externalLinksPWAPage = "https://mozilla-mobile.github.io/testapp/externalLinks.html"
+    private val clockAndCalendarPWAPage = "https://andiaj.github.io/testapp/clockAndCalendarForm.html"
     private val emailLink = "mailto://example@example.com"
     private val phoneLink = "tel://1234567890"
     private val shortcutTitle = "TEST_APP"
+    private val hour = 10
+    private val minute = 10
 
     @get:Rule
     val activityTestRule = HomeActivityIntentTestRule()
@@ -81,6 +85,92 @@ class PwaTest {
         }.openHomeScreenShortcut(shortcutTitle) {
             clickLinkMatchingText("Telephone link")
             assertNativeAppOpens(PHONE_APP, phoneLink)
+        }
+    }
+
+    @SmokeTest
+    @Test
+    fun cancelCalendarFormPWATest() {
+
+        navigationToolbar {
+        }.enterURLAndEnterToBrowser(clockAndCalendarPWAPage.toUri()) {
+        }.openThreeDotMenu {
+        }.clickInstall {
+            clickAddAutomaticallyButton()
+        }.openHomeScreenShortcut(shortcutTitle) {
+        }
+        pwaScreen {
+            clickForm("Calendar Form", true)
+            clickClockAndCalendarViewButton("CANCEL")
+            clickSubmitDateButton()
+            verifyNoDateIsSelected()
+        }
+    }
+
+    @SmokeTest
+    @Test
+    fun setAndClearCalendarFormPWATest() {
+
+        navigationToolbar {
+        }.enterURLAndEnterToBrowser(clockAndCalendarPWAPage.toUri()) {
+        }.openThreeDotMenu {
+        }.clickInstall {
+            clickAddAutomaticallyButton()
+        }.openHomeScreenShortcut(shortcutTitle) {
+        }
+        pwaScreen {
+            clickForm("Calendar Form", true)
+            selectDate()
+            clickClockAndCalendarViewButton("OK")
+            clickSubmitDateButton()
+            verifySelectedDate()
+            clickForm("Calendar Form", true)
+            clickClockAndCalendarViewButton("CLEAR")
+            clickSubmitDateButton()
+            verifyNoDateIsSelected()
+        }
+    }
+
+    @SmokeTest
+    @Test
+    fun cancelClockFormPWATest() {
+
+        navigationToolbar {
+        }.enterURLAndEnterToBrowser(clockAndCalendarPWAPage.toUri()) {
+        }.openThreeDotMenu {
+        }.clickInstall {
+            clickAddAutomaticallyButton()
+        }.openHomeScreenShortcut(shortcutTitle) {
+        }
+        pwaScreen {
+            clickForm("Clock Form", false)
+            clickClockAndCalendarViewButton("CANCEL")
+            clickSubmitTimeButton()
+            verifyNoTimeIsSelected(hour, minute)
+        }
+    }
+
+    @SmokeTest
+    @Test
+    fun setAndClearClockFormPWATest() {
+
+        navigationToolbar {
+        }.enterURLAndEnterToBrowser(clockAndCalendarPWAPage.toUri()) {
+        }.openThreeDotMenu {
+        }.clickInstall {
+            clickAddAutomaticallyButton()
+        }.openHomeScreenShortcut(shortcutTitle) {
+        }
+        pwaScreen {
+            clickForm("Clock Form", false)
+            selectTime(hour, minute)
+            clickClockAndCalendarViewButton("OK")
+            clickSubmitTimeButton()
+            verifySelectedTime(hour, minute)
+            clickForm("Clock Form", false)
+            clickClockAndCalendarViewButton("CLEAR")
+            clickSubmitTimeButton()
+            verifyNoTimeIsSelected(hour, minute)
         }
     }
 }
---
+++ app/src/androidTest/java/org/mozilla/fenix/ui/robots/PwaRobot.kt
@@ -0,0 +1,174 @@
+package org.mozilla.fenix.ui.robots
+
+import android.util.Log
+import android.widget.TimePicker
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.contrib.PickerActions
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.uiautomator.UiSelector
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.mozilla.fenix.helpers.TestAssetHelper.waitingTime
+import org.mozilla.fenix.helpers.TestHelper
+import org.mozilla.fenix.helpers.TestHelper.packageName
+import java.time.LocalDate
+
+class PwaRobot {
+
+    fun clickForm(formType: String, calendarForm: Boolean = false) {
+        mDevice.findObject(UiSelector().resourceId("$packageName:id/engineView"))
+            .waitForExists(waitingTime)
+        mDevice.findObject(UiSelector().textContains(formType)).waitForExists(waitingTime)
+
+        if (calendarForm) {
+            calendarBox.click()
+        } else {
+            clockBox.click()
+        }
+    }
+
+    fun clickClockAndCalendarViewButton(button: String) {
+        val clockAndCalendarButton = mDevice.findObject(UiSelector().textContains(button))
+        clockAndCalendarButton.click()
+    }
+
+    fun selectDate() {
+        mDevice.findObject(UiSelector().resourceId("android:id/month_view")).waitForExists(waitingTime)
+
+        val monthViewerCurrentDay =
+            mDevice.findObject(
+                UiSelector()
+                    .textContains("$currentDay")
+                    .descriptionContains("$currentDay $currentMonth $currentYear")
+            )
+
+        monthViewerCurrentDay.click()
+    }
+
+    fun selectTime(hour: Int, minute: Int) =
+        onView(isAssignableFrom(TimePicker::class.java)).perform(PickerActions.setTime(hour, minute))
+
+    fun clickSubmitDateButton() {
+        submitDateButton.waitForExists(waitingTime)
+        submitDateButton.click()
+    }
+
+    fun clickSubmitTimeButton() {
+        submitTimeButton.waitForExists(waitingTime)
+        submitTimeButton.click()
+    }
+
+    fun verifySelectedDate() {
+        mDevice.findObject(
+            UiSelector()
+                .textContains("Submit date")
+                .resourceId("submitDate")
+        ).waitForExists(waitingTime)
+
+        assertTrue(
+            mDevice.findObject(
+                UiSelector()
+                    .text("Selected date is: $currentDate")
+            ).waitForExists(waitingTime)
+        )
+    }
+
+    fun verifyNoDateIsSelected() {
+        mDevice.findObject(
+            UiSelector()
+                .textContains("Submit date")
+                .resourceId("submitDate")
+        ).waitForExists(waitingTime)
+
+        assertFalse(
+            mDevice.findObject(
+                UiSelector()
+                    .text("Selected date is: $currentDate")
+            ).waitForExists(waitingTime)
+        )
+    }
+
+    fun verifySelectedTime(hour: Int, minute: Int) {
+        mDevice.findObject(
+            UiSelector()
+                .textContains("Submit date")
+                .resourceId("submitDate")
+        ).waitForExists(waitingTime)
+
+        Log.i("Andi", "Selected time is: $hour:$minute")
+
+        assertTrue(
+            mDevice.findObject(
+                UiSelector()
+                    .text("Selected time is: $hour:$minute")
+            ).waitForExists(waitingTime)
+        )
+    }
+
+    fun verifyNoTimeIsSelected(hour: Int, minute: Int) {
+        mDevice.findObject(
+            UiSelector()
+                .textContains("Submit date")
+                .resourceId("submitDate")
+        ).waitForExists(waitingTime)
+
+        assertFalse(
+            mDevice.findObject(
+                UiSelector()
+                    .text("Selected date is: $hour:$minute")
+            ).waitForExists(waitingTime)
+        )
+    }
+
+    class Transition
+}
+
+fun pwaScreen(interact: PwaRobot.() -> Unit): PwaRobot.Transition {
+    PwaRobot().interact()
+    return PwaRobot.Transition()
+}
+
+val calendarBox =
+    mDevice.findObject(
+        UiSelector()
+            .index(0)
+            .resourceId("calendar")
+            .className("android.widget.Spinner")
+            .packageName("${TestHelper.packageName}")
+    )
+
+val clockBox =
+    mDevice.findObject(
+        UiSelector()
+            .index(0)
+            .resourceId("clock")
+            .className("android.view.View")
+            .packageName("${TestHelper.packageName}")
+    )
+
+val currentDate = LocalDate.now()
+val currentDay = currentDate.dayOfMonth
+val currentMonth = currentDate.month
+val currentYear = currentDate.year
+
+val submitDateButton =
+    mDevice.findObject(
+        UiSelector()
+            .textContains("Submit date")
+            .resourceId("submitDate")
+    )
+
+val submitTimeButton =
+    mDevice.findObject(
+        UiSelector()
+            .textContains("Submit time")
+            .resourceId("submitTime")
+    )
+
+val amClockButton =
+    mDevice.findObject(
+        UiSelector()
+            .index(0)
+            .textContains("AM")
+            .className("android.widget.RadioButton")
+    )
