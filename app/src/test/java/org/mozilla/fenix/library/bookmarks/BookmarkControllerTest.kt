e9b4089e7f187689b3d720eb07d298c6460e019d
czlucius <58442255+czlucius@users.noreply.github.com>
2022-01-27 19:18:25 +0800

    Change test name for handleBookmarkTapped

--- app/src/test/java/org/mozilla/fenix/library/bookmarks/BookmarkControllerTest.kt
+++ app/src/test/java/org/mozilla/fenix/library/bookmarks/BookmarkControllerTest.kt
@@ -110,7 +110,7 @@ class BookmarkControllerTest {
     }
 
     @Test
-    fun `handleBookmarkTapped should load the bookmark in a new tab`() {
+    fun `handleBookmarkTapped should load the bookmark in the current tab`() {
         var invokePendingDeletionInvoked = false
         val flags = EngineSession.LoadUrlFlags.select(EngineSession.LoadUrlFlags.ALLOW_JAVASCRIPT_URL)
 
