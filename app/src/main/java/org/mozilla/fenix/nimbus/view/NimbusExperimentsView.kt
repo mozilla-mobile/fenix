cc6608f563f1363daf16b1fe5fdb745f9e0c7d71
Elise Richards <erichards@mozilla.com>
2022-01-31 17:14:27 -0800

    For #23503: Only enable experiment enrollment when studies are enabled

--- app/src/main/java/org/mozilla/fenix/nimbus/view/NimbusExperimentsView.kt
+++ app/src/main/java/org/mozilla/fenix/nimbus/view/NimbusExperimentsView.kt
@@ -4,25 +4,39 @@
 
 package org.mozilla.fenix.nimbus.view
 
+import android.content.Context
 import androidx.navigation.NavController
 import mozilla.components.service.nimbus.ui.NimbusExperimentsAdapterDelegate
 import org.mozilla.experiments.nimbus.AvailableExperiment
+import org.mozilla.fenix.R
+import org.mozilla.fenix.components.FenixSnackbar
+import org.mozilla.fenix.ext.getRootView
+import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.nimbus.NimbusExperimentsFragmentDirections
+import org.mozilla.fenix.tabstray.ext.make
 
 /**
  * View used for managing Nimbus experiments.
  */
 class NimbusExperimentsView(
+    private val context: Context,
     private val navController: NavController
 ) : NimbusExperimentsAdapterDelegate {
 
     override fun onExperimentItemClicked(experiment: AvailableExperiment) {
-        val directions =
-            NimbusExperimentsFragmentDirections.actionNimbusExperimentsFragmentToNimbusBranchesFragment(
-                experimentId = experiment.slug,
-                experimentName = experiment.userFacingName
-            )
+        if (context.settings().isExperimentationEnabled) {
+            val directions =
+                NimbusExperimentsFragmentDirections.actionNimbusExperimentsFragmentToNimbusBranchesFragment(
+                    experimentId = experiment.slug,
+                    experimentName = experiment.userFacingName
+                )
 
-        navController.navigate(directions)
+            navController.navigate(directions)
+        } else {
+            val snackbarText = context.getString(R.string.experiments_snackbar)
+            FenixSnackbar.make(view = context.getRootView()!!)
+                .setText(snackbarText)
+                .show()
+        }
     }
 }
