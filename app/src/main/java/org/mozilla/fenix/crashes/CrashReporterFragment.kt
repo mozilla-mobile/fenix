bc59ede073e0a8d0ed3b639061230361384562df
Mugurell <Mugurell@users.noreply.github.com>
2022-02-03 11:21:22 +0200

    For #22722 - Reacting to the crashed flag
    Whenever the ".crashed" property of the currently displayed
    TabSessionState -> EngineState is true we will show an in-app crash reporter
    with the usual close tab / restore tab options and also the option to report
    all current non-fatal crashes to Mozilla if the setting for sending the crash
    reports is enabled in app settings.
    This closely mimics the previous crash reporter UI but there might be some
    subtle differences stemming from migrating to using a ComposeView.
    
    Whenever the ".crashed" property of the currently displayed
    TabSessionState -> EngineState is false we will set the in-app crash reporter
    to have a View.GONE visibility effectively removing it from the layout.
    
    The functionality for receiving the non-fatal crashes from the AC CrashReporter
    through an Intent is still kept and these crashes will be persisted in memory
    until the user closes / restores a tab and so also makes a decision about
    sending or not these crashes.
    
    Currently more tabs can crash following just one since more share the same
    process and as such there is no way to differentiate between them or link a
    certain Crash to a certain tab.
    They will all be acted upon at once from any tab the user chooses to close or
    restore.

--- app/src/main/java/org/mozilla/fenix/crashes/CrashReporterFragment.kt
+++ app/src/main/java/org/mozilla/fenix/crashes/CrashReporterFragment.kt
@@ -4,41 +4,67 @@
 
 package org.mozilla.fenix.crashes
 
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import mozilla.components.lib.crash.Crash
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.VisibleForTesting
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.mozilla.fenix.R
 import org.mozilla.fenix.databinding.FragmentCrashReporterBinding
-import org.mozilla.fenix.ext.hideToolbar
 import org.mozilla.fenix.ext.increaseTapArea
-import org.mozilla.fenix.ext.requireComponents
-import org.mozilla.fenix.ext.settings
 
 /**
  * Fragment shown when a tab crashes.
  */
-class CrashReporterFragment : Fragment(R.layout.fragment_crash_reporter) {
+class CrashReporterFragment @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    @VisibleForTesting
+    internal lateinit var binding: FragmentCrashReporterBinding
+    @VisibleForTesting val isBindingInitialized
+        get() = ::binding.isInitialized
+    @VisibleForTesting
+    internal lateinit var controller: CrashReporterController
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    /**
+     * Inflate if necessary and show this `View`.
+     *
+     * @param controller [CrashReporterController] delegated for all user interactions with this `View`.
+     */
+    fun show(controller: CrashReporterController) {
+        this.controller = controller
+        inflateViewIfNecessary()
+        visibility = VISIBLE
+    }
 
-        val binding = FragmentCrashReporterBinding.bind(view)
+    /**
+     * Remove this View from layout.
+     */
+    fun hide() {
+        visibility = GONE
+    }
 
-        val args: CrashReporterFragmentArgs by navArgs()
-        val crash = Crash.fromIntent(args.crashIntent)
+    @VisibleForTesting
+    internal fun inflateViewIfNecessary() {
+        if (isBindingInitialized) {
+            return
+        }
 
-        binding.title.text = getString(R.string.tab_crash_title_2, getString(R.string.app_name))
+        inflate()
+        bindViews()
+    }
 
-        val controller = CrashReporterController(
-            crash,
-            sessionId = requireComponents.core.store.state.selectedTabId,
-            navController = findNavController(),
-            components = requireComponents,
-            settings = requireContext().settings()
-        )
+    @VisibleForTesting
+    internal fun inflate() {
+        binding = FragmentCrashReporterBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    @VisibleForTesting
+    internal fun bindViews() {
+        binding.title.text =
+            context.getString(R.string.tab_crash_title_2, context.getString(R.string.app_name))
 
         binding.restoreTabButton.apply {
             increaseTapArea(TAP_INCREASE_DP)
@@ -55,12 +81,8 @@ class CrashReporterFragment : Fragment(R.layout.fragment_crash_reporter) {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        hideToolbar()
-    }
-
     companion object {
-        private const val TAP_INCREASE_DP = 12
+        @VisibleForTesting
+        internal const val TAP_INCREASE_DP = 12
     }
 }
---
+++ app/src/test/java/org/mozilla/fenix/crashes/CrashReporterFragmentTest.kt
@@ -0,0 +1,101 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.crashes
+
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.spyk
+import io.mockk.verify
+import mozilla.components.support.test.robolectric.testContext
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mozilla.fenix.R
+import org.mozilla.fenix.crashes.CrashReporterFragment.Companion.TAP_INCREASE_DP
+import org.mozilla.fenix.ext.increaseTapArea
+import org.mozilla.fenix.helpers.FenixRobolectricTestRunner
+
+@RunWith(FenixRobolectricTestRunner::class)
+class CrashReporterFragmentTest {
+    @Test
+    fun `WHEN show is called THEN remember the controller, inflate and display the View`() {
+        val view = spyk(CrashReporterFragment(testContext))
+        val controller: CrashReporterController = mockk()
+
+        view.show(controller)
+
+        assertTrue(view.controller === controller)
+        verify {
+            view.inflateViewIfNecessary()
+            view.visibility = VISIBLE
+        }
+    }
+
+    @Test
+    fun `WHEN hide is called THEN remove the View from layout`() {
+        val view = spyk(CrashReporterFragment(testContext))
+
+        view.hide()
+
+        verify { view.visibility = GONE }
+    }
+
+    @Test
+    fun `GIVEN the View is not shown WHEN needing to be shown THEN inflate the layout and bind all widgets`() {
+        val controller: CrashReporterController = mockk(relaxed = true)
+        val view = CrashReporterFragment(testContext)
+        view.controller = controller
+        assertFalse(view.isBindingInitialized)
+
+        mockkStatic("org.mozilla.fenix.ext.ViewKt") {
+            view.inflateViewIfNecessary()
+
+            assertTrue(view.isBindingInitialized)
+            assertEquals(
+                testContext.getString(R.string.tab_crash_title_2, testContext.getString(R.string.app_name)),
+                view.binding.title.text
+            )
+            verify {
+                view.binding.restoreTabButton.increaseTapArea(TAP_INCREASE_DP)
+                view.binding.closeTabButton.increaseTapArea(TAP_INCREASE_DP)
+            }
+
+            view.binding.sendCrashCheckbox.isChecked = true
+            view.binding.restoreTabButton.callOnClick()
+            verify { controller.handleCloseAndRestore(true) }
+
+            view.binding.sendCrashCheckbox.isChecked = false
+            view.binding.closeTabButton.callOnClick()
+            verify { controller.handleCloseAndRemove(false) }
+        }
+    }
+
+    @Test
+    fun `GIVEN the View is not shown WHEN needing to be shown THEN delegate the process to helper methods`() {
+        val view = spyk(CrashReporterFragment(testContext))
+
+        view.inflateViewIfNecessary()
+
+        verify {
+            view.inflate()
+            view.bindViews()
+        }
+    }
+
+    @Test
+    fun `GIVEN the View is to already shown WHEN needing to be shown again THEN return early and avoid duplicating the widgets setup`() {
+        val view = spyk(CrashReporterFragment(testContext))
+        view.inflate() // mock that the View is already inflated
+
+        view.inflateViewIfNecessary() // try inflating it again
+
+        verify(exactly = 1) { view.inflate() }
+        verify(exactly = 0) { view.bindViews() }
+    }
+}
