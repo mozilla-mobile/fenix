534838c0fbfe22ea5a48dfe77e613e3051c042e0
AndiAJ <andiaj@users.noreply.github.com>
2022-02-08 09:13:57 +0200

    Add and remove most visited top sites UI test

--- app/src/androidTest/java/org/mozilla/fenix/ui/TopSitesTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/TopSitesTest.kt
@@ -11,10 +11,12 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mozilla.fenix.customannotations.SmokeTest
 import org.mozilla.fenix.helpers.AndroidAssetDispatcher
 import org.mozilla.fenix.helpers.FeatureSettingsHelper
 import org.mozilla.fenix.helpers.HomeActivityIntentTestRule
 import org.mozilla.fenix.helpers.TestAssetHelper
+import org.mozilla.fenix.ui.robots.browserScreen
 import org.mozilla.fenix.ui.robots.homeScreen
 import org.mozilla.fenix.ui.robots.navigationToolbar
 
@@ -181,6 +183,31 @@ class TopSitesTest {
             defaultTopSites.forEach { item ->
                 verifyExistingTopSitesTabs(item)
             }
+        }
+    }
+
+    @SmokeTest
+    @Test
+    fun addAndRemoveMostViewedTopSiteTest() {
+        val defaultWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
+
+        for (i in 0..1) {
+            navigationToolbar {
+            }.enterURLAndEnterToBrowser(defaultWebPage.url) {
+                mDevice.waitForIdle()
+                waitForPageToLoad()
+            }
+        }
+
+        browserScreen {
+        }.goToHomescreen {
+            verifyExistingTopSitesList()
+            verifyExistingTopSitesTabs(defaultWebPage.title)
+        }.openContextMenuOnTopSitesWithTitle(defaultWebPage.title) {
+        }.deleteTopSiteFromHistory {
+        }.openThreeDotMenu {
+        }.openHistory {
+            verifyEmptyHistoryView()
         }
     }
 }
