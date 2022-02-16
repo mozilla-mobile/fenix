ef13b7dcd2bcdbc84bcee2d815edfcb91ead7fc9
Roger Yang <royang@mozilla.com>
2022-02-16 18:34:48 -0500

    No issue: Renew/remove metrics set to expire in April 2022 (#23786)
    (cherry picked from commit 20094cd1e9ea5d91aa32b432ff02d6581c7e538c)

--- app/src/main/java/org/mozilla/fenix/components/metrics/Event.kt
+++ app/src/main/java/org/mozilla/fenix/components/metrics/Event.kt
@@ -265,11 +265,8 @@ sealed class Event {
     object OpenedTabSuggestionClicked : Event()
 
     // Set default browser experiment metrics
-    object SetDefaultBrowserNewTabClicked : Event()
-    object CloseExperimentCardClicked : Event()
     object ToolbarMenuShown : Event()
     object SetDefaultBrowserToolbarMenuClicked : Event()
-    object SetDefaultBrowserSettingsScreenClicked : Event()
 
     // Home menu interaction
     object HomeMenuSettingsItemClicked : Event()
