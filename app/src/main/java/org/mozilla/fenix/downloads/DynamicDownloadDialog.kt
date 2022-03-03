094cd63242f175d2019351a6129e406d56aea859
mcarare <mihai.carare.dev@gmail.com>
2022-03-03 15:47:07 +0200

    For #24075: Add PDF download metrics.

--- app/src/main/java/org/mozilla/fenix/downloads/DynamicDownloadDialog.kt
+++ app/src/main/java/org/mozilla/fenix/downloads/DynamicDownloadDialog.kt
@@ -108,8 +108,6 @@ class DynamicDownloadDialog(
                         onCannotOpenFile(downloadState)
                     }
 
-
-
                     dismiss()
                 }
             }
