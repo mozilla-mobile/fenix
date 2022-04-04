b20a16f2799e3bdba3746b2cb68e9cea5313977a
AndiAJ <andiaj@users.noreply.github.com>
2022-04-04 14:36:54 +0300

    For #24462 #24376 #24377 #20418 re-enable top sites UI tests

--- app/src/androidTest/java/org/mozilla/fenix/ui/TopSitesTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/TopSitesTest.kt
@@ -9,7 +9,6 @@ import androidx.test.uiautomator.UiDevice
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.customannotations.SmokeTest
@@ -54,7 +53,6 @@ class TopSitesTest {
         featureSettingsHelper.resetAllFeatureFlags()
     }
 
-    @Ignore("Failing, see: https://github.com/mozilla-mobile/fenix/issues/24462")
     @SmokeTest
     @Test
     fun verifyAddToFirefoxHome() {
@@ -102,7 +100,6 @@ class TopSitesTest {
         mDevice.pressBack()
     }
 
-    @Ignore("Failing, see: https://github.com/mozilla-mobile/fenix/issues/24376")
     @Test
     fun verifyOpenTopSitePrivateTab() {
         val defaultWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
@@ -125,7 +122,6 @@ class TopSitesTest {
         }
     }
 
-    @Ignore("Failing, see: https://github.com/mozilla-mobile/fenix/issues/24377")
     @Test
     fun verifyRenameTopSite() {
         val defaultWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
@@ -150,7 +146,6 @@ class TopSitesTest {
         }
     }
 
-    @Ignore("Failing, see: https://github.com/mozilla-mobile/fenix/issues/20418")
     @Test
     fun verifyRemoveTopSite() {
         val defaultWebPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
