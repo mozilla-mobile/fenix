5c81b87b9e3dec52b64284a9062fd3b324bdb84c
AndiAJ <andiaj@users.noreply.github.com>
2022-04-19 14:56:43 +0300

    Re-enable selectAndSearchTextTest UI test

--- app/src/androidTest/java/org/mozilla/fenix/ui/ContextMenusTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/ContextMenusTest.kt
@@ -9,7 +9,6 @@ import androidx.test.uiautomator.UiDevice
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.customannotations.SmokeTest
@@ -238,7 +237,6 @@ class ContextMenusTest {
         }
     }
 
-    @Ignore("Failing, see: https://github.com/mozilla-mobile/fenix/issues/24457")
     @SmokeTest
     @Test
     fun selectAndSearchTextTest() {
