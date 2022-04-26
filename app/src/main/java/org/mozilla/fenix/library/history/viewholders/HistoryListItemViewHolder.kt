8e9464fee3b0f8fb78a8912b5b01827435f2c2b6
mike a <mavduevskiy@gmail.com>
2022-04-26 11:20:10 -0700

    Closes #24307: add synced history empty fragment, feature flag and history screen showing only local files

--- app/src/main/java/org/mozilla/fenix/library/history/viewholders/HistoryListItemViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/library/history/viewholders/HistoryListItemViewHolder.kt
@@ -7,6 +7,7 @@ package org.mozilla.fenix.library.history.viewholders
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import org.mozilla.fenix.FeatureFlags
 import org.mozilla.fenix.R
 import org.mozilla.fenix.databinding.HistoryListItemBinding
 import org.mozilla.fenix.ext.components
@@ -31,6 +32,10 @@ class HistoryListItemViewHolder(
     init {
         binding.recentlyClosedNavEmpty.recentlyClosedNav.setOnClickListener {
             historyInteractor.onRecentlyClosedClicked()
+        }
+
+        binding.syncedHistoryNavEmpty.syncedHistoryNav.setOnClickListener {
+            historyInteractor.onSyncedHistoryClicked()
         }
 
         binding.historyLayout.overflowView.apply {
@@ -113,11 +118,15 @@ class HistoryListItemViewHolder(
         }
     }
 
+    @Suppress("NestedBlockDepth")
     private fun toggleTopContent(
         showTopContent: Boolean,
         isNormalMode: Boolean,
     ) {
         binding.recentlyClosedNavEmpty.recentlyClosedNav.isVisible = showTopContent
+        binding.topSpacer.isVisible = showTopContent
+        binding.bottomSpacer.isVisible = showTopContent
+        binding.syncedHistoryNavEmpty.syncedHistoryNav.isVisible = showTopContent && FeatureFlags.showSyncedHistory
 
         if (showTopContent) {
             val numRecentTabs = itemView.context.components.core.store.state.closedTabs.size
@@ -131,6 +140,7 @@ class HistoryListItemViewHolder(
                 ),
                 numRecentTabs
             )
+
             binding.recentlyClosedNavEmpty.recentlyClosedNav.run {
                 if (isNormalMode) {
                     isEnabled = true
@@ -138,6 +148,18 @@ class HistoryListItemViewHolder(
                 } else {
                     isEnabled = false
                     alpha = DISABLED_BUTTON_ALPHA
+                }
+            }
+
+            if (FeatureFlags.showSyncedHistory) {
+                binding.syncedHistoryNavEmpty.syncedHistoryNav.run {
+                    if (isNormalMode) {
+                        isEnabled = true
+                        alpha = 1f
+                    } else {
+                        isEnabled = false
+                        alpha = DISABLED_BUTTON_ALPHA
+                    }
                 }
             }
         }
