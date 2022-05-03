a4a78717afdf834fae7981f2f597c4b1277bf0a7
mcarare <mihai.carare.dev@gmail.com>
2022-05-03 16:57:47 +0300

    For #24919: Add option menu in the address editor.

--- app/src/main/java/org/mozilla/fenix/settings/address/AddressEditorFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/address/AddressEditorFragment.kt
@@ -5,6 +5,9 @@
 package org.mozilla.fenix.settings.address
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -36,6 +39,8 @@ class AddressEditorFragment : SecureFragment(R.layout.fragment_address_editor) {
     private val isEditing: Boolean
         get() = args.address != null
 
+    private lateinit var menu: Menu
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -50,9 +55,15 @@ class AddressEditorFragment : SecureFragment(R.layout.fragment_address_editor) {
         )
 
         val binding = FragmentAddressEditorBinding.bind(view)
+        setHasOptionsMenu(true)
 
         addressEditorView = AddressEditorView(binding, interactor, args.address)
         addressEditorView.bind()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        menu.close()
     }
 
     override fun onResume() {
@@ -67,5 +78,26 @@ class AddressEditorFragment : SecureFragment(R.layout.fragment_address_editor) {
     override fun onStop() {
         super.onStop()
         this.view?.hideKeyboard()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.address_editor, menu)
+        this.menu = menu
+
+        menu.findItem(R.id.delete_address_button).isVisible = isEditing
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.delete_address_button -> {
+            args.address?.let {
+                addressEditorView.showConfirmDeleteAddressDialog(requireContext(), it.guid)
+            }
+            true
+        }
+        R.id.save_address_button -> {
+            addressEditorView.saveAddress()
+            true
+        }
+        else -> false
     }
 }
