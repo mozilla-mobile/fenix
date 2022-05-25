7523bba26e99f5907352d793803a513dfa98fe30
Mugurell <Mugurell@users.noreply.github.com>
2022-05-25 18:32:25 +0300

    For #25352 - Add a new FML feature to gate Pocket sponsored stories

--- app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -294,7 +294,7 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity {
             if (settings().showPocketRecommendationsFeature) {
                 components.core.pocketStoriesService.startPeriodicStoriesRefresh()
             }
-            if (FeatureFlags.isPocketSponsoredStoriesFeatureEnabled(this@HomeActivity)) {
+            if (settings().showPocketSponsoredStories) {
                 components.core.pocketStoriesService.startPeriodicSponsoredStoriesRefresh()
             }
         }
