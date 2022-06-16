1147dc73dc6885f01e522276880eca0115c51337
mike a <mavduevskiy@gmail.com>
2022-06-16 19:19:34 -0400

    added compose sign in

--- app/src/main/java/org/mozilla/fenix/library/history/viewholders/SignInViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/library/history/viewholders/SignInViewHolder.kt
@@ -1,18 +1,46 @@
 package org.mozilla.fenix.library.history.viewholders
 
+import android.util.Log
 import android.view.View
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.recyclerview.widget.RecyclerView
 import org.mozilla.fenix.R
 import org.mozilla.fenix.databinding.HistoryListSignInBinding
 import org.mozilla.fenix.library.history.HistoryViewItem
+import org.mozilla.fenix.tabstray.syncedtabs.SyncedTabsErrorItem
+import org.mozilla.fenix.tabstray.syncedtabs.SyncedTabsListItem
+import org.mozilla.fenix.theme.FirefoxTheme
+import org.mozilla.fenix.theme.Theme
 
 class SignInViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = HistoryListSignInBinding.bind(view)
     private lateinit var item: HistoryViewItem.SignInHistoryItem
 
+    init {
+        binding.composeContainer.setViewCompositionStrategy(
+            ViewCompositionStrategy
+                .DisposeOnViewTreeLifecycleDestroyed
+        )
+    }
+
     fun bind(item: HistoryViewItem.SignInHistoryItem) {
-        binding.signInMessage.text = item.instructionText
+        binding.composeContainer.setContent {
+            FirefoxTheme {
+                    SyncedTabsErrorItem(
+                        errorText = item.instructionText,
+                        errorButton = SyncedTabsListItem.ErrorButton(
+                            buttonText = "Sign in",
+                            onClick = {
+                                Log.d("SignInViewHolder", "Sign in pressed!")
+                            }
+                        )
+                    )
+                }
+        }
         this.item = item
     }
 
