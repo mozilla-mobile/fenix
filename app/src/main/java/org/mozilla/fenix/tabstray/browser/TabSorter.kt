14de01b51208588dae7b9c42f99de28f21ff62c9
Noah Bond <nbond@mozilla.com>
2022-07-13 13:53:30 -0700

    For #21898 - Remove search term tab groups from the Tabs Tray

--- app/src/main/java/org/mozilla/fenix/tabstray/browser/TabSorter.kt
+++ app/src/main/java/org/mozilla/fenix/tabstray/browser/TabSorter.kt
@@ -4,13 +4,11 @@
 
 package org.mozilla.fenix.tabstray.browser
 
-import mozilla.components.browser.state.state.TabGroup
 import mozilla.components.browser.state.state.TabPartition
 import mozilla.components.browser.state.state.TabSessionState
 import mozilla.components.browser.tabstray.TabsTray
 import mozilla.components.feature.tabs.tabstray.TabsFeature
 import org.mozilla.fenix.ext.maxActiveTime
-import org.mozilla.fenix.tabstray.SEARCH_TERM_TAB_GROUPS_MIN_SIZE
 import org.mozilla.fenix.tabstray.TabsTrayAction
 import org.mozilla.fenix.tabstray.TabsTrayStore
 import org.mozilla.fenix.tabstray.ext.isActive
@@ -23,16 +21,12 @@ class TabSorter(
     private val settings: Settings,
     private val tabsTrayStore: TabsTrayStore? = null
 ) : TabsTray {
-    private val groupsSet = mutableSetOf<String>()
 
     override fun updateTabs(tabs: List<TabSessionState>, tabPartition: TabPartition?, selectedTabId: String?) {
         val privateTabs = tabs.filter { it.content.private }
         val allNormalTabs = tabs - privateTabs
         val inactiveTabs = allNormalTabs.getInactiveTabs(settings)
-        val tabGroups = tabPartition?.getTabGroups(settings, groupsSet) ?: emptyList()
-        val tabGroupTabIds = tabGroups.flatMap { it.tabIds }
-        val normalTabs = (allNormalTabs - inactiveTabs).filterNot { tabGroupTabIds.contains(it.id) }
-        val minTabPartition = tabPartition?.let { TabPartition(tabPartition.id, tabGroups) }
+        val normalTabs = allNormalTabs - inactiveTabs
 
         // Private tabs
         tabsTrayStore?.dispatch(TabsTrayAction.UpdatePrivateTabs(privateTabs))
@@ -42,12 +36,6 @@ class TabSorter(
 
         // Normal tabs
         tabsTrayStore?.dispatch(TabsTrayAction.UpdateNormalTabs(normalTabs))
-
-        // Search term tabs
-        tabsTrayStore?.dispatch(TabsTrayAction.UpdateTabPartitions(minTabPartition))
-
-        groupsSet.clear()
-        groupsSet.addAll(tabGroups.map { it.id })
     }
 }
 
@@ -58,19 +46,6 @@ private fun List<TabSessionState>.getInactiveTabs(settings: Settings): List<TabS
     val inactiveTabsEnabled = settings.inactiveTabsAreEnabled
     return if (inactiveTabsEnabled) {
         filter { !it.isActive(maxActiveTime) }
-    } else {
-        emptyList()
-    }
-}
-
-/**
- * Returns a list of tab groups based on our preferences.
- */
-private fun TabPartition.getTabGroups(settings: Settings, groupsSet: Set<String>): List<TabGroup> {
-    return if (settings.searchTermTabGroupsAreEnabled) {
-        tabGroups.filter {
-            it.tabIds.size >= SEARCH_TERM_TAB_GROUPS_MIN_SIZE || groupsSet.contains(it.id)
-        }
     } else {
         emptyList()
     }
