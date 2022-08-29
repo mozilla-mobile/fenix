036b0f8a8c5e4716cff1a15062114ede51057a11
Gabriel Luong <gabriel.luong@gmail.com>
2022-08-29 13:03:58 -0400

    For #26696 - Enable showSynCFR for Nightly

--- app/src/main/java/org/mozilla/fenix/FeatureFlags.kt
+++ app/src/main/java/org/mozilla/fenix/FeatureFlags.kt
@@ -26,7 +26,7 @@ object FeatureFlags {
     /**
      * Enables the onboarding sync CFR on the home screen.
      */
-    val showSynCFR = Config.channel.isDebug
+    val showSynCFR = Config.channel.isNightlyOrDebug
 
     /**
      * Enables the onboarding jump back in CFR on the home screen.
