812f073e3925fe2ce024a1f1861052bd97beca20
Gabriel Luong <gabriel.luong@gmail.com>
2022-09-07 01:29:15 -0400

    For #26777 - Part 2: Refactor the Jump Back In onboarding CFR to use the CFRPopup

--- app/src/main/java/org/mozilla/fenix/onboarding/HomeCFRPresenter.kt
+++ app/src/main/java/org/mozilla/fenix/onboarding/HomeCFRPresenter.kt
@@ -10,11 +10,13 @@ import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
 import mozilla.components.service.glean.private.NoExtras
 import org.mozilla.fenix.GleanMetrics.Onboarding
+import org.mozilla.fenix.GleanMetrics.RecentTabs
 import org.mozilla.fenix.R
 import org.mozilla.fenix.compose.cfr.CFRPopup
 import org.mozilla.fenix.compose.cfr.CFRPopupProperties
 import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.home.recentsyncedtabs.view.RecentSyncedTabViewHolder
+import org.mozilla.fenix.home.recenttabs.view.RecentTabsHeaderViewHolder
 
 /**
  * Vertical padding needed to improve the visual alignment of the popup and respect the UX design.
@@ -22,58 +24,119 @@ import org.mozilla.fenix.home.recentsyncedtabs.view.RecentSyncedTabViewHolder
 private const val CFR_TO_ANCHOR_VERTICAL_PADDING = -16
 
 /**
- * Delegate for handling synced tab onboarding CFR.
+ * Delegate for handling the Home Onboarding CFR.
  *
  * @param context [Context] used for various Android interactions.
- * @param recyclerView [RecyclerView] will serve as anchor for the sync CFR.
+ * @param recyclerView [RecyclerView] will serve as anchor for the CFR.
  */
 class HomeCFRPresenter(
     private val context: Context,
     private val recyclerView: RecyclerView,
 ) {
 
-    private var syncCFR: CFRPopup? = null
-
     /**
-     * Find the synced view and if it is available, then show the synced tab CFR.
+     * Determine the CFR to be shown on the Home screen and show a CFR for the resultant view
+     * if any.
      */
-    fun showSyncCFR() {
-        findSyncTabsView()?.let {
-            CFRPopup(
-                text = context.getString(R.string.sync_cfr_message),
-                anchor = it,
-                properties = CFRPopupProperties(
-                    indicatorDirection = CFRPopup.IndicatorDirection.DOWN,
-                    popupVerticalOffset = CFR_TO_ANCHOR_VERTICAL_PADDING.dp,
-                ),
-                onDismiss = {
-                    when (it) {
-                        true -> Onboarding.syncCfrExplicitDismissal.record(NoExtras())
-                        false -> Onboarding.syncCfrImplicitDismissal.record(NoExtras())
-                    }
-                }
-            ).apply {
-                syncCFR = this
-                show()
+    fun show() {
+        when (val result = getCFRToShow()) {
+            is Result.SyncedTab -> showSyncedTabCFR(view = result.view)
+            is Result.JumpBackIn -> showJumpBackInCFR(view = result.view)
+            else -> {
+                // no-op
             }
-
-            context.settings().showSyncCFR = false
-            context.settings().shouldShowJumpBackInCFR = false
-
-            Onboarding.synCfrShown.record(NoExtras())
         }
     }
 
-    private fun findSyncTabsView(): View? {
-        val count = recyclerView.adapter?.itemCount ?: return null
+    private fun showSyncedTabCFR(view: View) {
+        CFRPopup(
+            text = context.getString(R.string.sync_cfr_message),
+            anchor = view,
+            properties = CFRPopupProperties(
+                indicatorDirection = CFRPopup.IndicatorDirection.DOWN,
+                popupVerticalOffset = CFR_TO_ANCHOR_VERTICAL_PADDING.dp,
+            ),
+            onDismiss = {
+                when (it) {
+                    true -> Onboarding.syncCfrExplicitDismissal.record(NoExtras())
+                    false -> Onboarding.syncCfrImplicitDismissal.record(NoExtras())
+                }
+            }
+        ).show()
+
+        // Turn off both the recent tab and synced tab CFR after the recent synced tab CFR is shown.
+        context.settings().showSyncCFR = false
+        context.settings().shouldShowJumpBackInCFR = false
+
+        Onboarding.synCfrShown.record(NoExtras())
+    }
+
+    @Suppress("MagicNumber")
+    private fun showJumpBackInCFR(view: View) {
+        CFRPopup(
+            text = context.getString(R.string.onboarding_home_screen_jump_back_contextual_hint_2),
+            anchor = view,
+            properties = CFRPopupProperties(
+                indicatorDirection = CFRPopup.IndicatorDirection.DOWN,
+                popupVerticalOffset = (-40).dp, // Offset the top spacer in the recent tabs header.
+            ),
+            onDismiss = {
+                when (it) {
+                    true -> RecentTabs.jumpBackInCfrDismissed.record(NoExtras())
+                    false -> RecentTabs.jumpBackInCfrCancelled.record(NoExtras())
+                }
+            }
+        ).show()
+
+        // Users can still see the recent synced tab CFR after the recent tab CFR is shown in
+        // subsequent navigation to the Home screen.
+        context.settings().shouldShowJumpBackInCFR = false
+
+        RecentTabs.jumpBackInCfrShown.record(NoExtras())
+    }
+
+    /**
+     * Returns a [Result] that indicates the CFR that should be shown on the Home screen if any
+     * based on the views available and the preferences.
+     */
+    private fun getCFRToShow(): Result {
+        var result: Result = Result.None
+        val count = recyclerView.adapter?.itemCount ?: return result
 
         for (index in count downTo 0) {
             val viewHolder = recyclerView.findViewHolderForAdapterPosition(index)
-            if (viewHolder is RecentSyncedTabViewHolder) {
-                return viewHolder.composeView
+
+            if (context.settings().showSyncCFR && viewHolder is RecentSyncedTabViewHolder) {
+                result = Result.SyncedTab(view = viewHolder.composeView)
+                break
+            } else if (context.settings().shouldShowJumpBackInCFR &&
+                viewHolder is RecentTabsHeaderViewHolder
+            ) {
+                result = Result.JumpBackIn(view = viewHolder.composeView)
             }
         }
 
-        return null
+        return result
+    }
+
+    /**
+     * The result of determining which CFR to show on the Home screen.
+     */
+    sealed class Result {
+        /**
+         * Indicates no CFR should be shown on the Home screen.
+         */
+        object None : Result()
+
+        /**
+         * Indicates a CFR should be shown for a Synced Tab and the associated [view] to anchor
+         * the CFR.
+         */
+        data class SyncedTab(val view: View) : Result()
+
+        /**
+         * Indicates a CFR should be for Jump Back In and the associated [view] to anchor the CFR.
+         */
+        data class JumpBackIn(val view: View) : Result()
     }
 }
