6525439709ce0a14cbc0b7c342ef9fbc6f485bfb
Christian Sadilek <christian.sadilek@gmail.com>
2022-09-11 09:57:53 -0400

    Revert "Closes #26750: remove unused files from WallpaperOnboarding"
    This reverts commit 97987dbf8b23abe7397290682ca2d9d33f281338.

--- app/src/main/java/org/mozilla/fenix/wallpapers/WallpaperOnboarding.kt
+++ app/src/main/java/org/mozilla/fenix/wallpapers/WallpaperOnboarding.kt
@@ -23,9 +23,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.mozilla.fenix.R
 import org.mozilla.fenix.settings.wallpaper.WallpaperThumbnails
 import org.mozilla.fenix.theme.FirefoxTheme
