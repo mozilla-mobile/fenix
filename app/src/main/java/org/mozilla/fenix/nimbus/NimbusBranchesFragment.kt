ff1135fc9a8ff48e5c5186bbfe0816e94216c60c
Vladislav Rashkov <mrcrozer@gmail.com>
2022-11-02 12:06:18 +0200

    Converting Nimbus Branches and Nimbus Fragments to Compose

--- app/src/main/java/org/mozilla/fenix/nimbus/NimbusBranchesFragment.kt
+++ app/src/main/java/org/mozilla/fenix/nimbus/NimbusBranchesFragment.kt
@@ -8,20 +8,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import mozilla.components.lib.state.ext.consumeFrom
+import mozilla.components.lib.state.ext.observeAsComposableState
 import mozilla.components.support.base.log.logger.Logger
+import org.mozilla.experiments.nimbus.Branch
 import org.mozilla.fenix.R
 import org.mozilla.fenix.components.StoreProvider
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.showToolbar
 import org.mozilla.fenix.nimbus.controller.NimbusBranchesController
-import org.mozilla.fenix.nimbus.view.NimbusBranchesView
+import org.mozilla.fenix.theme.FirefoxTheme
 
 /**
  * A fragment to show the branches of a Nimbus experiment.
@@ -30,44 +52,8 @@ import org.mozilla.fenix.nimbus.view.NimbusBranchesView
 class NimbusBranchesFragment : Fragment() {
 
     private lateinit var nimbusBranchesStore: NimbusBranchesStore
-    private lateinit var nimbusBranchesView: NimbusBranchesView
-    private lateinit var controller: NimbusBranchesController
 
     private val args by navArgs<NimbusBranchesFragmentArgs>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        val view =
-            inflater.inflate(R.layout.mozac_service_nimbus_experiment_details, container, false)
-
-        nimbusBranchesStore = StoreProvider.get(this) {
-            NimbusBranchesStore(NimbusBranchesState(branches = emptyList()))
-        }
-
-        controller = NimbusBranchesController(
-            context = requireContext(),
-            navController = findNavController(),
-            nimbusBranchesStore = nimbusBranchesStore,
-            experiments = requireContext().components.analytics.experiments,
-            experimentId = args.experimentId,
-        )
-
-        nimbusBranchesView =
-            NimbusBranchesView(view.findViewById(R.id.nimbus_experiment_branches_list), controller)
-
-        loadExperimentBranches()
-
-        return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        consumeFrom(nimbusBranchesStore) { state ->
-            nimbusBranchesView.update(state)
-        }
-    }
 
     override fun onResume() {
         super.onResume()
@@ -89,6 +75,122 @@ class NimbusBranchesFragment : Fragment() {
                 )
             } catch (e: Throwable) {
                 Logger.error("Failed to getActiveExperiments()", e)
+            }
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        nimbusBranchesStore = StoreProvider.get(this) {
+            NimbusBranchesStore(NimbusBranchesState(branches = emptyList()))
+        }
+
+        val controller = NimbusBranchesController(
+            context = requireContext(),
+            navController = findNavController(),
+            nimbusBranchesStore = nimbusBranchesStore,
+            experiments = requireContext().components.analytics.experiments,
+            experimentId = args.experimentId,
+        )
+
+        loadExperimentBranches()
+
+        return ComposeView(requireContext()).apply {
+
+            setContent {
+                FirefoxTheme {
+
+                    val branches = nimbusBranchesStore.observeAsComposableState { state -> state.branches }
+                    val selectedBranch = nimbusBranchesStore.observeAsComposableState { state -> state.selectedBranch }
+
+                    NimbusBranches(
+                        selectedBranch = selectedBranch.value ?: "",
+                        branchesList = branches.value ?: emptyList(),
+                        onSelectedBranch = { selected ->
+                            controller.onBranchItemClicked(selected)
+                        }
+                    )
+
+                }
+            }
+        }
+    }
+
+
+    /**
+     * The list for the Nimbus Branches,
+     *
+     * @param branchesList List of branches
+     * @param onSelectedBranch Callback for when item is selected.
+     */
+    @Composable
+    fun NimbusBranches(
+        selectedBranch: String,
+        branchesList: List<Branch> = listOf(),
+        onSelectedBranch: (Branch) -> Unit
+    ) {
+
+        LazyColumn (
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            items(branchesList) { branch ->
+                NimbusBranchItem(
+                    item = branch,
+                    onSelectedBranch = onSelectedBranch,
+                    selectedBranch = selectedBranch
+                )
+            }
+        }
+    }
+
+    /**
+     * Nimbus branch item
+     *
+     * @param item current branch
+     */
+    @Composable
+    fun NimbusBranchItem(
+        item: Branch,
+        onSelectedBranch: (Branch) -> Unit,
+        selectedBranch: String
+    ) {
+        Row (modifier = Modifier.fillMaxWidth()
+            .clickable {
+                onSelectedBranch(item)
+            },
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            AnimatedVisibility(item.slug.equals(selectedBranch)) {
+                Icon(
+                    modifier = Modifier.padding(start = 16.dp),
+                    painter = painterResource(R.drawable.mozac_ic_check),
+                    contentDescription = stringResource(
+                        R.string.content_description_close_button,
+                    ),
+                    tint = FirefoxTheme.colors.iconPrimary,
+                )
+            }
+            Column (modifier = Modifier.padding(horizontal = 16.dp)){
+                Text(
+                    text = item.slug,
+                    fontSize = 16.sp,
+                    maxLines = 1,
+                    color = FirefoxTheme.colors.textPrimary,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = item.slug,
+                    fontSize = 12.sp,
+                    maxLines = 1,
+                    color = FirefoxTheme.colors.textPrimary,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
     }
--- app/src/main/java/org/mozilla/fenix/nimbus/NimbusExperimentsFragment.kt
+++ app/src/main/java/org/mozilla/fenix/nimbus/NimbusExperimentsFragment.kt
@@ -5,81 +5,119 @@
 package org.mozilla.fenix.nimbus
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
-import androidx.core.view.isVisible
+import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.launch
-import mozilla.components.service.nimbus.ui.NimbusExperimentAdapter
-import mozilla.components.support.base.log.logger.Logger
+import org.mozilla.experiments.nimbus.AvailableExperiment
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.components
-import org.mozilla.fenix.ext.runIfFragmentIsAttached
 import org.mozilla.fenix.ext.showToolbar
-import org.mozilla.fenix.nimbus.view.NimbusExperimentsView
+import org.mozilla.fenix.theme.FirefoxTheme
 
 /**
  * Fragment use for managing Nimbus experiments.
  */
 @Suppress("TooGenericExceptionCaught")
-class NimbusExperimentsFragment : Fragment(R.layout.mozac_service_nimbus_experiments) {
-
-    private var adapter: NimbusExperimentAdapter? = null
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        bindRecyclerView(view)
-    }
+class NimbusExperimentsFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
         showToolbar(getString(R.string.preferences_nimbus_experiments))
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // Letting go of the resources to avoid memory leak.
-        adapter = null
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View = ComposeView(requireContext()).apply {
 
-    private fun bindRecyclerView(view: View) {
-        val experimentsView = NimbusExperimentsView(
-            navController = findNavController(),
-        )
-
-        val recyclerView = view.findViewById<RecyclerView>(R.id.nimbus_experiments_list)
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        val shouldRefresh = adapter != null
-
-        lifecycleScope.launch(IO) {
-            try {
+        setContent {
+            FirefoxTheme {
                 val experiments =
                     requireContext().components.analytics.experiments.getAvailableExperiments()
 
-                lifecycleScope.launch(Main) {
-                    runIfFragmentIsAttached {
-                        if (!shouldRefresh) {
-                            adapter = NimbusExperimentAdapter(
-                                experimentsView,
-                                experiments,
+                NimbusExperiments(
+                    availableExperiments = experiments,
+                    onSelectedExperiment = { selectedExperiment ->
+                        val directions =
+                            NimbusExperimentsFragmentDirections.actionNimbusExperimentsFragmentToNimbusBranchesFragment(
+                                experimentId = selectedExperiment.slug,
+                                experimentName = selectedExperiment.userFacingName,
                             )
-                        }
 
-                        view.findViewById<TextView>(R.id.nimbus_experiments_empty_message).isVisible =
-                            experiments.isEmpty()
-                        recyclerView.adapter = adapter
+                        findNavController().navigate(directions)
                     }
+                )
+            }
+        }
+    }
+
+    /**
+     * The list for the Nimbus Experiments,
+     *
+     * @param availableExperiments List of available items
+     * @param onSelectedExperiment Callback for when item is selected.
+     */
+    @Composable
+    fun NimbusExperiments(
+        availableExperiments: List<AvailableExperiment> = listOf(),
+        onSelectedExperiment: (AvailableExperiment) -> Unit
+    ) {
+        LazyColumn (
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            items(availableExperiments) { experiment ->
+                NimbusExperimentItem(item = experiment, onSelectedExperiment = onSelectedExperiment)
+            }
+        }
+    }
+    /**
+        * Nimbus experiment item
+        *
+        * @param item current experiment
+    */
+    @Composable
+    fun NimbusExperimentItem(
+        item: AvailableExperiment,
+        onSelectedExperiment: (AvailableExperiment) -> Unit
+    ) {
+        Box (
+            modifier = Modifier.clickable {
+                    onSelectedExperiment(item)
                 }
-            } catch (e: Throwable) {
-                Logger.error("Failed to getActiveExperiments()", e)
-                view.findViewById<TextView>(R.id.nimbus_experiments_empty_message).isVisible = true
+        ) {
+            Column (
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ){
+                Text(
+                    text = item.userFacingName,
+                    fontSize = 16.sp,
+                    maxLines = 1,
+                    color = FirefoxTheme.colors.textPrimary,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(
+                    text = item.userFacingDescription,
+                    color = FirefoxTheme.colors.textPrimary,
+                    fontSize = 14.sp
+                )
             }
         }
     }
