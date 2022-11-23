9e0f427dd92a1909bd74f616b37d978f3ad9751b
Charlie Humphreys <chumphreys@mozilla.com>
2022-11-23 11:42:28 -0500

    update for pr feedback and add sync login event

--- app/src/androidTest/java/org/mozilla/fenix/ui/NimbusEventTest.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/NimbusEventTest.kt
@@ -3,17 +3,21 @@ package org.mozilla.fenix.ui
 import android.content.Intent
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
+import mozilla.components.concept.sync.*
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.*
+import org.mozilla.fenix.components.TelemetryAccountObserver
 import org.mozilla.fenix.helpers.*
+import org.mozilla.fenix.helpers.TestHelper.appContext
 import org.mozilla.fenix.ui.robots.homeScreen
+import io.mockk.mockk
 
 class NimbusEventTest {
     private lateinit var mDevice: UiDevice
     private lateinit var mockWebServer: MockWebServer
 
     @get:Rule
-    val activityTestRule = HomeActivityIntentTestRule.withDefaultSettingsOverrides()
+    val homeActivityTestRule = HomeActivityIntentTestRule.withDefaultSettingsOverrides()
         .withIntent(Intent().apply {
             action = Intent.ACTION_VIEW
         })
@@ -43,6 +47,16 @@ class NimbusEventTest {
 
         Experimentation.withHelper {
             Assert.assertTrue(evalJexl("'app_opened'|eventSum('Days', 28, 0) > 0"))
+        }
+    }
+
+    @Test
+    fun telemetryAccountObserverTest() {
+        val observer = TelemetryAccountObserver(appContext)
+        observer.onAuthenticated(mockk(), AuthType.Signin)
+
+        Experimentation.withHelper {
+            Assert.assertTrue(evalJexl("'sync_auth_sign_in'|eventSum('Days', 28, 0) > 0"))
         }
     }
 }
