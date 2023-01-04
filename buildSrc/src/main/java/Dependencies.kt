923bb27abf29b37f0e6dcf394638d8f1308f81dd
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-01-04 17:21:47 -0500

    Bug 1801022 - Update AndroidX testing libraries to the latest releases

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -235,11 +235,12 @@ object FenixDependencies {
     //   https://developer.android.com/jetpack/androidx/releases/test
     // For the full IDs of these test dependencies, see:
     //   https://developer.android.com/training/testing/set-up-project#android-test-dependencies
-    private const val androidx_test_shared_version = "1.5.0" // this appears to be shared with many deps.
-    private const val androidx_test_junit = "1.1.4"
+    private const val androidx_test_shared_version = "1.5.0"
+    private const val androidx_test_junit = "1.1.5"
     private const val androidx_test_orchestrator = "1.4.2"
+    private const val androidx_test_runner = "1.5.2"
     const val androidx_test_core = "androidx.test:core:$androidx_test_shared_version"
-    private const val androidx_espresso_version = "3.5.0"
+    private const val androidx_espresso_version = "3.5.1"
     const val espresso_core = "androidx.test.espresso:espresso-core:$androidx_espresso_version"
     const val espresso_contrib = "androidx.test.espresso:espresso-contrib:$androidx_espresso_version"
     const val espresso_idling_resources = "androidx.test.espresso:espresso-idling-resource:$androidx_espresso_version"
@@ -248,7 +249,7 @@ object FenixDependencies {
     const val androidx_test_extensions = "androidx.test.ext:junit-ktx:$androidx_test_junit"
     // Monitor is unused
     const val orchestrator = "androidx.test:orchestrator:$androidx_test_orchestrator"
-    const val tools_test_runner = "androidx.test:runner:$androidx_test_shared_version"
+    const val tools_test_runner = "androidx.test:runner:$androidx_test_runner"
     const val tools_test_rules = "androidx.test:rules:$androidx_test_shared_version"
     // Truth is unused
     // Test services is unused
