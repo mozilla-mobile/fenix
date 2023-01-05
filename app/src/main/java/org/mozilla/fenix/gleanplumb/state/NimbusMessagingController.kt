82d79cf69bf2e08938195c1677eb11cd9426a77f
James Hugman <james@hugman.tv>
2023-01-05 21:20:48 +0000

    Refactor messaging middleware Part II

--- app/src/main/java/org/mozilla/fenix/gleanplumb/state/NimbusMessagingController.kt
+++ app/src/main/java/org/mozilla/fenix/gleanplumb/state/NimbusMessagingController.kt
@@ -20,7 +20,7 @@ open class NimbusMessagingController(
     protected val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO),
     private val clock: () -> Long = { System.currentTimeMillis() },
 ) {
-    protected fun onMessageDisplayed(oldMessage: Message): Message {
+    fun onMessageDisplayed(oldMessage: Message): Message {
         sendShownMessageTelemetry(oldMessage.id)
         val newMetadata = oldMessage.metadata.copy(
             displayCount = oldMessage.metadata.displayCount + 1,
@@ -41,25 +41,25 @@ open class NimbusMessagingController(
         return newMessage
     }
 
-    protected fun onMessageDismissed(message: Message) {
+    fun onMessageDismissed(message: Message) {
         coroutineScope.launch {
             val updatedMetadata = message.metadata.copy(dismissed = true)
             messagingStorage.updateMetadata(updatedMetadata)
         }
     }
 
-    protected fun onMessageClicked(message: Message) {
+    fun onMessageClicked(message: Message) {
         coroutineScope.launch {
             val updatedMetadata = message.metadata.copy(pressed = true)
             messagingStorage.updateMetadata(updatedMetadata)
         }
     }
 
-    internal fun sendShownMessageTelemetry(messageId: String) {
+    private fun sendShownMessageTelemetry(messageId: String) {
         Messaging.messageShown.record(Messaging.MessageShownExtra(messageId))
     }
 
-    internal fun sendExpiredMessageTelemetry(messageId: String) {
+    private fun sendExpiredMessageTelemetry(messageId: String) {
         Messaging.messageExpired.record(Messaging.MessageExpiredExtra(messageId))
     }
 }
