f0f1da69b8b5d558915e69655a933368bc3374be
mcarare <mihai.carare.dev@gmail.com>
2023-01-09 17:08:32 +0200

    Bug 1804377 - Avoid unwanted dismissal of tab history.

--- app/src/main/java/org/mozilla/fenix/ext/NavController.kt
+++ app/src/main/java/org/mozilla/fenix/ext/NavController.kt
@@ -4,6 +4,7 @@
 
 package org.mozilla.fenix.ext
 
+import android.annotation.SuppressLint
 import androidx.annotation.IdRes
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
@@ -33,4 +34,15 @@ fun NavController.navigateSafe(
     if (currentDestination?.id == resId) {
         this.navigate(directions)
     }
+}
+
+/**
+ * Checks if the Fragment with a [fragmentClassName] is on top of the back queue.
+ */
+@SuppressLint("RestrictedApi")
+fun NavController.hasTopDestination(fragmentClassName: String): Boolean {
+    return this.backQueue.lastOrNull()?.destination?.displayName?.contains(
+        fragmentClassName,
+        true,
+    ) == true
 }
