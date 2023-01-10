8a525aa22185dc2ed2d5d619f8938d11c441b108
MickeyMoz <sebastian@mozilla.com>
2023-01-10 15:02:40 +0000

    Update to Android-Components 110.0.20230110143122.

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "110.0.20230109190305"
+    const val VERSION = "110.0.20230110143122"
 }
