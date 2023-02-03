ae584ebf56ad8b2ebf5161105571f703316d6833
gitstart <gitstart@users.noreply.github.com>
2023-02-03 12:16:16 +0000

    For #1807369 - Added Modifier.clickable for Talkback to mention Learn more as a hyperlink in the Pocket section

--- app/src/main/java/org/mozilla/fenix/compose/ClickableSubstringLink.kt
+++ app/src/main/java/org/mozilla/fenix/compose/ClickableSubstringLink.kt
@@ -5,17 +5,20 @@
 package org.mozilla.fenix.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import org.mozilla.fenix.R
 import org.mozilla.fenix.theme.FirefoxTheme
 
 /**
@@ -79,6 +82,12 @@ fun ClickableSubstringLink(
 
     ClickableText(
         text = annotatedText,
+        Modifier.clickable(
+            enabled = true,
+            onClickLabel = stringResource(id = R.string.a11y_action_label_read_article),
+            role = null,
+            onClick = onClick,
+        ),
         onClick = {
             annotatedText
                 .getStringAnnotations("link", it, it)
